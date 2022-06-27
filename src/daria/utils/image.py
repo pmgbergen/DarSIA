4bd46fc96cc5a812b4a2369d5f967ea5f4c4e34a
EStorvik <erlend.storvik@uib.no>
2022-06-27 09:43:03 +0200

    Image class and ROI extractor is now functioning
    Former-commit-id: 5bf7dca9d3d195629a14eba42580dd58832f4cf4

--- src/daria/utils/image.py
+++ src/daria/utils/image.py
@@ -5,23 +5,25 @@ import numpy as np
 
 class Image:
 
-    # Image should as of now be given in BGR, which is the openCV standard.
-    def __init__(self, img: np.array, origo=[0, 0], width=1, height=1, depth=0, dim=2):
-        self.img = img
+    # input img can either be a path to the image file or a numpy array.
+    def __init__(self, img, origo=[0, 0], width=1, height=1, depth=0, dim=2):
+        if type(img) == np.ndarray:
+            self.img = img
+        elif type(img) == str:
+            self.img = cv2.imread(img)
         self.origo = origo
         self.width = width
         self.height = height
         self.shape = self.img.shape
         self.dim = dim
-        self.dx = self.width / self.shape[0]
-        self.dy = self.height / self.shape[1]
+        print(self.shape)
+        self.dx = self.width / self.shape[1]
+        self.dy = self.height / self.shape[0]
         if self.dim == 3:
             self.depth = depth
             self.dz = self.depth / self.shape[2]
 
-    # Figure out how to have two declarators.
-
-    # When in the presence of internet, figure out how to access the name of the class and feed it to the save file.
-    def write(self, name="im", path="images/modified/", format=".jpg"):
+    # Write image to file. Here, the BGR-format is used. Image path, name and format can be changed by passing them as strings to the method.
+    def write(self, name="img", path="images/modified/", format=".jpg"):
         cv2.imwrite(path + name + format, self.img)
         print(path + name + format)
--- src/daria/utils/subregions.py
+++ src/daria/utils/subregions.py
@@ -1,20 +1,34 @@
 from daria import Image
 import numpy as np
 
-
-# Need to take a lot of care here. The images are oriented from top to bottom, and then left to right.
-# This might be the golden standard in computer science, but I still think that we should make everything intuitive
-#  for the physisists and mathematicians and try to conform to classical x,y(,z) coordinate systems.
-
-# Should extract region based on physical coordinates
-def extractSubRegion(img: Image) -> Image:
-    pass
-
-
-# Should extract region based on pixels
-def extractSubRegionByPixel(img: Image, x: list, y: list) -> Image:
+# Extracts region based on physical coordinates
+def extractROI(img: Image, x: list, y: list) -> Image:
     im = img.img
-    im = im[(img.shape[0] - y[1]) : (img.shape[0] - y[0]), x[0] : x[1]]
+    x_pix = [
+        round((x[0] - img.origo[0]) / img.dx),
+        round((x[1] - img.origo[0]) / img.dx),
+    ]
+    y_pix = [
+        round((y[0] - img.origo[1]) / img.dy),
+        round((y[1] - img.origo[1]) / img.dy),
+    ]
+    im = im[
+        (img.shape[0] - y_pix[1]) : (img.shape[0] - y_pix[0]),
+        x_pix[0] : x_pix[1],
+    ]
+    o = [x[0], y[0]]
+    w = x[1] - x[0]
+    h = y[1] - y[0]
+    return Image(im, o, w, h)
+
+
+# Extracts region based on pixels
+def extractROIPixel(img: Image, x: list, y: list) -> Image:
+    im = img.img
+    im = im[
+        (img.shape[0] - y[1]) : (img.shape[0] - y[0]),
+        x[0] : x[1],
+    ]
     o = [img.origo[0] + x[0] * img.dx, img.origo[1] + y[0] * img.dy]
     w = ((x[1] - x[0]) / img.shape[0]) * img.width
     h = ((y[1] - y[0]) / img.shape[1]) * img.height
