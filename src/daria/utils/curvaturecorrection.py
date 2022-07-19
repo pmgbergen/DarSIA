3873ec74d4bd638dc868b9f91c2fbfef3cd2dd6c
EStorvik <erlend.storvik@uib.no>
2022-07-19 21:12:44 +0200

    WIP: Have working CG and mod_richardson as well as derivatives

--- src/daria/utils/curvaturecorrection.py
+++ src/daria/utils/curvaturecorrection.py
@@ -3,20 +3,38 @@ from scipy.ndimage import map_coordinates
 import numpy as np
 
 
+# TODO: Add an automatic way (using e.g, gradient decent) to choose the parameters.
 def curvature_correction(
     image: np.ndarray,
     width: float = 284,
     height: float = 150,
-    vertical_bulge: float = 0.5e-6,
     horizontal_crop: float = 0.965,
     horizontal_bulge: float = -3.1e-6,
     horizontal_stretch: float = 2.35e-6,
     horizontal_stretch_mid: float = -0,
-    vertical_shear: float = 2e-3,
     vertical_crop: float = 0.92,
+    vertical_bulge: float = 0.5e-6,
+    vertical_shear: float = 2e-3,
     interpolation_order: int = 1,
 ) -> np.ndarray:
+    """
+    Curvature correction.
 
+    Arguments:
+        img (np.ndarray): image array, with pixel access, using y, x ordering, such that the
+            top left corner of the image corresponds to the (0,0) pixel.
+        img (str): path to image, alternative way to feed the actual image.
+        width (float): physical width of image.
+        height (float): physical height of image.
+        vertical_bulge (float): parameter for the curvature correction related to the vertical bulge of the image.
+        horizontal_crop (float): parameter for the curvature correction for cropping the image.
+        horizontal_bulge (float): parameter for the curvature correction related to the horizontal bulge of the image.
+        horizontal_stretch (float): parameter for the curvature correction related to the horizontal stretch of the image.
+        horizontal_stretch_mid (float): parameter for the curvature correction related to the horizontal stretch of the image.
+        vertical_crop (float): parameter for the curvature correction for cropping the image.
+        vertical_bulge (float): parameter for the curvature correction related to the vertical bulge of the image.
+        vertical_shear (float): parameter for the curvature correction correcting for vertical shear in the image.
+    """
     if isinstance(image, np.ndarray):
         img = image
     elif isinstance(image, str):
@@ -26,19 +44,18 @@ def curvature_correction(
             "Invalid image data. Provide either a path to an image or an image array."
         )
 
+    # Read size of image
     Ny, Nx, _ = img.shape
 
-    # imgfloat = img.astype(np.float64)
-
     # Center image, and set physical values
-    x = (np.array(range(1, Nx + 1)) - round(Nx / 2)) / round(Nx / 2) * width / 2
-    y = (np.array(range(1, Ny + 1)) - round(Ny / 2)) / round(Ny / 2) * height / 2
+    x = (np.array(range(1, Nx + 1)) - round(Nx / 2)) / Nx * width
+    y = (np.array(range(1, Ny + 1)) - round(Ny / 2)) / Ny * height
     y = y.reshape((1, Ny))
 
-    # Original grid
+    # Make it into a meshgrid
     X, Y = np.meshgrid(x, y)
 
-    # Curvature corrected grid
+    # Curvature corrected grid using input parameters
     Xmod = horizontal_crop * np.multiply(
         np.multiply(X, (1 + horizontal_stretch * (X - horizontal_stretch_mid) ** 2)),
         (1 - vertical_bulge * Y**2),
