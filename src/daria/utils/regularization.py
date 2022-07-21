10d841064ba631530e8bddb3a719bed015de0741
EStorvik <erlend.storvik@uib.no>
2022-07-21 19:23:23 +0200

    Added a solver class and stopping criterion class

--- src/daria/utils/regularization.py
+++ src/daria/utils/regularization.py
@@ -4,68 +4,71 @@ from typing import Callable
 import daria as da
 import numpy as np
 
+from daria.utils.linearalgebra import frobenius_norm
+
 
 def tv_denoising(
     img: da.Image,
     mu: float,
-    l: float = 1,
-    solver: Callable = da.cg,
-    tol: float = 0.01,
-    max_iter: int = 1000,
-    norm: Callable = da.frobenius_norm,
+    l: float,
+    solver: da.Solver = da.CG(da.StoppingCriterion(0.01, 100, da.frobenius_norm)),
+    stoppingCriterion: da.StoppingCriterion = da.StoppingCriterion(
+        0.01, 100, da.frobenius_norm
+    ),
     verbose: bool = False,
 ) -> da.Image:
     """
     Anisotropic TV denoising using the Bregman split from Goldstein and Osher: min_u = |dxu|+|dyu|+mu/2||u-f||^2_2
 
     Arguments:
-        img: The DarIA image that should be regularized
-        mu: Regularization coefficient
-        l: Penalty coefficient from Goldstein and Osher's algorithm
-        solver: Linear solver
+        img (daria.Image): Image that should be regularized
+        mu (float): Regularization coefficient
+        l (float): Penalty coefficient from Goldstein and Osher's algorithm
+        solver (daria.Solver): Linear solver, default is Conjugate Gradients
+        stoppingCriterion (daria.StoppingCriterion): stopping criterion containing information about tolerance, maximum number of iterations and norm
+        verbose (bool): Set to true
     """
 
-    # Make a copy of th original image for return
+    # Set verbosity of stopping criterion
+    stoppingCriterion.verbose = verbose
+    solver.stoppingCriterion.verbose = verbose
+
+    # Make a copy of the original image for return
     reg_image = img.copy()
+
+    # Extract the two images
     rhs = img.img
     im = reg_image.img
+
+    # Create algorithm specific functionality
     dx = np.zeros(rhs.shape)
     dy = np.zeros(rhs.shape)
     bx = np.zeros(rhs.shape)
     by = np.zeros(rhs.shape)
 
+    # Left-hand-side operator
     def lhsoperator(x: np.ndarray):
         return mu * x - l * da.laplace(x)
 
+    # Right-hand-side operator
     def rhsoperator(rhs, dx, dy, bx, by):
         return mu * rhs + l * (da.forward_diff_x(dx - bx) + da.backward_diff_y(dy - by))
 
     def shrink(x):
-        n = norm(x)
+        n = da.frobenius_norm(x)
         return x / n * max(n - 1 / l, 0)
 
-    for i in range(max_iter):
+    iterations = 0
+    increment = im
+
+    while not (stoppingCriterion.check_relative(increment, im, iterations)):
         im_old = np.ndarray.copy(im)
-        im = solver(
-            lhsoperator,
-            rhsoperator(rhs, dx, dy, bx, by),
-            im,
-            tol,
-            norm,
-            verbose=verbose,
-        )
+        im = solver.apply(lhsoperator, rhsoperator(rhs, dx, dy, bx, by), im)
         dx = shrink(da.backward_diff_x(im) + bx)
         dy = shrink(da.backward_diff_y(im) + by)
         bx = bx + da.backward_diff_x(im) - dx
         by = by + da.backward_diff_y(im) - dy
-        nr = norm(im - im_old) / norm(im)
-        if nr < tol:
-            if verbose:
-                print("Final relative increment: ", nr)
-                print("Total number of Bregman split iterations:", i)
-            break
-        if verbose:
-            print("Relative increment norm: ", nr)
-            print("Iteration number: ", i)
+        increment = im - im_old
+        iterations += 1
     reg_image.img = im.astype(np.uint8)
     return reg_image
