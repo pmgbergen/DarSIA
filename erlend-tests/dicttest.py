38123bb83adbc6cfe0503c1dde5412199a5fd8fd
EStorvik <erlend.storvik@uib.no>
2022-08-03 10:17:57 +0200

    Pushng latest changes
    Former-commit-id: 94cdfc79e82d5ca8c591baa6e3d246e2b98a31dc

--- erlend-tests/dicttest.py
+++
@@ -1,8 +0,0 @@
-import daria as da
-import cv2
-
-im = cv2.imread("images/originals/Baseline.jpg", 0)
-
-img = da.Image(im)
-
-print(im)
--- erlend-tests/readwriteresize.py
+++
@@ -1,9 +0,0 @@
-import daria as dia
-import cv2
-
-
-img = cv2.imread("images/originals/Profilbilde.jpg")
-img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
-cv2.imwrite("images/modified/Profilbilde05.jpg", img)
-img = cv2.resize(img, (0, 0), fx=2.0, fy=2.0)
-cv2.imwrite("images/modified/Profilbilde1.jpg", img)
