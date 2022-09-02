1f3a41536a0ca0d1d41040516d8b090ff7cb3d9b
Jakub Both <jakub.both@uib.no>
2022-09-02 14:33:04 +0200

    MAINT: Convert positional arguments of curvature correction to keyword arguments.:

--- src/daria/corrections/curvaturecorrection.py
+++ src/daria/corrections/curvaturecorrection.py
@@ -8,35 +8,37 @@ import numpy as np
 # TODO: Add an automatic way (using e.g, gradient decent) to choose the parameters.
 def curvature_correction(
     image: np.ndarray,
-    width: float = 284,
-    height: float = 150,
-    horizontal_crop: float = 0.965,
-    horizontal_bulge: float = -3.1e-6,
-    horizontal_stretch: float = 2.35e-6,
-    horizontal_stretch_mid: float = -0,
-    vertical_crop: float = 0.92,
-    vertical_bulge: float = 0.5e-6,
-    vertical_shear: float = 2e-3,
-    interpolation_order: int = 1,
+    **kwargs
 ) -> np.ndarray:
     """
     Curvature correction.
 
-    Arguments:
-        img (np.ndarray): image array, with pixel access, using y, x ordering, such that the
-            top left corner of the image corresponds to the (0,0) pixel.
-        img (str): path to image, alternative way to feed the actual image.
-        width (float): physical width of image.
-        height (float): physical height of image.
-        vertical_bulge (float): parameter for the curvature correction related to the vertical bulge of the image.
-        horizontal_crop (float): parameter for the curvature correction for cropping the image.
-        horizontal_bulge (float): parameter for the curvature correction related to the horizontal bulge of the image.
-        horizontal_stretch (float): parameter for the curvature correction related to the horizontal stretch of the image.
-        horizontal_stretch_mid (float): parameter for the curvature correction related to the horizontal stretch of the image.
-        vertical_crop (float): parameter for the curvature correction for cropping the image.
-        vertical_bulge (float): parameter for the curvature correction related to the vertical bulge of the image.
-        vertical_shear (float): parameter for the curvature correction correcting for vertical shear in the image.
+    Args:
+        img (np.ndarray): image array
+        kwargs (optional keyword arguments):
+            width (float): physical width of image.
+            height (float): physical height of image.
+            horizontal_crop (float): parameter for the curvature correction for cropping the image.
+            horizontal_bulge (float): parameter for the curvature correction related to the horizontal bulge of the image.
+            horizontal_stretch (float): parameter for the curvature correction related to the horizontal stretch of the image.
+            horizontal_stretch_mid (float): parameter for the curvature correction related to the horizontal stretch of the image.
+            vertical_crop (float): parameter for the curvature correction for cropping the image.
+            vertical_bulge (float): parameter for the curvature correction related to the vertical bulge of the image.
+            vertical_shear (float): parameter for the curvature correction correcting for vertical shear in the image.
+            interpolation_order (int): interpolation order to map back transformed image to Cartesian pixel grid
     """
+    # Read in tuning parameters
+    width: float = kwargs.pop("width", 284)
+    height: float = kwargs.pop("height", 150)
+    horizontal_crop: float = kwargs.pop("horizontal_crop", 0.965)
+    horizontal_bulge: float = kwargs.pop("horizontal_bulge", -3.1e-6)
+    horizontal_stretch: float = kwargs.pop("horizontal_stretch", 2.35e-6)
+    horizontal_stretch_mid: float = kwargs.pop("horizontal_stretch_mid", -0)
+    vertical_crop: float = kwargs.pop("vertical_crop", 0.92)
+    vertical_bulge: float = kwargs.pop("vertical_bulge", 0.5e-6)
+    vertical_shear: float = kwargs.pop("vertical_shear", 2e-3)
+    interpolation_order: int = kwargs.pop("interpolation_order", 1)
+
     if isinstance(image, np.ndarray):
         img = image
     elif isinstance(image, str):
