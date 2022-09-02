70597ed5eab0bdf5e6e29ac341faf47f8f9a68d3
EStorvik <erlend.storvik@uib.no>
2022-09-02 18:19:22 +0200

    isort now properly skips certain files. ci.yml checks flake8 and isort

--- src/daria/__init__.py
+++ src/daria/__init__.py
@@ -4,6 +4,9 @@ Includes:
 
     utils: conversion, image class, subregions, coordinate system, and curvature correction
 
+
+isort:skip_file
+
 """
 from daria.corrections.curvature.curvaturecorrection import *
 from daria.image.coordinatesystem import *
