25bcbc3e1e037a4df4ee0e4c701fa9fdfbdd86b9
Jakub Both <jakub.both@uib.no>
2022-09-06 01:44:01 +0200

    ENH: Introduce new (cleaner?) procedure for curvature correction.
    Introduces a different (hopefully more interpretable) set of DOFs.
    
    
    Former-commit-id: 7bbde76853f2d26bc39d94dd44cae698d756fc96

---
+++ src/daria/corrections/shape/curvature.py
@@ -0,0 +1,116 @@
+from __future__ import annotations
+
+import numpy as np
+from scipy.ndimage import map_coordinates
+
+
+# TODO: Add an automatic way (using e.g, gradient decent) to choose the parameters.
+# OR determine manual tuning rules.
+def simple_curvature_correction(img: np.ndarray, **kwargs) -> np.ndarray:
+    """
+    Correct image for bulge and stretch effects.
+
+    Args:
+        img (np.ndarray): image array
+        kwargs (optional keyword arguments):
+            width (float): physical width of image.
+            height (float): physical height of image.
+            "horizontal_crop" (float): parameter for the curvature correction for cropping the
+                image in x-direction; only required when correcting for stretch as well.
+            "horizontal_bulge" (float): parameter for the curvature correction related to the
+                horizontal bulge of the image.
+            "horizontal_stretch" (float): parameter for the curvature correction related to the
+                horizontal stretch of the image; cropping will be required
+            "horizontal_center_offset" (int): offset in terms of pixel of the image center in
+                x-direction, as compared to the numerical center
+            "vertical_crop" (float): parameter for the curvature correction for cropping the
+                image in y-direction; only required when correcting for stretch as well.
+            vertical_bulge (float): parameter for the curvature correction related to the
+                vertical bulge of the image.
+            "vertical_stretch" (float): parameter for the curvature correction related to the
+                vertical stretch of the image; cropping will be required
+            "vertical_center_offset" (int): offset in terms of pixel of the image center in
+                y-direction, as compared to the numerical center
+            "interpolation_order (int)": interpolation order to map back transformed image to
+                Cartesian pixel grid
+
+    Returns:
+        np.ndarray: corrected image
+
+    # NOTE: The same image size is used, i.e., the aspect ratio of the image
+    # is taken the same as the input and it is therefore implicitly assumed
+    # that the input image already is warped such that the aspect ratio of
+    # the image is correct. Also it i
+    """
+    # Read in tuning parameters
+    width: float = kwargs.pop("width", 1.0)
+    height: float = kwargs.pop("height", 1.0)
+    horizontal_crop: float = kwargs.pop(
+        "horizontal_crop", 1.0
+    )  # TODO automatic based on stretch?
+    horizontal_bulge: float = kwargs.pop("horizontal_bulge", 0.0)
+    horizontal_stretch: float = kwargs.pop("horizontal_stretch", 0.0)
+    horizontal_center_offset: int = kwargs.pop("horizontal_center_offset", 0)
+    vertical_crop: float = kwargs.pop(
+        "vertical_crop", 1.0
+    )  # TODO automatic based on stretch?
+    vertical_bulge: float = kwargs.pop("vertical_bulge", 0.0)
+    vertical_stretch: float = kwargs.pop("vertical_stretch", 0.0)
+    vertical_center_offset: int = kwargs.pop("vertical_center_offset", 0)
+    interpolation_order: int = kwargs.pop("interpolation_order", 1)
+
+    # Assume a true image in the form of an array is provided
+    if not isinstance(img, np.ndarray):
+        raise Exception(
+            "Invalid image data. Provide either a path to an image or an image array."
+        )
+
+    # Read size of image
+    Ny, Nx = img.shape[:2]
+
+    # NOTE: Finding the true centre of the image actually depends on many factors
+    # including lense properties. Thus, the task is actually quite hard. Here, a
+    # simple approach is used, simply choosing the numerical centre of the image
+    # corrected by the user.
+    image_center = [
+        round(Nx / 2) + horizontal_center_offset,
+        round(Ny / 2) + vertical_center_offset,
+    ]
+
+    # Define coordinate system relative to image center, scaled with physical dimensions
+    x = (np.array(range(1, Nx + 1)) - image_center[0]) / Nx * width
+    y = (np.array(range(1, Ny + 1)) - image_center[1]) / Ny * height
+
+    # Construct associated meshgrid
+    X, Y = np.meshgrid(x, y)
+
+    # Warp the coordinate system nonlinearly, correcting for bulge and stretch effects.
+    Xmod = horizontal_crop * (
+        X
+        + horizontal_bulge * np.multiply(X, (np.max(Y) - Y) * (Y - np.min(Y)))
+        + horizontal_stretch * X**3
+    )
+    Ymod = vertical_crop * (
+        Y
+        + vertical_bulge * np.multiply(Y, (np.max(X) - X) * (X - np.min(X)))
+        + vertical_stretch * Y**3
+    )
+
+    # Map corrected grid back to positional arguments, i.e. invert the definition
+    # of the local coordinate system
+    Xmod = image_center[0] - 1 + Xmod * Nx / width
+    Ymod = image_center[1] - 1 + Ymod * Ny / height
+
+    # Create out grid as the corrected grid, but not in meshgrid format
+    out_grid = np.array([Ymod.ravel(), Xmod.ravel()])
+
+    # Define the shape corrected image.
+    img_mod = np.zeros_like(img, dtype=np.uint8)
+
+    # Do interpolate original image on the new grid
+    for i in range(img.shape[2]):
+        in_data = img[:, :, i]
+        im_array = map_coordinates(in_data, out_grid, order=interpolation_order)
+        img_mod[:, :, i] = im_array.reshape(img.shape[:2]).astype(np.uint8)
+
+    return img_mod
