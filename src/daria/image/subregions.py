c67d3044ed1cef460dec7e86894fecdb4b3a6177
Jakub Both <jakub.both@uib.no>
2022-09-11 00:30:42 +0200

    MAINT: Make subregions computations cheaper.

--- src/daria/image/subregions.py
+++ src/daria/image/subregions.py
@@ -1,74 +1,60 @@
 from daria import Image
 
 
-def extractROI(img: Image, x: list, y: list) -> Image:
+def extractROI(img: Image, x: list, y: list, return_roi: bool = False) -> Image:
     """Extracts region of interest based on physical coordinates.
 
     Arguments:
         x (list): list with two elements and containst the start and end point in x-direction;
-            points in physical coordinates.
+            points in metric units.
         y (list): list with two elements and containst the start and end point in y-direction;
-            points in physical coordinates.
+            points in metric units.
 
     Returns:
         Image: image object restricted to the ROI.
     """
 
-    # TODO conversion of pixels
+    # Assume that x and y are in increasing order.
+    assert x[0] < x[1] and y[0] < y[1]
 
-    # Determine pixel ranges in x- and y-direction, corresponding to the ROI
-    x_pix = [
-        round((x[0] - img.origo[0]) / img.dx),
-        round((x[1] - img.origo[0]) / img.dx),
-    ]
-    y_pix = [
-        round((y[0] - img.origo[1]) / img.dy),
-        round((y[1] - img.origo[1]) / img.dy),
-    ]
+    # Convert metric units to number of pixels, and define top-left and bottom-right
+    # corners of the roi, towards addressing the image with conventional ordering
+    # of x and y coordinates.
+    top_left_corner = img.coordinatesystem.coordinateToPixel((x[0], y[1]))
+    bottom_right_corner = img.coordinatesystem.coordinateToPixel((x[1], y[0]))
 
-    # Extract ROI
-    # TODO do not copy full image - expensive
-    # TODO conversion of pixels
-    img = img.img
-    img = img[
-        (img.shape[0] - y_pix[1]) : (img.shape[0] - y_pix[0]),
-        x_pix[0] : x_pix[1],
-    ]
+    # Define the ROI
+    roi = (slice(top_left_corner[0], bottom_right_corner[0]), slice(top_left_corner[1], bottom_right_corner[1]))
 
-    # Define metadata
+    # Define metadata (all quantities in metric units)
     origo = [x[0], y[0]]
     width = x[1] - x[0]
     height = y[1] - y[0]
 
-    # Construct and return image correpsonding to ROI
-    return Image(img=img, origo=origo, width=width, height=height)
+    # Construct and return image corresponding to ROI
+    if return_roi:
+        return Image(img=img.img[roi], origo=origo, width=width, height=height), roi
+    else:
+        return Image(img=img.img[roi], origo=origo, width=width, height=height)
 
-
-def extractROIPixel(img: Image, x: list, y: list) -> Image:
-    """Extracts region of interest based on pixels.
+def extractROIPixel(img: Image, roi: tuple) -> Image:
+    """Extracts region of interest based on pixel info.
 
     Arguments:
-        x (list of int):  list with two elements, containing the start and end pixels in
-            x-direction, using physical pixel ordering.
-        y (list of int):  list with two elements, containing the start and end pixels in
-            y-direction, using physical pixel ordering.
+        roi (tuple of slices): to be used straight away to extract a region of interest;
+            using the conventional pixel ordering
 
     Returns:
         Image: image object restricted to the ROI.
     """
-    # Restrict the image with repect to given pixel ranges
-    # TODO do not copy full image - expensive
-    # TODO conversion of pixels
-    img = img.img
-    img = img[
-        (img.shape[0] - y[1]) : (img.shape[0] - y[0]),
-        x[0] : x[1],
+    # Define metadata; Note that img.origo uses a Cartesian ordering, while roi
+    # uses the conventional pixel ordering
+    origo = [
+        img.origo[0] + roi[1].start * img.dx,
+        img.origo[1] + roi[0].stop * img.dy
     ]
-
-    # Define metadata
-    origo = [img.origo[0] + x[0] * img.dx, img.origo[1] + y[0] * img.dy]
-    width = ((x[1] - x[0]) / img.shape[0]) * img.width
-    height = ((y[1] - y[0]) / img.shape[1]) * img.height
+    width = (roi[1].stop - roi[1].start) * img.dx
+    height = (roi[0].stop - roi[0].start) * img.dy
 
     # Construct and return image corresponding to ROI
-    return Image(img=img, origo=origo, width=width, height=height)
+    return Image(img=img.img[roi], origo=origo, width=width, height=height)
