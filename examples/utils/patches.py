f05827ff2240d67bcf46d5149caac86de26db832
EStorvik <erlend.storvik@uib.no>
2022-09-10 09:45:06 +0200

    removed redundancies from patches

--- examples/utils/patches.py
+++ examples/utils/patches.py
@@ -13,9 +13,3 @@ patch.images[1, 1].show()
 
 reassembled = patch.assemble()
 reassembled.show()
-
-# a = np.array([[[1.0, 1.1], [2.0, 2.1]], [[3.0, 3.1], [4.0, 4.1]]])
-# b = np.array([[[5.0, 5.1], [6.0, 6.1]], [[7.0, 7.1], [8.0, 8.1]]])
-# print(np.c_[a[:], b[:]])
-# print(a)
-# print(b)
