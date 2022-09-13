d6037309da2385af1509a636bf751adba58368ac
Jakub Both <jakub.both@uib.no>
2022-09-13 03:12:41 +0200

    EX: Update color correction example, and add small image.
    Former-commit-id: 8c42c127dcce1b757e7b3eac2eecc7fa9eeb1b20

--- examples/correction/colour_correction_colorchecker.py
+++ examples/correction/colour_correction_colorchecker.py
@@ -1,35 +1,36 @@
+from pathlib import Path
+
 import cv2
+import matplotlib.pyplot as plt
+
 from daria.corrections.color import ColorCorrection
 
 # -------- Convert the image into linear RGB color space
 
 # Fetch image, in BGR
-img_BGR = cv2.imread("../../images/fluidflower/Baseline.jpg")
+img_BGR = cv2.imread(str(Path("./baseline.jpg")))
 
 # Convert to RGB (int)
 img_RGB = cv2.cvtColor(img_BGR, cv2.COLOR_BGR2RGB)
 
-# -------- Match colors and apply correction to original picture using the nonlinear RGB colour space
-
-# Apply colour correction based on color checker
-colorcorrection = ColorCorrection()
+# -------- Apply colour correction based on color checker
 
 # Need to specify a ROI which contains the color checker
-roi_cc = (slice(100, 600), slice(350, 600))
+roi_cc = (slice(0, 240), slice(0, 240))
 
 # Apply color correction
-corrected_baseline_RGB = colorcorrection.adjust(img_RGB, roi_cc, verbosity=True, whitebalancing=True)
+colorcorrection = ColorCorrection()
+corrected_baseline_RGB = colorcorrection.adjust(
+    img_RGB, roi_cc, verbosity=False, whitebalancing=True
+)
+
+# NOTE: For some reason, when choosing verbosity=True the colour checker is displayed,
+# but no other images can be properly displayed afterwards anymore.
 
 # -------- Plot images pre and post correction, and store both images
 
-cv2.imwrite("colour-correction/baseline.jpg", img_BGR)
-cv2.namedWindow("original baseline", cv2.WINDOW_NORMAL)
-cv2.imshow("original baseline", img_BGR)
-cv2.waitKey(0)
-
-# Convert to BGR - for plotting
-corrected_baseline_BGR = cv2.cvtColor(corrected_baseline_RGB, cv2.COLOR_RGB2BGR)
-cv2.imwrite("colour-correction/baseline_corrected.jpg", corrected_baseline_BGR)
-cv2.namedWindow("corrected baseline", cv2.WINDOW_NORMAL)
-cv2.imshow("corrected baseline", corrected_baseline_BGR)
-cv2.waitKey(0)
+fig, ax = plt.subplots(1, num=1)
+ax.imshow(img_RGB)
+fig, ax = plt.subplots(1, num=2)
+ax.imshow(corrected_baseline_RGB)
+plt.show()
