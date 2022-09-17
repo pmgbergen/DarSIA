6cfa1a9422efb3df895abc06f4c6c7a69d4c25df
Jakub Both <jakub.both@uib.no>
2022-09-18 01:13:41 +0200

    ENH: Fix many small issue, and make translating images work!

--- src/daria/analysis/compaction.py
+++ src/daria/analysis/compaction.py
@@ -1,5 +1,6 @@
 """
-Module containing class for compaction analysis.
+Module containing class for translation analysis, relevant e.g. for
+studying compaction of porous media.
 """
 
 from typing import Optional
@@ -12,9 +13,9 @@ from scipy.interpolate import RBFInterpolator
 import daria
 
 
-class CompactionAnalysis:
+class TranslationAnalysis:
     """
-    Class for compaction analysis.
+    Class for translation analysis.
     """
 
     def __init__(
@@ -25,7 +26,7 @@ class CompactionAnalysis:
         translationEstimator: daria.TranslationEstimator,
     ) -> None:
         """
-        Constructor for CompactionAnalysis.
+        Constructor for TranslationAnalysis.
 
         Args:
             img_src (daria.Image): base image
@@ -43,8 +44,8 @@ class CompactionAnalysis:
             img_src, *self.N_patches, rel_overlap=self.rel_overlap
         )
 
-        # Determine the centers of the patches
-        self.patch_centers = self.patches_src.centers
+        # Determine the centers of the patches, in Cartesian coordinates
+        self.patch_centers_cartesian = self.patches_src.global_centers_cartesian
 
     def update_params(
         self, N_patches: Optional[list[int]] = None, rel_overlap: Optional[float] = None
@@ -77,28 +78,39 @@ class CompactionAnalysis:
                 self.img_src, *self.N_patches, rel_overlap=self.rel_overlap
             )
 
-            # Determine the centers of the patches
+            # Determine the centers of the patches in Cartesian coordinates
             if need_update_N_patches:
-                self.patch_centers = self.patches_src.centers
+                self.patch_centers_cartesian = self.patches_src.global_centers_cartesian
 
-    def find_compaction_map(self, img_dst: daria.Image, units: list[str]=["metric", "metric"]) -> tuple:
+    def find_translation(
+        self, img_dst: daria.Image, units: list[str] = ["metric", "pixel"]
+    ) -> tuple:
         """
-        Find compaction map in order to match the base image to the provided image.
+        Find translation map as translation required to match the base image to the
+        provided image.
 
-        The compaction will be measure in metric units.
+        The final translation map will be stored as callable function. And it allows
+        various input and output spaces (metric vs. pixel).
 
         Args:
             img_dst (daria.Image): test image
             units (list of str): units for input (first entry) and output (second entry)
-                of the resulting compaction map; accepts either "metric" (default for both)
+                of the resulting translation map; accepts either "metric" (default for both)
                 or "pixel".
 
         Returns:
-            RBFInterpolator: compaction map as interpolator in space
-            np.ndarray: flag indicating on which patches the routine has been successful
+            Callable: translation map defined as interpolator
+            bool: flag indicating on which patches the routine has been successful
         """
         # Assert correct units
         assert all([unit in ["metric", "pixel"] for unit in units])
+
+        # Overall strategy:
+        # 1. Determine translation on patches.
+        # 2. Add potentially known boundary conditions.
+        # 3. Create interpolator which is the main result.
+
+        # ! ---- Step 1. Patch analysis.
 
         # Construct patches of the test image
         patches_dst = daria.Patches(
@@ -131,7 +143,8 @@ class CompactionAnalysis:
                 img_src = self.patches_src(i, j)
                 img_dst = patches_dst(i, j)
 
-                # Determine effective translation measured in number of pixels
+                # Determine effective translation, operating on pixel coordinates using
+                # reverse matrix indexing.
                 (
                     translation,
                     intact_translation,
@@ -147,130 +160,161 @@ class CompactionAnalysis:
                     # Flag success
                     have_translation[i, j] = True
 
-                    # Fetch the center of the patch in metric units, which will be the input for
-                    # later construction of the interpolator
-                    center = self.patch_centers[i,j]
+                    # Fetch the center of the patch in metric units, which will be the input
+                    # for later construction of the interpolator
+                    center = self.patch_centers_cartesian[i, j]
 
-                    # Convert to pixel units if required
+                    # Convert to pixel units using reverse matrix indexing, if required
                     if units[0] == "pixel":
-                        # TODO
-                        center = center
+                        center = self.img_src.coordinatesystem.coordinateToPixel(
+                            center, reverse=True
+                        )
 
                     # Extract the effective displacement, stored in the constant part of
-                    # the affine map, in pixel units
+                    # the affine map, in pixel units, using reverse matrix indexing.
                     displacement = translation[:, -1]
 
-                    # Convert to metric units if required
+                    # Convert to metric units if required - note that displacement is a
+                    # vector, and that it is given using reverse matrix indexing
                     if units[1] == "metric":
-                        # TODO
-                        # Also take into account the flip, i.e., flip the direction with minus sign
-                        displacement = displacement
-
-                    print("test")
-                    print(displacement)
-                    print(self.patches_src.baseImg.coordinatesystem.pixelsToLength(displacement, "xy"))
+                        displacement = self.img_src.coordinatesystem.pixelToCoordinateVector(
+                            displacement, reverse=True
+                        )
 
                     # Store the displacement for the centers of the patch.
+                    # NOTE: In any case, the first and second components
+                    # correspond to the x and y component.
                     input_coordinates.append(center)
                     patch_translation_x.append(displacement[0])
                     patch_translation_y.append(displacement[1])
 
-                    ## For debugging purposes only
-                    # if False:
+        # ! ---- Step 2. Boundary conditions.
 
-                    #    # Apply translation
-                    #    (h, w) = img_dst.shape[:2]
-                    #    aligned_img_src = cv2.warpAffine(img_src.img, translation, (w, h))
+        # Fetch predetermined conditions (do not have to be on the boundary)
+        extra_coordinates_x, extra_translation_x = self.bc_x(units)
+        extra_coordinates_y, extra_translation_y = self.bc_y(units)
 
-                    #    # Plot the original two images and the corrected ones
-                    #    fig, ax = plt.subplots(2, 1)
-                    #    ax[0].imshow(
-                    #        skimage.util.compare_images(
-                    #            img_src.img, img_dst.img, method="blend"
-                    #        )
-                    #    )
-                    #    ax[1].imshow(
-                    #        skimage.util.compare_images(
-                    #            aligned_img_src, img_dst.img, method="blend"
-                    #        )
-                    #    )
-                    #    plt.show()
-
-        # Goal: Interpolate the effective translation in all patches
-
-        # Supplement computed data with some boundary condition data.
-        # For instance at the left and right boundary, there is no displacement
-        # in x direction, while there is none in y-direction at the bottom.
-        # Since this will result in two different data sets (for the coordinates)
-        # a copy has to be made.
-        input_coordinates_translation_x = input_coordinates.copy()
-        input_coordinates_translation_y = input_coordinates.copy()
-
-        # In order to still be able to plot the computed values without
-        # the boundary data, also the translation data has to be copied.
-        # FIXME: Copy needed? Only for plotting of displacement arrows.
-        patch_translation_x_with_bc = patch_translation_x.copy()
-        patch_translation_y_with_bc = patch_translation_y.copy()
-
-        # TODO adapt according to unit[0] how to loop through the sets
-
-        # Begin with known translation at the left and right boundary
-        for y_loc in np.linspace(0, img_src.height, self.N_patches[1] + 1):
-            # Left boundary patches
-            input_coordinates_translation_x.append(img_src.origo + np.array([0, y_loc]))
-            patch_translation_x_with_bc.append(0.0)
-
-            # Right boundary patches
-            input_coordinates_translation_x.append(img_src.origo + np.array([img_src.width, y_loc]))
-            patch_translation_x_with_bc.append(0.0)
-
-        # Also add known translation at the top and bottom
-        for x_loc in np.linspace(0, img_src.width, self.N_patches[0] + 1):
-            # Bottom boundary patches
-            input_coordinates_translation_y.append(img_src.origo + np.array([x_loc, 0]))
-            patch_translation_y_with_bc.append(0.0)
+        # ! ---- Step 3. Interpolation.
 
         # Finally define separate interpolators for the translation in x and y
         # directions.
         self.interpolator_translation_x = RBFInterpolator(
-            np.array(input_coordinates_translation_x),
-            patch_translation_x_with_bc,
+            input_coordinates + extra_coordinates_x,
+            patch_translation_x + extra_translation_x,
         )
         self.interpolator_translation_y = RBFInterpolator(
-            np.array(input_coordinates_translation_y),
-            patch_translation_y_with_bc,
+            input_coordinates + extra_coordinates_y,
+            patch_translation_y + extra_translation_y,
         )
 
-        return (
-            self.interpolator_translation_x,
-            self.interpolator_translation_y,
-            have_translation,
-        )
+        # Convert interpolators to a callable displacement/translation map
+        def translation(arg):
+            return np.array(
+                [
+                    self.interpolator_translation_x(arg),
+                    self.interpolator_translation_y(arg),
+                ]
+            )
 
-    def find_translation(self, img_dst: daria.Image) -> None:
+        self.translation = translation
+
+        # Store success
+        self.have_translation = have_translation.copy()
+
+        return self.translation, self.have_translation.any()
+
+    def bc_x(self, units: list[str]) -> tuple:
         """
-        Compute compaction map as interpolator and apply to patch centers.
+        Prescribed (boundary) conditions for the displacement in x direction.
+
+        Can be overwritten. Here, tailored to FluidFlower scenarios, fix
+        the displacement in x-direction at the vertical boundaries of the
+        image.
 
         Args:
-            img_dst (daria.Image): test image
+            units (list of str): "metric" or "pixel"
 
         Returns:
-            np.ndarray: translation in x direction
-            np.ndarray: translation in y direction
+            list of np.ndarray: coordinates
+            list of float: translation in x direction
         """
-        # Determine interpolators for both component of the translation
-        (
-            self.interpolator_translation_x,
-            self.interpolator_translation_y,
-            self.have_translation,
-        ) = self.find_compaction_map(img_dst)
+        # The loop over the boundary will depend on whether the coordinates
+        # are interpreted as pixels or in metric units. Define the respective
+        # sets here, and combine left and right vertical boundaries for simplicity,
+        # as they will get assigned the same translation.
+        vertical_boundary: list[np.ndarray] = []
+        if units[0] == "metric":
+            # Add the left vertical boundary
+            vertical_boundary += [
+                self.img_src.origo + np.array([0, y_pos])
+                for y_pos in np.linspace(0, self.img_src.height, self.N_patches[1] + 1)
+            ]
+            # Add the right vertical boundary
+            vertical_boundary += [
+                self.img_src.origo + np.array([self.img_src.width, y_pos])
+                for y_pos in np.linspace(0, self.img_src.height, self.N_patches[1] + 1)
+            ]
 
-        def displacement(arg):
-            return np.array([self.interpolator_translation_x(arg), self.interpolator_translation_y(arg)])
+        elif units[0] == "pixel":
+            # Add the left vertical boundary - comply to reverse matrix indexing
+            vertical_boundary += [
+                np.array([0, y_pos])
+                for y_pos in np.linspace(
+                    0, self.img_src.num_pixels_height, self.N_patches[1] + 1
+                )
+            ]
+            # Add the right vertical boundary - comply to reverse matrix indexing
+            vertical_boundary += [
+                np.array([self.img_src.num_pixels_width, y_pos])
+                for y_pos in np.linspace(
+                    0, self.img_src.num_pixels_height, self.N_patches[1] + 1
+                )
+            ]
 
-        self.displacement = displacement
+        return vertical_boundary, len(vertical_boundary) * [0.0]
 
-    def translate_centers(self, img_dst: daria.Image, plot_translation: bool = False) -> None:
+    def bc_y(self, units: list[str]) -> tuple:
+        """
+        Prescribed (boundary) conditions for the displacement in y direction.
+
+        Args:
+            units (list of str): "metric" or "pixel"
+
+        Can be overwritten. Here, tailored to FluidFlower scenarios, fix
+        the displacement in y-direction at the horizontal boundaries of the
+        image.
+
+        Returns:
+            list of np.ndarray: coordinates
+            list of float: translation in y direction
+        """
+
+        # The loop over the boundary will depend on whether the coordinates
+        # are interpreted as pixels or in metric units. Define the respective
+        # sets here, and combine left and right vertical boundaries for simplicity,
+        # as they will get assigned the same translation.
+        horizontal_boundary: list[np.ndarray] = []
+        if units[0] == "metric":
+            # Add the bottom horizontal boundary
+            horizontal_boundary += [
+                self.img_src.origo + np.array([x_pos, 0])
+                for x_pos in np.linspace(0, self.img_src.width, self.N_patches[0] + 1)
+            ]
+
+        elif units[0] == "pixel":
+            # Add the bottom horizontal boundary - comply to reverse matrix indexing
+            horizontal_boundary += [
+                np.array([x_pos, self.img_src.num_pixels_height])
+                for x_pos in np.linspace(
+                    0, self.img_src.num_pixels_width, self.N_patches[0] + 1
+                )
+            ]
+
+        return horizontal_boundary, len(horizontal_boundary) * [0.0]
+
+    def translate_centers(
+        self, img_dst: daria.Image, plot_translation: bool = False
+    ) -> None:
         """
         Translate centers of the base image.
 
@@ -285,8 +329,8 @@ class CompactionAnalysis:
         input_arg = np.transpose(
             np.vstack(
                 (
-                    np.flipud(self.patch_centers[:, :, 0].T).flatten(),
-                    np.flipud(self.patch_centers[:, :, 1].T).flatten(),
+                    np.flipud(self.patch_centers_cartesian[:, :, 0].T).flatten(),
+                    np.flipud(self.patch_centers_cartesian[:, :, 1].T).flatten(),
                 )
             )
         )
@@ -298,7 +342,7 @@ class CompactionAnalysis:
         patch_centers_y_pixels = np.zeros(tuple(reversed(self.N_patches)), dtype=int)
         for j in range(self.N_patches[1]):
             for i in range(self.N_patches[0]):
-                center = self.patch_centers[i, j]
+                center = self.patch_centers_cartesian[i, j]
                 pixel = self.img_src.coordinatesystem.coordinateToPixel(center)
                 patch_centers_x_pixels[self.N_patches[1] - 1 - j, i] = pixel[1]
                 patch_centers_y_pixels[self.N_patches[1] - 1 - j, i] = pixel[0]
@@ -330,3 +374,33 @@ class CompactionAnalysis:
                 )
             )
             plt.show()
+
+    def translate_image(
+        self,
+        img_src: daria.Image,
+        reverse: bool = False,
+        plot_transformation: bool = False,
+    ) -> daria.Image:
+        """
+        Apply translation to an entire image by using piecwise perspective transformation.
+
+        Args:
+            img_dst (daria.Image): source image to be translated (not necessarily self.img_src)
+            method (str): either "forward" or "reverse" for applying positive or negative
+                displacement
+            plot_transformation (bool): plot the transformed image
+
+        Returns:
+            daria.Image: translated image
+        """
+
+        # Construct patches of the base image without overlap
+        patches_src = daria.Patches(img_src, *self.N_patches, rel_overlap=0.0)
+
+        # Create piecewise perspective transform on the patches
+        perspectiveTransform = daria.PiecewisePerspectiveTransform()
+        transformed_img: daria.Image = perspectiveTransform.find_and_warp(
+            patches_src, self.translation, reverse
+        )
+
+        return transformed_img
