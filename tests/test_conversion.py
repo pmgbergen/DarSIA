889bf3252d7c48afa00e85b9d391a1f7db8541ab
EStorvik <erlend.storvik@uib.no>
2022-09-21 17:38:38 +0200

    removed redundant test

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -1,9 +1,2 @@
 import numpy as np
-from daria import BGR2RGB
-
-
-def test_bgr2rgb():
-    imgBGR = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
-    imgRGB = np.array([[[3, 2, 1], [6, 5, 4]], [[9, 8, 7], [12, 11, 10]]])
-    np.testing.assert_array_equal(BGR2RGB(imgBGR), imgRGB)
-    np.testing.assert_array_equal(BGR2RGB(imgRGB), imgBGR)
+import daria
