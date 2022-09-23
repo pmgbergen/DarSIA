133ee906c0112741382a0473c3254798a0ce5437
Jakub Both <jakub.both@uib.no>
2022-09-23 09:36:03 +0200

    MAINT: Add missing file (resolution) to init

--- src/daria/__init__.py
+++ src/daria/__init__.py
@@ -17,6 +17,7 @@ from daria.mathematics.stoppingcriterion import *
 from daria.mathematics.solvers import *
 from daria.mathematics.regularization import *
 from daria.utils.conversions import *
+from daria.utils.resolution import *
 from daria.corrections.shape.curvature import *
 from daria.corrections.shape.translation import *
 from daria.corrections.shape.piecewiseperspective import *
