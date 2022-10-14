21d0037776a59b06b341e99477e200a41acccc0d
EStorvik <erlend.storvik@uib.no>
2022-10-14 10:47:28 +0200

    Apply jakubs comments

--- src/daria/analysis/segmentationcomparison.py
+++ src/daria/analysis/segmentationcomparison.py
@@ -9,13 +9,15 @@ def compare_segmentations(*segmentations, **kwargs) -> np.ndarray:
 
     Args:
         segmentations (asterisk argument): Allows to provide an arbitraty
-                number of segmented images to be compared
+                number of segmented numpy arrays of integers to be compared
         optional keyword arguments (kwargs):
             components (list): list of the different components that are
-                found in the segmented images. So far only two are allowed
+                considered in the segmented images. So far only two are allowed
                 to be provided.
+            non_active_component (int): value of nonactive component in the segmented
+                images.
             gray_colors (np.ndarray): array of three different scales of
-                gray, one for each of the different combinations of
+                gray (in RGB format), one for each of the different combinations of
                 components in the segmentations.
             colors (np.ndarray): Array of different colors that should
                 indicate uniqe components in each segmentation.
@@ -24,16 +26,19 @@ def compare_segmentations(*segmentations, **kwargs) -> np.ndarray:
     """
     # Define components
     components: list = kwargs.pop("components", [1, 2])
+    non_active_component: int = kwargs.pop("non_active_component", 0)
 
     # Define number of segmentations
-    N: int = len(segmentations)
+    number_of_segmented_images: int = len(segmentations)
 
     # Define gray colors
     gray_colors: np.ndarray = kwargs.pop(
         "gray_colors",
         np.array([[180, 180, 180], [220, 220, 220], [200, 200, 200]], dtype=np.uint8),
     )
-    gray_base: np.ndarray = np.fix(gray_colors / N).astype(np.uint8)
+    gray_base: np.ndarray = np.fix(gray_colors / number_of_segmented_images).astype(
+        np.uint8
+    )
 
     # Define unique colors
     colors: np.ndarray = kwargs.pop(
@@ -57,8 +62,8 @@ def compare_segmentations(*segmentations, **kwargs) -> np.ndarray:
 
     # Assert that there are a sufficient amount of colors
     # and that all of the segmentations are of equal size
-    assert len(colors[:, 0, 0]) >= N
-    for i in range(N - 1):
+    assert len(colors[:, 0, 0]) >= number_of_segmented_images
+    for i in range(number_of_segmented_images - 1):
         assert segmentations[0].shape == segmentations[i + 1].shape
 
     # Create the return array
@@ -67,52 +72,41 @@ def compare_segmentations(*segmentations, **kwargs) -> np.ndarray:
     )
 
     # Enter gray everywhere there are ovelaps of different segmentations
-    for k in range(N):
-        for i in range(k + 1, N):
+    for k in range(number_of_segmented_images):
+        for i in range(k + 1, number_of_segmented_images):
 
-            # Overlap of first component
-            return_image[
-                np.where(
+            # Overlap of components
+            for c_num, c in enumerate(components):
+                return_image[
                     np.logical_and(
-                        segmentations[k] == components[0],
-                        segmentations[i] == components[0],
+                        segmentations[k] == c,
+                        segmentations[i] == c,
                     )
-                )
-            ] += gray_base[0]
-
-            # Overlap of second component
-            return_image[
-                np.where(
-                    np.logical_and(
-                        segmentations[k] == components[1],
-                        segmentations[i] == components[1],
-                    )
-                )
-            ] += gray_base[1]
+                ] += gray_base[c_num]
 
             # Overlap of different components
             return_image[
-                np.where(
-                    np.logical_or(
-                        np.logical_and(
-                            segmentations[k] == components[0],
-                            segmentations[i] == components[1],
-                        ),
-                        np.logical_and(
-                            segmentations[k] == components[1],
-                            segmentations[i] == components[0],
-                        ),
-                    )
+                np.logical_or(
+                    np.logical_and(
+                        segmentations[k] == components[0],
+                        segmentations[i] == components[1],
+                    ),
+                    np.logical_and(
+                        segmentations[k] == components[1],
+                        segmentations[i] == components[0],
+                    ),
                 )
             ] += gray_base[2]
 
     # Determine locations (and make modifications to return image) of unique components
-    for c in components:
-        for k in range(N):
+    for c_num, c in enumerate(components):
+        for k in range(number_of_segmented_images):
             only_tmp: np.ndarray = segmentations[k] == c
-            for j in filter(lambda j: j != k, range(N)):
-                only_tmp = np.logical_and(only_tmp, segmentations[j] == 0)
+            for j in filter(lambda j: j != k, range(number_of_segmented_images)):
+                only_tmp = np.logical_and(
+                    only_tmp, segmentations[j] == non_active_component
+                )
 
-            return_image[np.where(only_tmp)] = colors[k, components.index(c)]
+            return_image[only_tmp] = colors[k, c_num]
 
     return return_image
