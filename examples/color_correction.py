581c8c3c7bd2012f12a916108b0a7b7c0f56a440
Jakub Both <jakub.both@uib.no>
2022-10-15 23:45:19 +0200

    EX: Update color correction example and include the new manual color correction.

--- examples/color_correction.py
+++ examples/color_correction.py
@@ -1,35 +1,74 @@
 import os
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 import daria
 
-# -------- Convert the image into linear RGB color space
-
-# Need to specify a ROI which contains the color checker
-roi_cc = (slice(0, 240), slice(0, 240))
+# ! ---- Read uncorrected image
 
 # Define path to image folder
-image_folder = f"{os.path.dirname(__file__)}/images/"
+image = f"{os.path.dirname(__file__)}/images/baseline.jpg"
+
+# Create an uncorrected image for comparison
+uncorrected_baseline = daria.Image(image, width=2.8, height=1.5)
+
+# ! ---- Setup the machine learning based color correction
+
+# Need to specify a ROI which contains the color checker
+ml_roi_cc = (slice(0, 240), slice(0, 240))
+ml_based_color_correction = daria.ColorCorrection(
+    roi=ml_roi_cc,
+    # verbosity = True,
+)
+
+# NOTE: Setting the flag verbosity to True allows to debug some
+# of the details/progress of the color checker.
 
 # Create the color correction and apply it at initialization of image class
-color_correction = daria.ColorCorrection(roi=roi_cc)
-baseline_corrected = daria.Image(
-    image_folder + "baseline.jpg",
-    color_correction=color_correction,
+ml_corrected_baseline = daria.Image(
+    image,
+    color_correction=ml_based_color_correction,
     width=2.8,
     height=1.5,
 )
 
-# Create an uncorrected image for comparison
-baseline_uncorrected = daria.Image(image_folder + "baseline.jpg", width=2.8, height=1.5)
+# ! ---- Setup the manual color correction
+
+# Need to specify a ROI which contains the color checker
+manual_roi_cc = np.array(
+    [
+        [154, 176],
+        [222, 176],
+        [222, 68],
+        [154, 68],
+    ]
+)
+manual_color_correction = daria.ManualColorCorrection(
+    roi=manual_roi_cc,
+    # verbosity = True,
+)
+
+# NOTE: Setting the flag verbosity to True allows to debug some
+# of the details/progress of the color checker. In particular,
+# the final color checker after calibration is displayed.
+
+# Create the color correction and apply it at initialization of image class
+manual_corrected_baseline = daria.Image(
+    image,
+    color_correction=manual_color_correction,
+    width=2.8,
+    height=1.5,
+)
 
 # -------- Plot corrected and uncorrected images
 
-fig, ax = plt.subplots(1, num=1)
-ax.imshow(baseline_corrected.img)
-fig, ax = plt.subplots(1, num=2)
-ax.imshow(baseline_uncorrected.img)
+plt.figure()
+plt.imshow(uncorrected_baseline.img)
+plt.figure()
+plt.imshow(ml_corrected_baseline.img)
+plt.figure()
+plt.imshow(manual_corrected_baseline.img)
 plt.show(block=False)
 # Pause longer if it is desired to keep the images on the screen
 plt.pause(10)
