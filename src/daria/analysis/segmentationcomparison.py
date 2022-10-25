4aa4b2117ae1dfcf03702a16024b9ece9e968e08
EStorvik <erlend.storvik@uib.no>
2022-10-25 14:47:09 +0200

    work on plotting

--- src/daria/analysis/segmentationcomparison.py
+++ src/daria/analysis/segmentationcomparison.py
@@ -4,6 +4,7 @@ from os import remove
 import numpy as np
 from matplotlib.cm import get_cmap
 
+import daria as da
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import cv2
@@ -20,9 +21,13 @@ class CompareSegmentations:
         Args:
             number_of_segmented_images (int): Number of segmentations to be compared
             Optional keyword arguments (kwargs):
+                segmentation_names (list): list of names for the different segmented 
+                    images. So far only used in legends and color dictionary.
                 components (list): list of the different components that are
                     considered in the segmented images. So far only two are allowed
                     to be provided.
+                component_names (list):  list of names for the different components.
+                    So far only used in legends, and color dictionary.
                 non_active_component (int): value of nonactive component in the segmented
                     images.
                 gray_colors (np.ndarray): array of three different scales of
@@ -35,10 +40,13 @@ class CompareSegmentations:
         """
 
         self.number_of_segmented_images = number_of_segmented_images
+        self.segmentation_names: list = kwargs.pop("segmentation_names", [f"Segmentation {i}" for i in range(self.number_of_segmented_images)])
 
         # Define components
         self.components: list = kwargs.pop("components", [1, 2])
         self.non_active_component: int = kwargs.pop("non_active_component", 0)
+        self.component_names: list = kwargs.pop("component_names", ["Component 0", "Component 1"])
+
 
         # Define gray colors
         self.gray_colors: np.ndarray = kwargs.pop(
@@ -76,13 +84,13 @@ class CompareSegmentations:
 
         self.color_dictionary: dict = {}
         for i in range(self.number_of_segmented_images):
-            self.color_dictionary[f"Unique Seg{i} Comp0"] = self.colors[i,0]
-            self.color_dictionary[f"Unique Seg{i} Comp1"] = self.colors[i,1]
+            self.color_dictionary[f"Unique apperance of {self.component_names[0]} in {self.segmentation_names[i]}"] = self.colors[i,0]
+            self.color_dictionary[f"Unique apperance of {self.component_names[1]} in {self.segmentation_names[i]}"] = self.colors[i,1]
 
         for i in range(self.number_of_segmented_images-1):    
-            self.color_dictionary[f"{i+1} overlapping of comp0"] = self.gray_base[0]*(i+1)
-            self.color_dictionary[f"{i+1} overlapping of comp1"] = self.gray_base[1]*(i+1)
-            self.color_dictionary[f"{i+1} overlapping of mix comp0 and comp1"] = self.gray_base[2]*(i+1)
+            self.color_dictionary[f"{i+2} segmentations overlaps in {self.component_names[0]}"] = self.gray_base[0]*(i+1)
+            self.color_dictionary[f"{i+2} segmentations overlaps in {self.component_names[1]}"] = self.gray_base[1]*(i+1)
+            self.color_dictionary[f"Mixture {self.component_names[0]} and {self.component_names[1]} for {i+2} segmentations"] = self.gray_base[2]*(i+1)
             
         
 
@@ -155,18 +163,21 @@ class CompareSegmentations:
                 return_image[only_tmp] = self.colors[k, c_num]
 
         if plot_result:
-            self._plot(return_image, "Comparison")
+            self.plot(return_image, "Comparison")
 
         return return_image
 
-    def _plot(self, im, fig_name):
+    def plot(self, im, fig_name = "Comparison", legend_anchor = (0.7, 1)):
 
         plt.figure(fig_name)
         plt.imshow(im)
 
 
-        # get the colors of the values, according to the 
-        # colormap used by imshow
+        patches = self._get_legend_patches(im)
+        plt.legend(handles=patches, bbox_to_anchor=legend_anchor, loc=2, borderaxespad=0. )
+        plt.show()
+
+    def _get_legend_patches(self, im):
         colors = self._get_unique_colors(im)
 
         # function to return key for any value
@@ -179,9 +190,7 @@ class CompareSegmentations:
 
         # create a patch (proxy artist) for every color 
         patches = [ mpatches.Patch(color=c/255, label=get_key(c,self.color_dictionary)) for c in colors ]
-        # put those patched as legend-handles into the legend
-        plt.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0. )
-        plt.show()
+        return patches
 
     def _get_unique_colors(self, im) -> np.ndarray:
         nx, ny, _ = im.shape
@@ -189,9 +198,43 @@ class CompareSegmentations:
         unique =  np.unique(flat_im, axis = 0)
         return unique[1:]
 
-    def _remove_background(self, im) -> np.ndarray:
+    def _remove_background(self, im, alpha_colors = 1.) -> np.ndarray:
         tmp = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
         _,alpha = cv2.threshold(tmp,0,255,cv2.THRESH_BINARY)
+        alpha = alpha_colors*alpha
+        print(alpha.dtype)
+        alpha[self._get_transitions(im)] = 1
+        print(alpha.dtype)
         b, g, r = cv2.split(im)
+        print(b.dtype)
         rgba = [b,g,r, alpha]
         return cv2.merge(rgba,4)
+
+    def _get_transitions(self, im) -> np.ndarray:
+        im = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+        diff_im = da.forward_diff_x(im) + da.forward_diff_y(im)
+        return diff_im != 0
+
+    def plot_overlay_segmentation(self, comparison_image: np.ndarray, base_image: np.ndarray, figure_name: str = "Comparison", alpha_colors: float = 0.6, legend_anchor: tuple[float] = (1.,1.)) -> None:
+        """
+        Plots a comparison image overlayed a base image using matplotlib.
+
+        Args:
+            comparison_image (np.ndarray): The image containing comparison of segmentations.
+            base_image (np.ndarray): The base image that is to be overlayed.
+            figure_name (str): Figure name.
+            comparison_alpha (float): Tha alpha value to determine transparency of the 
+        """
+        no_background_comparison_image = self._remove_background(comparison_image, alpha_colors)
+        # b, g, r = cv2.split(base_image)
+        # rgba = [b, g, r , np.ones_like(b)]
+        # base_image_alpha = cv2.merge(rgba, 4)
+        plt.figure(figure_name)
+        plt.imshow(base_image)
+        plt.imshow(no_background_comparison_image)
+        patches = self._get_legend_patches(comparison_image)
+        plt.legend(handles=patches, bbox_to_anchor=legend_anchor, loc=2, borderaxespad=0. )
+        plt.show()
+
+    def _white_background(self, im) -> np.ndarray:
+        pass
