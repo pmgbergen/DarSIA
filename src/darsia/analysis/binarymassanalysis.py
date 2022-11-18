8e87e6de2db1b85c05c8842c926107561003a10e
EStorvik <erlend.storvik@uib.no>
2022-11-18 16:34:54 +0100

    STY: black, isort, flake8

--- src/darsia/analysis/binarymassanalysis.py
+++ src/darsia/analysis/binarymassanalysis.py
@@ -4,28 +4,30 @@ A class with utilities for computing mass based on segmentation.
 
 from __future__ import annotations
 
-import darsia as da
-import numpy as np
-from typing import Optional, Union
-from scipy.interpolate import RBFInterpolator
 from pathlib import Path
-
-
+from typing import Optional, Union
 from warnings import warn
+
+import numpy as np
+from scipy.interpolate import RBFInterpolator
+
+import darsia as da
 
 
 class BinaryMassAnalysis:
     """
     Class for for analysing mass of components in 2D segmented images
-    
+
     Attributes:
-        base_image (darsia.Image): A base darsia image. In principle only needed for size properties,
-            like numer of pixels, height and width.
+        base_image (darsia.Image): A base darsia image. In principle only needed for size
+            properties, like numer of pixels, height and width.
         pixelarea (float): product of dx and dy from base_image.
-        porosity (np.ndarray): porosity map of equal size to base_image. Default is array of ones.
-        depth_map (np.ndarray): depth map contain information about depth associated to each pixel.
-            Default is an array of ones.
-        height_map (np.ndarray): Associates each pixel with its distance from the top of the image.
+        porosity (np.ndarray): porosity map of equal size to base_image. Default is array
+            of ones.
+        depth_map (np.ndarray): depth map contain information about depth associated to
+            each pixel. Default is an array of ones.
+        height_map (np.ndarray): Associates each pixel with its distance from the top of
+            the image.
     """
 
     def __init__(
@@ -39,15 +41,17 @@ class BinaryMassAnalysis:
         Initializer for mass analysis class.
 
         Arguments:
-            base_image (darsia.Image): In principle only needed for size properties, 
+            base_image (darsia.Image): In principle only needed for size properties,
                 like numer of pixels, height and width.
-                depth_map (Optional[np.ndarray]): depth map with depth associated to each pixel.
-                depth_measurements (Optional[tuple[np.ndarray, ...]]): tuple of depth measurements. 
-                    Should be provided as horizontal coordinates and vertical coordinates as a 
-                    meshgrid, and depth cooridinates corresponding to each horizontal and vertical entry.
-                porosity (Optional[Union[np.ndarray, float]]): porosity map. Can be provided as a uniform 
-                    scalar value as well.
-                
+                depth_map (Optional[np.ndarray]): depth map with depth associated to
+                    each pixel.
+                depth_measurements (Optional[tuple[np.ndarray, ...]]): tuple of depth
+                    measurements. Should be provided as horizontal coordinates and
+                    vertical coordinates as a meshgrid, and depth cooridinates
+                    corresponding to each horizontal and vertical entry.
+                porosity (Optional[Union[np.ndarray, float]]): porosity map. Can be
+                    provided as a uniform scalar value as well.
+
         """
         self.base_image = base_image
         self.pixelarea: float = self.base_image.dx * self.base_image.dy
@@ -61,7 +65,7 @@ class BinaryMassAnalysis:
         else:
             self.porosity = np.ones(self.base_image.img.shape[:2])
             warn("Please provide a porosity. Now it is assumed to be 1.")
-        
+
         # Define depth map
         if depth_measurements is not None:
             self.depth_map = self._compute_depth_map(depth_measurements)
@@ -70,16 +74,17 @@ class BinaryMassAnalysis:
         else:
             warn(
                 "Please provide a depth_map (of at least the same size as "
-                " the segmented images that are to be analyzed) or a tuple of depth measurements."
+                " the segmented images that are to be analyzed) or a tuple of "
+                " depth measurements."
                 " Now, a constant depth of 1 is assumed."
             )
             self.depth_map = np.ones(self.base_image.img.shape[:2])
-        
-        # Defines a height map associating each pixel with its physical height (distance from top)
+
+        # Defines a height map associating each pixel with its physical
+        # height (distance from top)
         self.height_map = np.linspace(
             0, self.base_image.height, self.base_image.img.shape[0]
         )[:, None] * np.ones_like(self.base_image.img)
-
 
     def volume_map(self, segmentation: np.ndarray, component: int) -> np.ndarray:
         """
@@ -89,10 +94,11 @@ class BinaryMassAnalysis:
 
         Arguments:
             segmentation (np.ndarray): segmented image
-            component (int): the value that the component of interest takes in the segmentation.
-        
+            component (int): the value that the component of interest takes in the
+                segmentation.
+
         Returns:
-            volume map (np.ndarray): volume associated to each pixel containing the prescribed 
+            volume map (np.ndarray): volume associated to each pixel containing the prescribed
                 component.
         """
 
@@ -105,12 +111,13 @@ class BinaryMassAnalysis:
                 raise Exception(
                     f"The shape of the depthmap is {self.depth_map.shape} and the shape of the"
                     f" segmentations is {segmentation.shape}."
-                    " Please make sure that the depthmap is at least as large as the segmentation."
-                    " For optimal accuracy they should be equal in size."
+                    " Please make sure that the depthmap is at least as large as the"
+                    " segmentation. For optimal accuracy they should be equal in size."
                 )
             warn(
                 f"The shape of the depthmap is {self.depth_map.shape} and the shape of the"
-                f" segmentations is {segmentation.shape}. These should be the same for optimal accuracy."
+                f" segmentations is {segmentation.shape}. These should be the same for optimal"
+                " accuracy."
             )
 
             # Compute volume map
@@ -121,7 +128,7 @@ class BinaryMassAnalysis:
         else:
             # Compute volume map
             volume_map = (self.porosity * self.depth_map) * self.pixelarea
-        
+
         # Extract only the correct volumes (depending on segmentation and chosen component)
         volume = np.zeros_like(volume_map)
         volume[segmentation == component] = volume_map[segmentation == component]
@@ -137,20 +144,20 @@ class BinaryMassAnalysis:
 
         Arguments:
             external_pressure (float): external pressure, for example atmospheric pressure.
-        
-        Returns: 
+
+        Returns:
             (np.ndarray): array associating each pixel with the CO2 density.
 
         TODO: Discuss whether this function should be provided here
         """
         return 1000 * (
             1.805726990977443 * (external_pressure + 0.101325 * self.height_map)
-            -0.009218969932330845
+            - 0.009218969932330845
         )
 
     def free_co2_mass(
         self, segmentation: np.ndarray, external_pressure: float, co2_component: int = 2
-        ) -> float:
+    ) -> float:
         """
         Given a segmentation, external pressure and the number
         associated to free CO2 (default is 2), it returns the
@@ -159,7 +166,8 @@ class BinaryMassAnalysis:
         Arguments:
             segmentation (np.ndarray): segmented image
             external_pressure (float): external pressure, for example atmospheric pressure.
-            co2_component (int): value of the component in segmentation that corresponds to co2.
+            co2_component (int): value of the component in segmentation that
+            corresponds to co2.
 
         Returns:
             (float): Mass of co2.
@@ -167,16 +175,18 @@ class BinaryMassAnalysis:
         volume = self.volume_map(segmentation, co2_component)
         density_map = self.external_pressure_to_density_co2(external_pressure)
         return np.sum(volume * density_map)
-    
 
-    def _compute_depth_map(self, depth_measurements: tuple[np.ndarray, ...]) -> np.ndarray:
+    def _compute_depth_map(
+        self, depth_measurements: tuple[np.ndarray, ...]
+    ) -> np.ndarray:
         """
         Compute depth map, based on the reported measurements.
 
         Arguments:
-             depth_measurements (Optional[tuple[np.ndarray, ...]]): tuple of depth measurements. 
-                    Should be provided as horizontal coordinates, vertical coordinates, and depth
-                    cooridinates corresponding to each horizontal and vertical entry.
+             depth_measurements (Optional[tuple[np.ndarray, ...]]): tuple of depth
+                    measurements. Should be provided as horizontal coordinates,
+                    vertical coordinates, and depth cooridinates corresponding
+                    to each horizontal and vertical entry.
         Returns:
             (np.ndarray): depth map
         """
@@ -216,7 +226,7 @@ class BinaryMassAnalysis:
 
         Arguments:
             path (Union[str, Path]): location where depthmap will be saved.
-        
+
         TODO: Might be an unnecessary function.
         """
         np.save(path, self.depth_map)
