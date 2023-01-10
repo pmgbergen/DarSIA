96925e3f3d1eb588781f756f64bea8efe27b2487
Jakub Both <jakub.both@uib.no>
2023-01-10 13:35:28 +0100

    Update piecewise warp for incompatible image sizes.

--- src/darsia/corrections/shape/piecewiseperspective.py
+++ src/darsia/corrections/shape/piecewiseperspective.py
@@ -9,6 +9,7 @@ import cv2
 import numpy as np
 
 import darsia
+from math import ceil
 
 
 class PiecewisePerspectiveTransform:
@@ -50,6 +51,11 @@ class PiecewisePerspectiveTransform:
         (h, w) = patches.base.img.shape[:2]
         dtype = patches.base.img.dtype
         transformed_img = np.zeros(patches.base.img.shape, dtype=np.float32)
+        transformed_img_new = np.zeros(patches.base.img.shape, dtype=np.float32)
+
+        import time
+        total = 0
+        total_new = 0
 
         # Loop over all patches in a grid fashion:
         for i in range(patches.num_patches_x):
@@ -59,7 +65,7 @@ class PiecewisePerspectiveTransform:
                 global_corners = patches.global_corners_reverse_matrix[i, j]
                 local_corners = patches.local_corners_reverse_matrix[i, j]
 
-                # Determine the coordinates after applying the trasnformation
+                # Determine the coordinates after applying the transformation
                 # NOTE: Strangely, the interpolator reshapes the arrays from
                 # input to output.
                 pts_src = local_corners
@@ -68,19 +74,62 @@ class PiecewisePerspectiveTransform:
                     + (-1.0 if reverse else 1.0) * displacement(global_corners).T
                 )
 
+                # TODO rm and rename the eff variables?
+
+                # # Find perspective transform mapping src to dst pixels in reverse matrix format
+                # P = cv2.getPerspectiveTransform(
+                #     pts_src.astype(np.float32), pts_dst.astype(np.float32)
+                # )
+
+                # # Map patch onto transformed region
+                # patch_img = patches.images[i][j].img.astype(np.float32)
+                # tic = time.time()
+                # print(patch_img.shape, w, h)
+                # transformed_img += cv2.warpPerspective(
+                #     patch_img, P, (w, h), flags=cv2.INTER_LINEAR
+                # )
+                # total += time.time() - tic
+
+                # Find effective origin, width and height of the transformed patch
+                origin_eff = np.array([
+                    max(0, np.min(pts_dst[:,0])),
+                    max(0, np.min(pts_dst[:,1]))
+                ]).astype(np.int32)
+                pts_dst_eff = pts_dst - origin_eff
+                w_eff = ceil(min(w, np.max(pts_dst[:,0])) - max(0, origin_eff[0]))
+                h_eff = ceil(min(h, np.max(pts_dst[:,1])) - max(0, origin_eff[1]))
+
+                # Continue with the next patch, if the effective size becomes at most a single pixel.
+                if min(h_eff, w_eff) <= 1:
+                    continue
+
+                # NOTE: Flip of x and y to row and col.
+                roi_eff = (
+                    slice(max(0, int(origin_eff[1])), min(h, int(origin_eff[1] + h_eff))),
+                    slice(max(0, int(origin_eff[0])), min(w, int(origin_eff[0] + w_eff))))
+                roi_patch_eff = (
+                    slice(0, int(min(origin_eff[1] + h_eff, h) - origin_eff[1])),
+                    slice(0, min(origin_eff[0] + w_eff, w) - origin_eff[0])
+                )
+
                 # Find perspective transform mapping src to dst pixels in reverse matrix format
-                P = cv2.getPerspectiveTransform(
-                    pts_src.astype(np.float32), pts_dst.astype(np.float32)
+                P_eff = cv2.getPerspectiveTransform(
+                    pts_src.astype(np.float32), pts_dst_eff.astype(np.float32)
                 )
 
                 # Map patch onto transformed region
                 patch_img = patches.images[i][j].img.astype(np.float32)
-                transformed_img += cv2.warpPerspective(
-                    patch_img, P, (w, h), flags=cv2.INTER_LINEAR
+                tic = time.time()
+                tmp = cv2.warpPerspective(
+                    patch_img, P_eff, (w_eff, h_eff), flags=cv2.INTER_LINEAR
                 )
+                transformed_img_new[roi_eff] += tmp[roi_patch_eff]
+                total_new += time.time() - tic
 
         # Convert to the same data type as the input image
-        transformed_img = transformed_img.astype(dtype)
+        transformed_img = transformed_img_new.astype(dtype)
+        #print(f"total time: {total}")
+        print(f"total time new: {total_new}")
 
         # Use same metadata as for the base of the patches
         metadata = patches.base.metadata
