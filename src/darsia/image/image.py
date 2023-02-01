1eab6e3a5932dbd39c22da106af3e3895453feb2
EStorvik <erlend.storvik@hvl.no>
2023-02-01 15:15:37 +0100

    modification for readability

--- src/darsia/image/image.py
+++ src/darsia/image/image.py
@@ -240,8 +240,11 @@ class Image:
         """
         if self.img.shape != other.img.shape:
             warn("Images have different shapes. Resizing second argument to match.")
-            other_img = cv2.resize(other.img, self.img.shape[::-1][1:3])
-            return da.Image(self.img - other_img, copy.copy(self.metadata))
+            return da.Image(
+                self.img
+                - cv2.resize(np.copy(other.img), tuple(reversed(self.img.shape[:2]))),
+                copy.copy(self.metadata),
+            )
         else:
             return da.Image(self.img - other.img, copy.copy(self.metadata))
 
