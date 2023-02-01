e0081d7b6da5ee29c2d1b6c4f1b017640ae86c39
EStorvik <erlend.storvik@hvl.no>
2023-02-01 15:42:04 +0100

    Overhaul of derivatives to allow for 3D arrays as well

--- src/darsia/mathematics/derivatives.py
+++ src/darsia/mathematics/derivatives.py
@@ -1,24 +1,25 @@
 """
-Contains several functions for taking derivatives of images in single-color space
+Contains several functions for taking derivatives of images in single-color space. All functions are based on numpy.diff(), and are very hardcoded with respect to dimension and axes.
 """
 from __future__ import annotations
 
+from typing import Optional
 import numpy as np
 
 
-def backward_diff(im: np.ndarray, axis: int, dimension: int = 2) -> np.ndarray:
+def backward_diff(im: np.ndarray, axis: int) -> np.ndarray:
     """
     Backward difference of image matrix in direction of axis.
 
     Arguments:
-        im (np.ndarray): image array
+        im (np.ndarray): image array in single-color space
         axis (int): axis along which the difference is taken
-        dimension (int): dimension of the image array (default: 2), 
-                         only 2,3 allowed
+
 
     Returns:
         np.ndarray: backward difference image matrix
     """
+    dimension = im.ndim
     assert axis < dimension, "axis must be smaller than dimension"
 
     if dimension == 2:
@@ -39,18 +40,18 @@ def backward_diff(im: np.ndarray, axis: int, dimension: int = 2) -> np.ndarray:
         raise NotImplementedError("Only 2 and 3 dimensional images are supported")
 
 
-def forward_diff(im: np.ndarray, axis: int, dimension: int = 2) -> np.ndarray:
+def forward_diff(im: np.ndarray, axis: int) -> np.ndarray:
     """
     Forward difference of image matrix in direction of axis.
 
     Arguments:
         im (np.ndarray): image array
         axis (int): axis along which the difference is taken
-        dimension (int): dimension of the image array (default: 2),
-                            only 2,3 allowed
+
     Returns:
         np.ndarray: forward difference image matrix
     """
+    dimension = im.ndim
     assert axis < dimension, "axis must be smaller than dimension"
 
     if dimension == 2:
@@ -67,65 +68,32 @@ def forward_diff(im: np.ndarray, axis: int, dimension: int = 2) -> np.ndarray:
         elif axis == 2:
             return np.diff(im, axis=2, prepend=im[:, :, :1])
 
+    else:
+        raise NotImplementedError("Only 2 and 3 dimensional images are supported")
 
 
 
-
-def laplace_x(im: np.ndarray) -> np.ndarray:
+def laplace(im: np.ndarray, axis: Optional[int] = None) -> np.ndarray:
     """
     Laplace operator with homogeneous boundary conditions of image matrix
-    in horizontal direction.
+    in direction of axis.
 
     Arguments:
         im (np.ndarray): image array
+        axis (int): axis along which the difference is taken
+
 
     Returns:
         np.ndarray: horizontal Laplace image matrix
     """
-    return 0.5 * (
-        forward_diff_x(backward_diff_x(im)) + backward_diff_x(forward_diff_x(im))
-    )
+    dimension = im.ndim
+    if axis is None:
+        laplace = 0
+        for ax in range(dimension):
+            laplace += 0.5*(backward_diff(forward_diff(im, ax, dimension),ax, dimension) + forward_diff(backward_diff(im, ax, dimension),ax, dimension))
 
+    else:
+        assert axis < dimension, "axis must be smaller than dimension"
+        laplace = 0.5*(backward_diff(forward_diff(im, axis, dimension), axis, dimension) + forward_diff(backward_diff(im, axis, dimension), axis, dimension))
 
-def laplace_y(im: np.ndarray) -> np.ndarray:
-    """
-    Laplace operator with homogeneous boundary conditions of image matrix
-    in vertical direction.
-
-    Arguments:
-        im (np.ndarray): image array
-
-    Returns:
-        np.ndarray: vertical Laplace image matrix
-    """
-    return 0.5 * (
-        forward_diff_y(backward_diff_y(im)) + backward_diff_y(forward_diff_y(im))
-    )
-
-
-def laplace(im: np.ndarray) -> np.ndarray:
-    """
-    Laplace operator with homogeneous boundary conditions of image matrix.
-
-    Arguments:
-        im (np.ndarray): image array
-
-    Returns:
-        np.ndarray: Laplace image matrix
-    """
-    return laplace_x(im) + laplace_y(im)
-
-
-
-def partial_difference_forward(im: np.ndarray, axis: int) -> np.ndarray:
-    """
-    Partial difference of image matrix in given direction.
-
-    Arguments:
-        im (np.ndarray): image array
-        axis (int): direction of partial difference
-
-    Returns:
-        np.ndarray: partial difference image matrix
-    """
-    return np.diff(im, axis=axis, prepend=im[:1, :])
+    return laplace
