fbf5b95050019d3abe64b9dac45edd352b4ed97f
Jakub Both <jakub.both@uib.no>
2023-02-01 23:06:52 +0100

    DOC: Add documentation and comments.

--- src/darsia/transformations/signals/binarydataselector.py
+++ src/darsia/transformations/signals/binarydataselector.py
@@ -9,17 +9,32 @@ import darsia
 import skimage
 import cv2
 
+
 class BaseCriterion:
     """Abstract criterion class."""
 
     def bind(self, signal: np.ndarray, unprocessed_signal: np.ndarray) -> None:
+        """
+        Binding routine, allowing to fix and prepare.
+
+        Args:
+            signal (np.ndarray): processed signal
+            unprocessed_signal (np.ndarray): unprocessed signal
+        """
         self.signal = signal
 
     @abc.abstractmethod
     def __call__(self, roi) -> np.ndarray:
+        """
+        Main method, to be overwritten.
+        """
         pass
 
+
 class ValueCriterion(BaseCriterion):
+    """
+    Criterion checking for absolute maximal values.
+    """
 
     def __init__(self, threshold: float) -> None:
         self.threshold = threshold
@@ -27,15 +42,24 @@ class ValueCriterion(BaseCriterion):
     def __call__(self, roi) -> np.ndarray:
         return np.max(self.signal[roi]) > self.threshold
 
+
 class RelativeValueCriterion(BaseCriterion):
+    """
+    Criterion checking for relative maximal values.
+    """
 
     def __init__(self, threshold: float) -> None:
         self.threshold = threshold
-    
+
     def __call__(self, roi) -> np.ndarray:
         return np.max(self.signal[roi]) > self.threshold * np.min(self.signal[roi])
 
+
 class TransformedValueCriterion(BaseCriterion):
+    """
+    Criterion checking for absolute maximal values in a transformed
+    version of the unprocessed signal.
+    """
 
     def __init__(self, transformation: callable, threshold: float) -> None:
         self.transformation = transformation
@@ -47,21 +71,32 @@ class TransformedValueCriterion(BaseCriterion):
     def __call__(self, roi) -> np.ndarray:
         return np.max(self.processed_signal[roi]) > self.threshold
 
-class GradientCriterion(BaseCriterion):
 
-    def __init__(self, threshold: Optional[float] = None, prefix: str = "", **kwargs) -> None:
+class GradientCriterion(BaseCriterion):
+    """
+    Criterion checking for absolute maximal value of the
+    gradient modulus of the signal.
+    """
+
+    def __init__(
+        self, threshold: Optional[float] = None, prefix: str = "", **kwargs
+    ) -> None:
         self.threshold = threshold
 
     def bind(self, signal: np.ndarray, unprocessed_signal: np.ndarray) -> None:
         dx = darsia.forward_diff_x(signal)
         dy = darsia.forward_diff_y(signal)
         self.gradient_modulus = np.sqrt(dx**2 + dy**2)
-        
+
     def __call__(self, roi) -> np.ndarray:
         return np.max(self.gradient_modulus[roi]) > self.threshold
 
+
 class CombinedCriterion(BaseCriterion):
-    
+    """
+    General combination of criteria, provided as list.
+    """
+
     def __init__(self, criteria: list[BaseCriterion]) -> None:
         self.criteria = criteria
 
@@ -72,9 +107,24 @@ class CombinedCriterion(BaseCriterion):
     def __call__(self, signal: np.ndarray) -> np.ndarray:
         return all([criterion(signal) for criterion in self.criteria])
 
-class BinaryDataSelector:
 
-    def __init__(self, criterion: Optional[BaseCriterion] = None, prefix: str = "", **kwargs) -> None:
+class BinaryDataSelector:
+    """
+    Data selector acting on criteria, both volume and contour based.
+    """
+
+    def __init__(
+        self, criterion: Optional[BaseCriterion] = None, prefix: str = "", **kwargs
+    ) -> None:
+        """
+        Constructor, initializing criteria. Possibly from keyword arguments.
+
+        Provide the following possibilities:
+            * absolute value based
+            * relative value based
+            * absolute value for signal and transformed unprocessed signal
+            * gradient modulus based
+        """
         if criterion is not None:
             self.criterion = criterion
         else:
@@ -84,13 +134,17 @@ class BinaryDataSelector:
             if criterion_key == "value":
                 self.criterion = ValueCriterion(threshold)
                 self.type = "volume"
-            
+
+            elif criterion_key == "relative value":
+                self.criterion = RelativeValueCriterion(threshold)
+                self.type = "volume"
+
             elif criterion_key == "value/value extra color":
                 # Value
                 value_criterion = ValueCriterion(threshold[0])
 
                 # Value of extra color
-                color: Union[str, Callable] = kwargs.pop(prefix+"extra color")
+                color: Union[str, Callable] = kwargs.pop(prefix + "extra color")
                 transformation = darsia.MonochromaticReduction(color=color)
                 extra_color_criterion = TransformedValueCriterion(
                     transformation,
@@ -98,13 +152,11 @@ class BinaryDataSelector:
                 )
 
                 # Combine both
-                self.criterion = CombinedCriterion([value_criterion, extra_color_criterion])
+                self.criterion = CombinedCriterion(
+                    [value_criterion, extra_color_criterion]
+                )
                 self.type = "volume"
 
-            elif criterion_key == "relative value":
-                self.criterion = RelativeValueCriterion(threshold)
-                self.type = "volume"
-               
             elif criterion_key == "gradient modulus":
                 self.criterion = GradientModulusCriterion(threshold)
                 self.type = "contour"
@@ -112,7 +164,9 @@ class BinaryDataSelector:
             else:
                 raise ValueError(f"Criterion type {criterion_key} not supported.")
 
-    def __call__(self, signal: np.ndarray, mask: np.ndarray, unprocessed_signal: np.ndarray) -> np.ndarray:
+    def __call__(
+        self, signal: np.ndarray, mask: np.ndarray, unprocessed_signal: np.ndarray
+    ) -> np.ndarray:
         """
         Select from data labeled regions which satisfy some criterion.
 
@@ -129,11 +183,9 @@ class BinaryDataSelector:
 
         # Initialize result
         cleaned_mask = np.zeros_like(mask, dtype=bool)
-        
+
         # Label the mask
-        labels, num_labels = skimage.measure.label(
-            mask, return_num=True
-        )
+        labels, num_labels = skimage.measure.label(mask, return_num=True)
 
         # Investigate each labeled region separately; omit label 0, which corresponds
         # to non-marked area.
