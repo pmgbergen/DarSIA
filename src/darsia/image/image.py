b0b3d1a0cd3e8e8f9fbe3d1d405949c6233a456d
EStorvik <erlend.storvik@hvl.no>
2023-02-02 11:36:44 +0100

    removed redundant copy

--- src/darsia/image/image.py
+++ src/darsia/image/image.py
@@ -242,7 +242,7 @@ class Image:
             warn("Images have different shapes. Resizing second argument to match.")
             return da.Image(
                 self.img
-                - cv2.resize(np.copy(other.img), tuple(reversed(self.img.shape[:2]))),
+                - cv2.resize(other.img, tuple(reversed(self.img.shape[:2]))),
                 copy.copy(self.metadata),
             )
         else:
