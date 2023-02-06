74e400afe47ee91bda7e997d33dc20b19b2552a9
Jakub Both <jakub.both@uib.no>
2023-02-06 11:56:43 +0100

    MAINT: Restructure init to circumvent circular import

--- src/darsia/__init__.py
+++ src/darsia/__init__.py
@@ -16,6 +16,7 @@ from darsia.mathematics.stoppingcriterion import *
 from darsia.mathematics.solvers import *
 from darsia.mathematics.regularization import *
 from darsia.measure.norms import *
+from darsia.measure.integration import *
 from darsia.utils.conversions import *
 from darsia.utils.resolution import *
 from darsia.utils.box import *
@@ -28,15 +29,6 @@ from darsia.corrections.shape.curvature import *
 from darsia.corrections.shape.drift import *
 from darsia.corrections.color.colorcorrection import *
 from darsia.corrections.color.experimentalcolorcorrection import *
-from darsia.analysis.translationanalysis import *
-from darsia.analysis.concentrationanalysis import *
-from darsia.analysis.compactionanalysis import *
-from darsia.analysis.segmentationcomparison import *
-from darsia.analysis.contouranalysis import *
-from darsia.manager.analysisbase import *
-from darsia.manager.concentrationanalysisbase import *
-from darsia.manager.traceranalysis import *
-from darsia.manager.co2analysis import *
 from darsia.transformations.signals.basemodel import *
 from darsia.transformations.signals.linearmodel import *
 from darsia.transformations.signals.thresholdmodel import *
@@ -46,3 +38,12 @@ from darsia.transformations.signals.binarydataselector import *
 from darsia.transformations.colors.monochromatic import *
 from darsia.regularization.tvd import *
 from darsia.regularization.binaryinpaint import *
+from darsia.analysis.translationanalysis import *
+from darsia.analysis.concentrationanalysis import *
+from darsia.analysis.compactionanalysis import *
+from darsia.analysis.segmentationcomparison import *
+from darsia.analysis.contouranalysis import *
+from darsia.manager.analysisbase import *
+from darsia.manager.concentrationanalysisbase import *
+from darsia.manager.traceranalysis import *
+from darsia.manager.co2analysis import *
