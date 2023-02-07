8e156b88efba487337bd4115ad0aaec8b3ae8605
Jakub Both <jakub.both@uib.no>
2023-02-07 15:02:54 +0100

    MAINT: Inherit monochromatic reduction from general one, fix cv2 issues.

--- src/darsia/transformations/colors/monochromatic.py
+++ src/darsia/transformations/colors/monochromatic.py
@@ -2,10 +2,15 @@
 Module providing dimension reductions to monochromatic/scalar signals.
 """
 
+import cv2
+import matplotlib.pyplot as plt
 import numpy as np
+import skimage
+
+import darsia
 
 
-class MonochromaticReduction:
+class MonochromaticReduction(darsia.SignalReduction):
     def __init__(self, **kwargs) -> None:
         self.color = kwargs.pop("color", "gray")
 
@@ -54,23 +59,32 @@ class MonochromaticReduction:
             return img_v
 
         elif self.color == "gray":
-            # Assume RGB input
-            return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            # Assume RGB input. NOTE: Make sure that the input is in correct
+            # format (CV2 requires np.float32).
+            return cv2.cvtColor(img.astype(np.float32), cv2.COLOR_RGB2GRAY)
+
         elif self.color == "red":
             return img[:, :, 0]
+
         elif self.color == "green":
             return img[:, :, 1]
+
         elif self.color == "blue":
             return img[:, :, 2]
+
         elif self.color == "red+green":
             return img[:, :, 0] + img[:, :, 1]
+
         elif self.color == "negative-key":
             cmy = 1 - img
             key = np.min(cmy, axis=2)
             return 1 - key
+
         elif callable(self.color):
             return self.color(img)
+
         elif self.color == "":
             return img
+
         else:
             raise ValueError(f"Mono-colored space {self.color} not supported.")
