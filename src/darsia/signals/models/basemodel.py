f35b575d702c3596e5585cb51edb208f196518d9
Jakub Both <jakub.both@uib.no>
2023-02-17 09:33:35 +0100

    MAINT: Require each darsia.Model to allow updating parameters.

--- src/darsia/signals/models/basemodel.py
+++ src/darsia/signals/models/basemodel.py
@@ -22,3 +22,10 @@ class Model:
         Calibration routine of model.
         """
         pass
+
+    @abc.abstractmethod
+    def update_model_parameters(self, parameters: np.ndarray, pos_model) -> None:
+        """
+        Update routine of model parameters.
+        """
+        pass
