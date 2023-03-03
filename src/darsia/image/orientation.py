284debd89d0ca07bbb58dae809dd86ec72df2ac9
Jakub Both <jakub.both@uib.no>
2023-03-03 16:24:52 +0100

    ENH: Add utils functionality to convert matrix and Cartesian indexing.

---
+++ src/darsia/image/orientation.py
@@ -0,0 +1,101 @@
+"""
+Module containing auxiliary interpreters
+for orientation of axes used for images.
+
+Conventions. For unified use in the code base,
+we introduce conventions of coordinate systems.
+
+In 2d: The Cartesian coordinate system is
+defined as x-y-coordinate system. E.g.
+for an image with ij-matrix indexing the first
+component corresponds to the y-axis with reversed
+orientation, while the second axis corresponds
+to the standard x-axis.
+
+In 3d: The Cartesian coordinate system is
+defines as xyz coordinate system. E.g.
+for an image with ijk-matrix indexing, the first
+component corresponds to the z axis with
+reversed orientation, the second component corresponds
+to the y-axis, and the third component corresponds to
+the x-axis.
+
+"""
+
+
+def interpret_orientation(axis: str, orientation: str) -> tuple[int, bool]:
+    """Interpretation of axes and their orientation.
+
+    Args:
+        axis (str): target axis, e.g., "x"
+        orientation (str): orientation of an image, e.g., "ijk"
+
+    Returns:
+        int: component corresponding to the axis. Covered: "x", "y", "z",
+            "i", "j", "k".
+        bool: flag controlling whether the axis has to be reverted
+            when converting. Covered: "xyz", "ijk".
+
+    """
+
+    # Consider all possible combinations.
+
+    # ! ---- 2D Cartesian indexing
+
+    if orientation == "xy":
+        if axis == "x":
+            return 0, False
+        elif axis == "y":
+            return 1, False
+        elif axis == "i":
+            return 1, True
+        elif axis == "j":
+            return 0, False
+
+    # ! ---- 2D matrix indexing
+
+    elif orientation == "ij":
+        if axis == "x":
+            return 1, False
+        elif axis == "y":
+            return 0, True
+        elif axis == "i":
+            return 0, False
+        elif axis == "j":
+            return 1, False
+
+    # ! ---- 3D Cartesian indexing
+
+    if orientation == "xyz":
+        if axis == "x":
+            return 0, False
+        elif axis == "y":
+            return 1, False
+        elif axis == "z":
+            return 2, False
+        elif axis == "i":
+            return 2, True
+        elif axis == "j":
+            return 0, False
+        elif axis == "k":
+            return 1, False
+
+    # ! ---- 3D atrix indexing
+
+    elif orientation == "ijk":
+        if axis == "x":
+            return 1, False
+        elif axis == "y":
+            return 2, False
+        elif axis == "z":
+            return 0, True
+        elif axis == "i":
+            return 0, False
+        elif axis == "j":
+            return 1, False
+        elif axis == "k":
+            return 2, False
+
+    # If the method reaches this point, something went wrong.
+    # This fact is used as safety check.
+    raise ValueError
