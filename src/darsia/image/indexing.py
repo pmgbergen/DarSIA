86738c1486684ee6b419dca60b65fc737b35811f
Jakub Both <jakub.both@uib.no>
2023-03-08 13:31:03 +0100

    FIX: conversion for 3d differs from 2d. add this.

--- src/darsia/image/indexing.py
+++ src/darsia/image/indexing.py
@@ -25,31 +25,43 @@ the x-axis.
 from typing import Union
 
 
-def to_matrix_indexing(axis: Union[str, int]) -> str:
+def to_matrix_indexing(axis: Union[str, int], indexing: str) -> str:
     """Conversion of single axis in Cartesian to matrix indexing.
 
     Args:
         axis (str or int): input axis in Cartesian indexing sense.
+        indexing (str): reference Cartesian indexing, also identifying
+            the dimension.
 
     Returns:
         str: converted axis in matrix indexing sense.
 
     """
+    assert indexing in "xy", "xyz"
+
     # Convert numeric axis description
     if isinstance(axis, int):
         axis = "xyz"[axis]
 
-    if axis == "x":
-        return "j"
-    elif axis == "y":
-        return "i"
-    elif axis == "z":
-        return "k"
-    else:
-        raise ValueError
+    if indexing == "xy":
+        if axis == "x":
+            return "j"
+        elif axis == "y":
+            return "i"
+        else:
+            raise ValueError
+    elif indexing == "xyz":
+        if axis == "x":
+            return "k"
+        elif axis == "y":
+            return "j"
+        elif axis == "z":
+            return "i"
+        else:
+            raise ValueError
 
 
-def to_cartesian_indexing(axis: Union[str, int]) -> str:
+def to_cartesian_indexing(axis: Union[str, int], indexing: str) -> str:
     """Conversion of single axis in matrix indexing to Cartesian indexing.
 
     Args:
@@ -63,14 +75,22 @@ def to_cartesian_indexing(axis: Union[str, int]) -> str:
     if isinstance(axis, int):
         axis = "ijk"[axis]
 
-    if axis == "i":
-        return "y"
-    elif axis == "j":
-        return "x"
-    elif axis == "k":
-        return "z"
-    else:
-        raise ValueError
+    if indexing == "ij":
+        if axis == "i":
+            return "y"
+        elif axis == "j":
+            return "x"
+        else:
+            raise ValueError
+    elif indexing == "ijk":
+        if axis == "i":
+            return "z"
+        elif axis == "j":
+            return "y"
+        elif axis == "k":
+            return "x"
+        else:
+            raise ValueError
 
 
 def interpret_indexing(axis: str, indexing: str) -> tuple[int, bool]:
@@ -126,17 +146,17 @@ def interpret_indexing(axis: str, indexing: str) -> tuple[int, bool]:
         elif axis == "i":
             return 2, True
         elif axis == "j":
-            return 0, False
-        elif axis == "k":
             return 1, False
+        elif axis == "k":
+            return 0, True
 
-    # ! ---- 3D atrix indexing
+    # ! ---- 3D matrix indexing
 
     elif indexing == "ijk":
         if axis == "x":
-            return 1, False
+            return 2, True
         elif axis == "y":
-            return 2, False
+            return 1, False
         elif axis == "z":
             return 0, True
         elif axis == "i":
