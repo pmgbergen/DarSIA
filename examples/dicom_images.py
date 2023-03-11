32b8b2a52601545a5073a3530d671c21b470e84f
Jakub Both <jakub.both@uib.no>
2023-03-11 22:38:49 +0100

    EX: Adapt DICOM example according to the newest updates.

--- examples/dicom_images.py
+++ examples/dicom_images.py
@@ -7,9 +7,6 @@ file types.
 
 import os
 
-import matplotlib.pyplot as plt
-import numpy as np
-
 import darsia
 
 # ! ---- DICOM images
@@ -19,16 +16,22 @@ folder = f"{os.path.dirname(__file__)}/images"
 dicom_paths = folder + "/dicom"
 
 # Read dicom images as 4d space-time, or list of 3d images.
-dicom_image_4d = darsia.imread(dicom_paths, series=True)
-dicom_images_3d = darsia.imread(dicom_paths, series=False)
+dicom_image_4d = darsia.imread(dicom_paths, dim=3)
 
 # Print some of the specs
 print(f"The dimensions of the space time dicom image are: {dicom_image_4d.dimensions}")
 
+# Restrict to relevant ROI in z direction.
+roi_z = slice(90, 120)
+dicom_image_roi: darsia.OpticalImage = dicom_image_4d.subregion(
+    voxels=(roi_z, slice(0, None), slice(0, None))
+)
+
+# Apply vertical averaging
+vertical_averaging = darsia.AxisAveraging(axis="z", dim=3)
+dicom_image_flat: darsia.OpticalImage = vertical_averaging(dicom_image_roi)
+
 # Display each time slice, integrated over the z-axis
-for time_index in range(dicom_image_4d.time_num):
-   dicom_slice: darsia.OpticalImage = dicom_image_4d.time_slice(time_index)
-   z_axis, _ = darsia.interpret_indexing(axis="z", indexing="ijk")
-   int_img = np.sum(dicom_slice.img, axis=z_axis)
-   plt.imshow(int_img)
-   plt.show()
+for time_index in range(dicom_image_flat.time_num):
+    dicom_slice: darsia.OpticalImage = dicom_image_flat.time_slice(time_index)
+    dicom_slice.show(time_index, 3)
