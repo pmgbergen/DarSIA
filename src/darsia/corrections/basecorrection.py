90d600b7baf08be99ccb26f8926948c2d42d8555
Jakub Both <jakub.both@uib.no>
2023-03-23 12:40:21 +0100

    MAINT: Use Image in base correction.

--- src/darsia/corrections/basecorrection.py
+++ src/darsia/corrections/basecorrection.py
@@ -11,9 +11,9 @@ import darsia
 class BaseCorrection(ABC):
     def __call__(
         self,
-        image: Union[np.ndarray, darsia.Image, darsia.GeneralImage],
+        image: Union[np.ndarray, darsia.Image],
         return_image: bool = False,
-    ) -> Union[np.ndarray, darsia.Image, darsia.GeneralImage]:
+    ) -> Union[np.ndarray, darsia.Image]:
         """
         Manager for color correction depending on the input type.
 
@@ -26,9 +26,6 @@ class BaseCorrection(ABC):
         if isinstance(image, np.ndarray):
             return self.correct_array(image)
         elif isinstance(image, darsia.Image):
-            image.img = self.correct_array(image.img)
-            return image
-        elif isinstance(image, darsia.GeneralImage):
             if image.series and hasattr(self, "correct_array_series"):
                 # Apply transformation to entrie space time image
                 image.img = self.correct_array_series(image.img)
@@ -40,20 +37,23 @@ class BaseCorrection(ABC):
                 for time_index in range(image.time_num):
                     if image.scalar:
                         # Apply transformation to single time slices for scalar data
-                        corrected_slices.append(self.correct_array(image.img[..., time_index]))
+                        corrected_slices.append(
+                            self.correct_array(image.img[..., time_index])
+                        )
                     else:
                         # Apply transformation to single time slices for vectorial data
-                        corrected_slices.append(self.correct_array(image.img[..., time_index, :]))
+                        corrected_slices.append(
+                            self.correct_array(image.img[..., time_index, :])
+                        )
 
                 # Stack slices together again
-                image.img = np.stack(corrected_slices, axis = image.space_dim)
+                image.img = np.stack(corrected_slices, axis=image.space_dim)
 
             else:
                 # Apply transformation to single image
                 image.img = self.correct_array(image.img)
 
             return image
-
 
     @abstractmethod
     def correct_array(
