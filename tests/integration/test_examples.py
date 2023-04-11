70d308780bf6bd78ff08d773f4b372d5587c5207
Jakub Both <jakub.both@uib.no>
2023-04-11 10:32:34 +0200

    TST: Add vtu example to integration tests.

--- tests/integration/test_examples.py
+++ tests/integration/test_examples.py
@@ -42,13 +42,25 @@ def test_imread():
     #            f'python {str(Path(f"{os.path.dirname(__file__)}/../../examples/dicom_images.py"))}'
     #        )
     #    )
-    #    assert not (
-    #        os.system(
-    #            f'python {str(Path(f"{os.path.dirname(__file__)}/../../examples/vtu_images.py"))}'
-    #        )
-    #    )
     assert not (
         os.system(
             f'python {str(Path(f"{os.path.dirname(__file__)}/../../examples/reading_images.py"))}'
+        )
+    )
+
+
+def test_imread_vtu_images():
+
+    # Test whether images required for test are available
+    folder = Path("../../examples/images")
+    images_exist = len(list(sorted(folder.glob("fracture_flow*.vtu")))) == 2
+
+    if not images_exist:
+        pytest.xfail("Images required for test not available.")
+
+    # If so, run test.
+    assert not (
+        os.system(
+            f'python {str(Path(f"{os.path.dirname(__file__)}/../../examples/vtu_images.py"))}'
         )
     )
