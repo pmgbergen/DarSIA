6d385e37ad90069713dbd10e8a13a19c80a1a789
Jakub Both <jakub.both@uib.no>
2023-04-17 15:53:54 +0200

    TST: Add test addressing reset of time.

--- tests/unit/test_subregion.py
+++ tests/unit/test_subregion.py
@@ -32,6 +32,31 @@ def test_time_interval_scalar_image_2d():
     assert np.allclose(sub_image.time, [1, 2])
 
 
+def test_time_interval_scalar_image_2d_reset_time():
+    """Test whether one can extract time intervals correctly from space-time images."""
+
+    # Construct space-time image
+    meta = {
+        "dim": 2,
+        "series": True,
+        "time": [0, 1, 2, 3, 4],
+    }
+    arr = np.random.rand(3, 4, 5)
+    image = darsia.ScalarImage(arr, **meta)
+
+    # Construct subimage
+    sub_image = image.time_interval(slice(1, 3), reset_time=True)
+
+    # Test whether the dimensions of the image array are correct
+    assert np.allclose(sub_image.shape, (3, 4, 2))
+
+    # Test whether the data is correct
+    assert np.allclose(sub_image.img, arr[:, :, 1:3])
+
+    # Test whether the time is restricted correctly
+    assert np.allclose(sub_image.time, [0, 1])
+
+
 def test_time_interval_advanced():
     """Test whether one can extract time intervals correctly from space-time images."""
 
