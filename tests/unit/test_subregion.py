72a3d03d93aa4c3b1dfce6a85aa90064f31dcc4a
Jakub Both <jakub.both@uib.no>
2023-04-17 13:48:46 +0200

    TST: Test temporal subregions for general images.

--- tests/unit/test_subregion.py
+++ tests/unit/test_subregion.py
@@ -7,14 +7,13 @@ import numpy as np
 import darsia
 
 
-def test_time_interval_scalar_image():
+def test_time_interval_scalar_image_2d():
     """Test whether one can extract time intervals correctly from space-time images."""
 
     # Construct space-time image
     meta = {
         "dim": 2,
         "series": True,
-        "scalar": True,
         "time": [0, 1, 2, 3, 4],
     }
     arr = np.random.rand(3, 4, 5)
@@ -28,6 +27,32 @@ def test_time_interval_scalar_image():
 
     # Test whether the data is correct
     assert np.allclose(sub_image.img, arr[:, :, 1:3])
+
+    # Test whether the time is restricted correctly
+    assert np.allclose(sub_image.time, [1, 2])
+
+
+def test_time_interval_advanced():
+    """Test whether one can extract time intervals correctly from space-time images."""
+
+    # Construct space-time image
+    meta = {
+        "dim": 3,
+        "series": True,
+        "scalar": False,
+        "time": [0, 1, 2, 3, 4],
+    }
+    arr = np.random.rand(3, 4, 5, 6, 7)
+    image = darsia.Image(arr, **meta)
+
+    # Construct subimage
+    sub_image = image.time_interval(slice(1, 3))
+
+    # Test whether the dimensions of the image array are correct
+    assert np.allclose(sub_image.shape, (3, 4, 5, 2, 7))
+
+    # Test whether the data is correct
+    assert np.allclose(sub_image.img, arr[:, :, :, 1:3, :])
 
     # Test whether the time is restricted correctly
     assert np.allclose(sub_image.time, [1, 2])
