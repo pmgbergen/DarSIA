2f3973302bbeb2037e6d0712f5e4ceeaaf47cf54
Jakub Both <jakub.both@uib.no>
2023-04-29 01:18:52 +0200

    EX: Add new plot functions to dicom test.

--- examples/dicom_images.py
+++ examples/dicom_images.py
@@ -21,10 +21,20 @@ dicom_image_4d = darsia.imread(dicom_paths, dim=3)
 # Print some of the specs
 print(f"The dimensions of the space time dicom image are: {dicom_image_4d.dimensions}")
 
+# Extract one time slice and plot
+dicom_image_4d.time_slice(9).show(
+    duration=20, thresh=0.05, relative=True, scaling=10, side_view="integrated"
+)
+
 # Restrict to relevant ROI in z direction.
 roi_z = slice(90, 120)
 dicom_image_roi: darsia.OpticalImage = dicom_image_4d.subregion(
     voxels=(roi_z, slice(0, None), slice(0, None))
+)
+
+# Plot time interval [8, 9]
+dicom_image_roi.time_interval(slice(8, 10)).show(
+    duration=20, thresh=0.05, relative=True, scaling=10, side_view="integrated"
 )
 
 # Apply vertical averaging
