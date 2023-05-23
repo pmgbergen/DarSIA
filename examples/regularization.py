4b09008042d7a375c205473286ac057a145aa173
EStorvik <erlend.storvik@hvl.no>
2023-05-23 10:06:56 +0200

    heterogeneityi in regularization example

--- examples/regularization.py
+++ examples/regularization.py
@@ -5,16 +5,21 @@ import matplotlib.pyplot as plt
 # Load 3D image
 im = np.load("C:/Users/erst/src/image_analysis/images/dicom_3d_test.npy")
 
+# Make regularization parameter heterogenous (for testing purposes)
+mu = np.ones_like(im)
+mu[:, 0:100, :] = 0.5
+
+
 # Regularize image using anisotropic tvd
 im_regularized_tvd = da.split_bregman_anisotropic_tvd(
     im=im,
-    mu=1,
+    mu=mu,
     omega=0.1,
     maxiter=30,
     tol=1e-6,
     verbose=True,
     dim=3,
-    solver_type="jacobi",  # "mg", "cg" also possible
+    solver_type="cg",  # "mg", "jacobi" also possible
     solver_iter=20,
 )
 
