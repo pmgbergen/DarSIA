8eff38458382d79be285bbd0036f9ff6542d02b4
Jakub Both <jakub.both@uib.no>
2023-06-14 14:59:14 +0200

    DOC: Add comments.

--- src/darsia/utils/linear_solvers/solver.py
+++ src/darsia/utils/linear_solvers/solver.py
@@ -1,3 +1,8 @@
+"""Module containing abstract base class for solvers for the problem:
+mass_coeff * x - diffusion_coeff * laplace(x) = rhs
+
+"""
+
 import abc
 from typing import Optional, Union
 
@@ -5,9 +10,9 @@ import numpy as np
 
 
 class Solver:
-
-    """Abstract base class for solvers for the problem
+    """Abstract base class for solvers for the problem:
     mass_coeff * x - diffusion_coeff * laplace(x) = rhs
+
     """
 
     def __init__(
@@ -18,14 +23,31 @@ class Solver:
         maxiter: int = 100,
         tol: Optional[float] = None,
         verbose=False,
+        **kwargs,
     ) -> None:
         self.mass_coeff = mass_coeff
+        """Mass coefficient."""
         self.diffusion_coeff = diffusion_coeff
+        """Diffusion coefficient."""
         self.dim = dim
+        """Spatial dimension of the problem."""
         self.maxiter = maxiter
+        """Maximum number of iterations."""
         self.tol = tol
+        """Tolerance for convergence."""
         self.verbose = verbose
+        """Verbosity."""
 
     @abc.abstractmethod
     def __call__(self, x0: np.ndarray, rhs: np.ndarray) -> np.ndarray:
+        """Main method of the solver - run the solver.
+
+        Args:
+            x0 (np.ndarray): initial guess
+            rhs (np.ndarray): right hand side
+
+        Returns:
+            np.ndarray: solution
+
+        """
         pass
