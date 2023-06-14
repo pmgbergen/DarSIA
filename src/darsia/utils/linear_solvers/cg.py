0f473b54b8a887a90a45d50d0f9d354d6cf7d3f2
Jakub Both <jakub.both@uib.no>
2023-06-14 15:01:40 +0200

    MAINT: Remove redundant call of init, and add doc.

--- src/darsia/utils/linear_solvers/cg.py
+++ src/darsia/utils/linear_solvers/cg.py
@@ -1,4 +1,4 @@
-from typing import Optional, Union
+from typing import Optional
 
 import numpy as np
 import scipy.sparse as sps
@@ -8,81 +8,52 @@ import darsia as da
 
 
 class CG(da.Solver):
+    """Conjugate gradient solver based on scipy's cg for the problem:
+    mass_coeff * x - diffusion_coeff * laplace(x) = rhs
 
-    """Conjugate gradient solver based on scipy's cg.
-    For the problem: mass_coeff * x - diffusion_coeff * laplace(x) = rhs
     """
-
-    def __init__(
-        self,
-        mass_coeff: Union[float, np.ndarray],
-        diffusion_coeff: Union[float, np.ndarray],
-        dim: int = 2,
-        maxiter: int = 100,
-        tol: Optional[float] = None,
-        verbose=False,
-    ) -> None:
-        """
-        Initialize the solver.
-
-        Args:
-            mass_coeff [np.ndarray,float]: mass coefficient
-            diffusion_coeff [np.ndarray,float]: diffusion coefficient
-            dim [int]: dimension of the problem
-            maxiter [int]: maximum number of iterations
-            tol [Optional[float]]: tolerance
-            verbose [bool]: print information
-        """
-        super().__init__(
-            mass_coeff=mass_coeff,
-            diffusion_coeff=diffusion_coeff,
-            dim=dim,
-            maxiter=maxiter,
-            tol=tol,
-            verbose=verbose,
-        )
-        if self.tol is None:
-            self.tol = 1e-2
 
     def __call__(
         self, x0: np.ndarray, rhs: np.ndarray, h: Optional[float] = None
     ) -> np.ndarray:
-        """
-        Solve the problem.
+        """Solve the problem.
 
         Args:
-            x0 [np.ndarray]: initial guess
-            rhs [np.ndarray]: right hand side
-            h [Optional[float]]: grid spacing
+            x0 (np.ndarray): initial guess
+            rhs (np.ndarray): right hand side
+            h (float, optional): grid spacing
 
         Returns:
-            solution [np.ndarray]
+            np.ndarray: solution
+
         """
+        # Safety check
         assert x0.shape == rhs.shape
 
-        # Compute the degrees of freedom
+        # Determine the degrees of freedom
         _dof = np.prod(x0.shape)
 
-        # Create the linear operator
+        # Define the action of the problem on a flat array
         def _mv(x: np.ndarray) -> np.ndarray:
+            # Reshape the input to nd array
             x = np.reshape(x, x0.shape)
+            # Apply the operator and flatten the output
             return (
                 self.mass_coeff * x
                 - da.laplace(x, dim=self.dim, h=h, diffusion_coeff=self.diffusion_coeff)
             ).flatten()
 
+        # Define the linear operator
         lhsoperator = LinearOperator((_dof, _dof), matvec=_mv)
 
         # Solve the problem using scipys conjugate gradient solver
-        out_im = np.reshape(
-            sps.linalg.cg(
-                lhsoperator,
-                rhs.flatten(),
-                x0.flatten(),
-                tol=self.tol,
-                maxiter=self.maxiter,
-            )[0],
-            x0.shape[: self.dim],
+        x, _ = sps.linalg.cg(
+            lhsoperator,
+            rhs.flatten(),
+            x0.flatten(),
+            tol=self.tol,
+            maxiter=self.maxiter,
         )
 
-        return out_im
+        # Return the solution reshaped to the original shape
+        return np.reshape(x, x0.shape[: self.dim])
