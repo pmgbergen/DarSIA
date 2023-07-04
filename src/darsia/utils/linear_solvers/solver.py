358625908523c31aab114df12dfd6d7b3c28ce03
Jakub Both <jakub.both@uib.no>
2023-07-04 15:20:32 +0200

    MAINT: Provide possibility to feed the solver with parameters at later stage.

--- src/darsia/utils/linear_solvers/solver.py
+++ src/darsia/utils/linear_solvers/solver.py
@@ -17,26 +17,46 @@ class Solver:
 
     def __init__(
         self,
-        mass_coeff: Union[float, np.ndarray],
-        diffusion_coeff: Union[float, np.ndarray],
         dim: int = 2,
-        maxiter: int = 100,
+        maxiter: int = 1,
         tol: Optional[float] = None,
+        mass_coeff: Optional[Union[float, np.ndarray]] = None,
+        diffusion_coeff: Optional[Union[float, np.ndarray]] = None,
         verbose=False,
         **kwargs,
     ) -> None:
-        self.mass_coeff = mass_coeff
-        """Mass coefficient."""
-        self.diffusion_coeff = diffusion_coeff
-        """Diffusion coefficient."""
         self.dim = dim
         """Spatial dimension of the problem."""
         self.maxiter = maxiter
         """Maximum number of iterations."""
         self.tol = tol
         """Tolerance for convergence."""
+        self.mass_coeff = mass_coeff
+        """Mass coefficient."""
+        self.diffusion_coeff = diffusion_coeff
+        """Diffusion coefficient."""
         self.verbose = verbose
         """Verbosity."""
+
+    def update_params(
+        self,
+        dim: Optional[int] = None,
+        mass_coeff: Optional[Union[float, np.ndarray]] = None,
+        diffusion_coeff: Optional[Union[float, np.ndarray]] = None,
+    ) -> None:
+        """Update parameters of the solver.
+
+        Args:
+            dim (int, optional): spatial dimension of the problem
+            mass_coeff (float or array, optional): mass coefficient
+            diffusion_coeff (float or array, optional): diffusion coefficient
+
+        """
+        self.dim = dim if dim is not None else self.dim
+        self.mass_coeff = mass_coeff if mass_coeff is not None else self.mass_coeff
+        self.diffusion_coeff = (
+            diffusion_coeff if diffusion_coeff is not None else self.diffusion_coeff
+        )
 
     @abc.abstractmethod
     def __call__(self, x0: np.ndarray, rhs: np.ndarray) -> np.ndarray:
