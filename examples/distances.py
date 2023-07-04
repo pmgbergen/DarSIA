3ddfc33530c69beb07a4f1ca47119ac16d460fa4
Jakub Both <jakub.both@uib.no>
2023-07-04 10:39:45 +0200

    TST: Upgrade example for computing wasserstein distances - include darsia's EMD.

--- examples/distances.py
+++ examples/distances.py
@@ -21,8 +21,8 @@ mass2_array *= np.sum(mass1_array) / np.sum(mass2_array)
 assert np.sum(mass1_array) == np.sum(mass2_array)
 
 # Convert the arrays to actual DarSIA Images
-mass1 = darsia.Image(mass1_array, width=2, height=1)
-mass2 = darsia.Image(mass2_array, width=2, height=1)
+mass1 = darsia.Image(mass1_array, width=20, height=10, scalar=True, dim=2, series=False)
+mass2 = darsia.Image(mass2_array, width=20, height=10, scalar=True, dim=2, series=False)
 
 # Plot if requested
 if False:
@@ -31,6 +31,10 @@ if False:
     plt.figure("Mass 2")
     plt.imshow(mass2.img)
     plt.show()
+
+# ! ---- Compute EMD using CV2
+
+# NOTE cv2.EMD has strict memory limitations. Coarsening is required.
 
 # Setup EMD object, including a resize routine (needed for cv2.EMD)
 # Determine distance between different masses
@@ -45,4 +49,65 @@ emd = darsia.EMD(resize)
 
 # Determine the EMD
 distance = emd(mass1, mass2)
-print(f"The distance between the two mass distributions is: {distance} meters.")
+print(f"The cv2.emd distance between the two mass distributions is: {distance} meters.")
+print()
+
+
+# ! ---- Compute EMD using Newton
+
+options = {
+    "L": 1e-2,
+    "num_iter": 1000,
+    "tol": 1e-8,
+    "tol_distance": 1e-5,
+    "regularization": 1e-12,
+    "scaling": 30,
+    "depth": 10,
+    "verbose": False,
+}
+
+distance_newton, _, _, _, status_newton = darsia.wasserstein_distance(
+    mass1,
+    mass2,
+    method="Newton",
+    options=options,
+    plot_solution=True,
+    return_solution=True,
+)
+print(
+    f"""The Wasserstein distance (computed using the Newton method) between the two """
+    f"""mass distributions is: {distance_newton} meters."""
+)
+print("Newton status:", status_newton, "\n")
+
+
+# ! ---- Compute EMD using Bregman solver
+
+options = {
+    "L": 1e2,
+    "num_iter": 1000,
+    "tol": 1e-8,
+    "regularization": 1e-12,
+    "scaling": 30,
+    "depth": 1,  # TODO increase depth.
+    "verbose": False,
+    "update_l": True,
+    "tol_distance": 1e-6,
+    "max_iter_increase_diff": 20,
+    "l_factor": 2,
+    "L_max": 1e6,
+}
+distance_bregman, _, _, _, status_bregman = darsia.wasserstein_distance(
+    mass1,
+    mass2,
+    method="Bregman",
+    options=options,
+    plot_solution=True,
+    return_solution=True,
+)
+
+print(
+    f"""The Wasserstein distance (computed using Bregman split) between the two """
+    f"""mass distributions is: {distance} meters."""
+)
+print("Bregman status:", status_bregman)
