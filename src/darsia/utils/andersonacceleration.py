d4f62aa810d104cea10a26ff43bc215e2736bc88
Jakub Both <jakub.both@uib.no>
2023-08-28 21:34:39 +0200

    ENH: Add restart and adaptive dimension to AA.

--- src/darsia/utils/andersonacceleration.py
+++ src/darsia/utils/andersonacceleration.py
@@ -1,14 +1,30 @@
-from typing import Union
+"""Anderson acceleration as described by Walker and Ni in doi:10.2307/23074353."""
+
+from typing import Optional, Union
 
 import numpy as np
 import scipy as sp
 
 
 class AndersonAcceleration:
-    """Anderson acceleration as described by Walker and Ni in doi:10.2307/23074353."""
+    """Anderson acceleration for fixed-point methods."""
 
-    def __init__(self, dimension: Union[int, tuple[int]], depth: int = 0) -> None:
-        """Initialize Anderson acceleration."""
+    def __init__(
+        self,
+        dimension: Optional[Union[int, tuple[int]]] = None,
+        depth: int = 0,
+        restart: Optional[int] = None,
+    ) -> None:
+        """Initialize Anderson acceleration.
+
+        Args:
+            dimension (int, tuple[int]): dimension of the problem. If a tuple is given,
+                the problem is assumed to be a tensor problem and the dimension is
+                calculated as the product of the tuple entries.
+            depth (int): depth of the acceleration. If 0, no acceleration is applied.
+            restart (int): restart of the acceleration. If None, no restart is applied.
+
+        """
 
         if isinstance(dimension, np.integer):
             self._dimension = dimension
@@ -17,15 +33,14 @@ class AndersonAcceleration:
             self.tensor_shape = dimension
             self._dimension = int(np.prod(dimension))
             self._tensor = True
+        elif dimension is None:
+            self._tensor = False
+            pass
         else:
             raise ValueError("Dimension not recognized.")
 
         self._depth = depth
-
-        # Initialize arrays for iterates.
-        self.reset()
-        self._fkm1: np.ndarray = self._Fk.copy()
-        self._gkm1: np.ndarray = self._Gk.copy()
+        self._restart = restart
 
     def reset(self) -> None:
         """Reset Anderson acceleration."""
@@ -35,6 +50,10 @@ class AndersonAcceleration:
         self._Gk: np.ndarray = np.zeros(
             (self._dimension, self._depth)
         )  # changes in fixed point applications
+        self._fkm1: np.ndarray = self._Fk.copy()
+        self._gkm1: np.ndarray = self._Gk.copy()
+
+        self._inner_iteration = 0
 
     def __call__(self, gk: np.ndarray, fk: np.ndarray, iteration: int) -> np.ndarray:
         """Apply Anderson acceleration.
@@ -54,15 +73,18 @@ class AndersonAcceleration:
             gk = np.ravel(gk)
             fk = np.ravel(fk)
 
-        if iteration == 0:
-            self._Fk = np.zeros((self._dimension, self._depth))  # changes in increments
-            self._Gk = np.zeros(
-                (self._dimension, self._depth)
-            )  # changes in fixed point applications
+        if self._restart is not None:
+            self._inner_iteration = iteration % self._restart
+        else:
+            self._inner_iteration = iteration
 
-        mk = min(iteration, self._depth)
+        # Reset if necessary.
+        if self._inner_iteration == 0:
+            self._dimension = len(gk)
+            self.reset()
 
-        # Apply actual acceleration (not in the first iteration).
+        # Apply actual acceleration (not in the first iteration, of any restart loop).
+        mk = min(self._inner_iteration, self._depth)
         if mk > 0:
             # Build matrices of changes
             col = (iteration - 1) % self._depth
