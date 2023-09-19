a1330576e8f7a5fb6a0e45b53f7f8b8b608e038e
Jakub Both <jakub.both@uib.no>
2023-09-20 00:58:58 +0200

    TST: Add many more fv tests.

--- tests/unit/test_fv.py
+++ tests/unit/test_fv.py
@@ -117,3 +117,77 @@ def test_mass_face_3d():
     # Check diagonal values
     assert len(np.unique(np.diag(mass))) == 1
     assert np.isclose(mass[0, 0], 0.5 * 0.25)
+
+
+def test_tangential_reconstruction_2d_1():
+    grid = darsia.Grid(shape=(2, 3), voxel_size=[0.5, 0.25])
+    tangential_reconstruction = darsia.FVTangentialReconstruction(grid).mat.todense()
+
+    # Check shape
+    assert np.allclose(
+        tangential_reconstruction.shape, (grid.num_faces, grid.num_faces)
+    )
+
+    # Check values - first for exterior faces
+    assert np.allclose(tangential_reconstruction[0], [0, 0, 0, 0.25, 0.25, 0, 0])
+    assert np.allclose(tangential_reconstruction[4], [0.25, 0.25, 0, 0, 0, 0, 0])
+
+    # Check values - then for interior faces
+    assert np.allclose(tangential_reconstruction[1], [0, 0, 0, 0.25, 0.25, 0.25, 0.25])
+
+
+def test_tangential_reconstruction_2d_2():
+    grid = darsia.Grid(shape=(3, 4), voxel_size=[0.5, 0.25])
+    tangential_reconstruction = darsia.FVTangentialReconstruction(grid).mat.todense()
+
+    # Check shape
+    assert np.allclose(
+        tangential_reconstruction.shape, (grid.num_faces, grid.num_faces)
+    )
+
+    # Check values - first for exterior faces
+    assert np.allclose(np.nonzero(tangential_reconstruction[0])[1], [8, 9])
+    assert np.allclose(np.nonzero(tangential_reconstruction[7])[1], [15, 16])
+
+    # Check values - then for interior faces
+    assert np.allclose(np.nonzero(tangential_reconstruction[2])[1], [8, 9, 11, 12])
+    assert np.allclose(np.nonzero(tangential_reconstruction[15])[1], [4, 5, 6, 7])
+
+    # Apply once and prove values
+    tangential_reconstruction_sparse = darsia.FVTangentialReconstruction(grid).mat
+    normal_flux = np.arange(grid.num_faces)
+    tangential_flux = tangential_reconstruction_sparse.dot(normal_flux)
+    assert np.allclose(
+        tangential_flux[np.array([0, 1, 4, 8, 12, 16])], [4.25, 4.75, 13, 0.5, 3.5, 3]
+    )
+
+
+def test_face_to_cell_2d():
+    grid = darsia.Grid(shape=(3, 4), voxel_size=[0.5, 0.25])
+    num_faces = grid.num_faces
+    flat_flux = np.arange(num_faces)
+    cell_flux = darsia.face_to_cell(grid, flat_flux)
+
+    # Check shape
+    assert np.allclose(cell_flux.shape, (*grid.shape, grid.dim))
+
+    # Check values
+    print(cell_flux)
+    assert np.allclose(cell_flux[0, 0], [0, 4])
+    assert np.allclose(cell_flux[2, 3], [3.5, 8])
+    assert np.allclose(cell_flux[1, 1], [2.5, 10.5])
+
+
+def test_face_to_cell_3d():
+    grid = darsia.Grid(shape=(3, 4, 5), voxel_size=[0.5, 0.25, 2])
+    num_faces = grid.num_faces
+    flat_flux = np.arange(num_faces)
+    cell_flux = darsia.face_to_cell(grid, flat_flux)
+
+    # Check shape
+    assert np.allclose(cell_flux.shape, (*grid.shape, grid.dim))
+
+    # Check values
+    assert np.allclose(cell_flux[0, 0, 0], [0, 20, 42.5])
+    assert np.allclose(cell_flux[2, 3, 4], [19.5, 42, 66])
+    assert np.allclose(cell_flux[1, 1, 1], [10.5, 51.5, 95])
