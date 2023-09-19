17108ad05fd74e277ea04a75d37956156b081ab9
Jakub Both <jakub.both@uib.no>
2023-09-19 23:50:01 +0200

    TST: Add test for mass matrices.

--- tests/unit/test_fv.py
+++ tests/unit/test_fv.py
@@ -57,3 +57,63 @@ def test_divergence_3d():
         divergence[16, np.array([10, 11, 50, 53, 89, 101])],
         [-0.5, 0.5, -1, 1, -0.125, 0.125],
     )
+
+
+def test_mass_2d():
+    grid = darsia.Grid(shape=(4, 5), voxel_size=[0.5, 0.25])
+    mass = darsia.FVMass(grid).mat.todense()
+
+    # Check shape
+    assert np.allclose(mass.shape, (grid.num_cells, grid.num_cells))
+
+    # Check diagonal structure
+    assert np.linalg.norm(mass - np.diag(np.diag(mass))) < 1e-10
+
+    # Check diagonal values
+    assert len(np.unique(np.diag(mass))) == 1
+    assert np.isclose(mass[0, 0], 0.125)
+
+
+def test_mass_3d():
+    grid = darsia.Grid(shape=(3, 4, 5), voxel_size=[0.5, 0.25, 2])
+    mass = darsia.FVMass(grid).mat.todense()
+
+    # Check shape
+    assert np.allclose(mass.shape, (grid.num_cells, grid.num_cells))
+
+    # Check diagonal structure
+    assert np.linalg.norm(mass - np.diag(np.diag(mass))) < 1e-10
+
+    # Check diagonal values
+    assert len(np.unique(np.diag(mass))) == 1
+    assert np.isclose(mass[0, 0], 0.25)
+
+
+def test_mass_face_2d():
+    grid = darsia.Grid(shape=(4, 5), voxel_size=[0.5, 0.25])
+    mass = darsia.FVMass(grid, mode="faces", lumping=True).mat.todense()
+
+    # Check shape
+    assert np.allclose(mass.shape, (grid.num_faces, grid.num_faces))
+
+    # Check diagonal structure
+    assert np.linalg.norm(mass - np.diag(np.diag(mass))) < 1e-10
+
+    # Check diagonal values
+    assert len(np.unique(np.diag(mass))) == 1
+    assert np.isclose(mass[0, 0], 0.5 * 0.125)
+
+
+def test_mass_face_3d():
+    grid = darsia.Grid(shape=(3, 4, 5), voxel_size=[0.5, 0.25, 2])
+    mass = darsia.FVMass(grid, mode="faces", lumping=True).mat.todense()
+
+    # Check shape
+    assert np.allclose(mass.shape, (grid.num_faces, grid.num_faces))
+
+    # Check diagonal structure
+    assert np.linalg.norm(mass - np.diag(np.diag(mass))) < 1e-10
+
+    # Check diagonal values
+    assert len(np.unique(np.diag(mass))) == 1
+    assert np.isclose(mass[0, 0], 0.5 * 0.25)
