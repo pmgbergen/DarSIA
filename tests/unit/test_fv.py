90ff14810fc7f937e031712f989d2bd908254820
Jakub Both <jakub.both@uib.no>
2023-09-21 12:34:37 +0200

    TST: Cover tangential and full face reconstruction.

--- tests/unit/test_fv.py
+++ tests/unit/test_fv.py
@@ -121,7 +121,9 @@ def test_mass_face_3d():
 
 def test_tangential_reconstruction_2d_1():
     grid = darsia.Grid(shape=(2, 3), voxel_size=[0.5, 0.25])
-    tangential_reconstruction = darsia.FVTangentialReconstruction(grid).mat.todense()
+    tangential_reconstruction = (
+        darsia.FVTangentialFaceReconstruction(grid).mat[0].todense()
+    )
 
     # Check shape
     assert np.allclose(
@@ -138,27 +140,110 @@ def test_tangential_reconstruction_2d_1():
 
 def test_tangential_reconstruction_2d_2():
     grid = darsia.Grid(shape=(3, 4), voxel_size=[0.5, 0.25])
-    tangential_reconstruction = darsia.FVTangentialReconstruction(grid).mat.todense()
+    tangential_reconstruction_dense = (
+        darsia.FVTangentialFaceReconstruction(grid).mat[0].todense()
+    )
 
     # Check shape
     assert np.allclose(
-        tangential_reconstruction.shape, (grid.num_faces, grid.num_faces)
+        tangential_reconstruction_dense.shape, (grid.num_faces, grid.num_faces)
     )
 
     # Check values - first for exterior faces
-    assert np.allclose(np.nonzero(tangential_reconstruction[0])[1], [8, 9])
-    assert np.allclose(np.nonzero(tangential_reconstruction[7])[1], [15, 16])
+    assert np.allclose(np.nonzero(tangential_reconstruction_dense[0])[1], [8, 9])
+    assert np.allclose(np.nonzero(tangential_reconstruction_dense[7])[1], [15, 16])
 
     # Check values - then for interior faces
-    assert np.allclose(np.nonzero(tangential_reconstruction[2])[1], [8, 9, 11, 12])
-    assert np.allclose(np.nonzero(tangential_reconstruction[15])[1], [4, 5, 6, 7])
+    assert np.allclose(
+        np.nonzero(tangential_reconstruction_dense[2])[1], [8, 9, 11, 12]
+    )
+    assert np.allclose(np.nonzero(tangential_reconstruction_dense[15])[1], [4, 5, 6, 7])
 
     # Apply once and prove values
-    tangential_reconstruction_sparse = darsia.FVTangentialReconstruction(grid).mat
+    tangential_reconstruction = darsia.FVTangentialFaceReconstruction(grid)
     normal_flux = np.arange(grid.num_faces)
-    tangential_flux = tangential_reconstruction_sparse.dot(normal_flux)
+    tangential_flux = tangential_reconstruction(normal_flux)
     assert np.allclose(
         tangential_flux[np.array([0, 1, 4, 8, 12, 16])], [4.25, 4.75, 13, 0.5, 3.5, 3]
+    )
+
+
+def test_full_reconstruction_2d_2():
+    grid = darsia.Grid(shape=(3, 4), voxel_size=[0.5, 0.25])
+
+    # Apply once and prove values
+    tangential_reconstruction = darsia.FVFullFaceReconstruction(grid)
+    normal_flux = np.arange(grid.num_faces)
+    full_flux = tangential_reconstruction(normal_flux)
+
+    # Check shape
+    assert np.allclose(full_flux.shape, (grid.num_faces, 2))
+
+    # Check values
+    assert np.allclose(full_flux[0], [0, 4.25])
+    assert np.allclose(full_flux[1], [1, 4.75])
+    assert np.allclose(full_flux[4], [4, 13])
+    assert np.allclose(full_flux[8], [0.5, 8])
+    assert np.allclose(full_flux[12], [3.5, 12])
+    assert np.allclose(full_flux[16], [3, 16])
+
+
+def test_tangential_reconstruction_3d():
+    grid = darsia.Grid(shape=(3, 3, 3), voxel_size=[0.5, 0.25, 2])
+    tangential_reconstruction = darsia.FVTangentialFaceReconstruction(grid)
+
+    # Apply once and probe values
+    normal_flux = np.arange(grid.num_faces)
+    tangential_flux = tangential_reconstruction(normal_flux, concatenate=False)
+
+    # Corner block
+    assert np.allclose(
+        [tangential_flux[0][0], tangential_flux[1][0]], [(18 + 19) / 4, (36 + 37) / 4]
+    )
+    assert np.allclose(
+        [tangential_flux[0][18], tangential_flux[1][18]], [(0 + 2) / 4, (36 + 39) / 4]
+    )
+    assert np.allclose(
+        [tangential_flux[0][36], tangential_flux[1][36]], [(0 + 6) / 4, (18 + 24) / 4]
+    )
+
+    # Center block
+    assert np.allclose(
+        [tangential_flux[0][8], tangential_flux[1][8]],
+        [(24 + 25 + 27 + 28) / 4, (39 + 40 + 48 + 49) / 4],
+    )
+    assert np.allclose(
+        [tangential_flux[0][25], tangential_flux[1][25]],
+        [(6 + 7 + 8 + 9) / 4, (37 + 40 + 46 + 49) / 4],
+    )
+    assert np.allclose(
+        [tangential_flux[0][40], tangential_flux[1][40]],
+        [(2 + 3 + 8 + 9) / 4, (19 + 22 + 25 + 28) / 4],
+    )
+
+
+def test_full_reconstruction_3d():
+    grid = darsia.Grid(shape=(3, 3, 3), voxel_size=[0.5, 0.25, 2])
+    full_reconstuction = darsia.FVFullFaceReconstruction(grid)
+
+    # Apply once and probe values
+    normal_flux = np.arange(grid.num_faces)
+    full_flux = full_reconstuction(normal_flux)
+
+    # Corner block
+    assert np.allclose(full_flux[0], [0, (18 + 19) / 4, (36 + 37) / 4])
+    assert np.allclose(full_flux[18], [(0 + 2) / 4, 18, (36 + 39) / 4])
+    assert np.allclose(full_flux[36], [(0 + 6) / 4, (18 + 24) / 4, 36])
+
+    # Center block
+    assert np.allclose(
+        full_flux[8], [8, (24 + 25 + 27 + 28) / 4, (39 + 40 + 48 + 49) / 4]
+    )
+    assert np.allclose(
+        full_flux[25], [(6 + 7 + 8 + 9) / 4, 25, (37 + 40 + 46 + 49) / 4]
+    )
+    assert np.allclose(
+        full_flux[40], [(2 + 3 + 8 + 9) / 4, (19 + 22 + 25 + 28) / 4, 40]
     )
 
 
