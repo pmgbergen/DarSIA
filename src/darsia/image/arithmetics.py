631043260181e4b6f532c7edce47b25f8ab52ddc
Jakub Both <jakub.both@uib.no>
2023-09-21 19:50:44 +0200

    MAINT: Improve error message for compatibility check of coordinate systems.

--- src/darsia/image/arithmetics.py
+++ src/darsia/image/arithmetics.py
@@ -32,9 +32,10 @@ def weight(img: darsia.Image, weight: Union[float, int, darsia.Image]) -> darsia
         weighted_img.img *= weight
 
     elif isinstance(weight, darsia.Image):
-        assert darsia.check_equal_coordinatesystems(
+        equal_coordinate_system, log = darsia.check_equal_coordinatesystems(
             img.coordinatesystem, weight.coordinatesystem, exclude_size=True
         )
+        assert equal_coordinate_system, f"{log}"
         space_dim = img.space_dim
         assert len(weight.img.shape) == space_dim
 
