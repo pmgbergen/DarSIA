ad3ea42a0c3794b50be06adca422333746f862e7
Jakub Both <jakub.both@uib.no>
2023-10-16 21:04:07 +0200

    DOC: Add simple documentation.

--- src/darsia/utils/kernels.py
+++ src/darsia/utils/kernels.py
@@ -1,25 +1,73 @@
+"""Standard kernels accompanied by abstract base class.
+
+Provided:
+    * Linear kernel
+    * Gaussian kernel
+
+"""
+
 from abc import ABC
+
 import numpy as np
 
 
 class BaseKernel(ABC):
-    def __call__(self, x, y):
+    """Abstract base class for kernel."""
+
+    def __call__(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """Compute kernel between two arrays.
+
+        Args:
+            x (np.ndarray): first array
+            y (np.ndarray): second array
+
+        Returns:
+            np.ndarray: kernel between x and y
+
+        """
         pass
 
 
-# define linear kernel shifted to avoid singularities
 class LinearKernel(BaseKernel):
-    def __init__(self, a=0):
-        self.a = a
+    """Linear kernel.
 
-    def __call__(self, x, y):
+    NOTE: Allows to to avoid singularities by shifting the kernel by a constant.
+
+    """
+
+    def __init__(self, a: float = 0):
+        self.a = a
+        """Shift of the kernel."""
+
+    def __call__(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """Compute kernel between two arrays.
+
+        Args:
+            x (np.ndarray): first array
+            y (np.ndarray): second array
+
+        Returns:
+            np.ndarray: kernel between x and y
+
+        """
         return np.sum(np.multiply(x, y), axis=-1) + self.a
 
 
-# define gaussian kernel
 class GaussianKernel(BaseKernel):
-    def __init__(self, gamma=1):
-        self.gamma = gamma
+    """Gaussian kernel."""
 
-    def __call__(self, x, y):
+    def __init__(self, gamma: float = 1.0):
+        self.gamma = gamma
+        """Gamma parameter of the kernel."""
+
+    def __call__(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """Compute kernel between two arrays.
+
+        Args:
+            x (np.ndarray): first array
+            y (np.ndarray): second array
+
+        Returns:
+            np.ndarray: kernel between x and y
+        """
         return np.exp(-self.gamma * np.sum(np.multiply(x - y, x - y), axis=-1))
