0fe7e72bb7a1c7d4cb3fdfd2359b7c8ed61542cf
Jakub Both <jakub.both@uib.no>
2023-11-04 11:25:05 +0100

    BUG: Use correct order of fluxes in quiver plot

--- src/darsia/utils/plotting.py
+++ src/darsia/utils/plotting.py
@@ -40,6 +40,7 @@ def plot_2d_wasserstein_distance(
     if save_plot:
         folder = kwargs.get("folder", ".")
         Path(folder).mkdir(parents=True, exist_ok=True)
+        dpi = kwargs.get("dpi", 500)
     show_plot = kwargs.get("show", True)
 
     # Control of flux arrows
@@ -59,34 +60,36 @@ def plot_2d_wasserstein_distance(
     plt.colorbar(label="pressure")
     plt.xlabel("x [m]")
     plt.ylabel("y [m]")
+
+    # Save the plot
     if save_plot:
         plt.savefig(
             folder + "/" + name + "_beckman_solution_pressure.png",
-            dpi=500,
+            dpi=dpi,
             transparent=True,
         )
 
     # Plot the fluxes
     plt.figure("Beckman solution fluxes")
-    plt.pcolormesh(X, Y, mass_diff, cmap="turbo")  # , vmin=-1, vmax=3.5)
+    plt.pcolormesh(X, Y, mass_diff, cmap="turbo")
     plt.colorbar(label="mass difference")
     plt.quiver(
         X[::resolution, ::resolution],
         Y[::resolution, ::resolution],
-        scaling * flux[::resolution, ::resolution, 0],
         -scaling * flux[::resolution, ::resolution, 1],
+        scaling * flux[::resolution, ::resolution, 0],
         angles="xy",
-        scale_units="xy",
-        # scale=1,
         alpha=0.25,
         width=0.01,
     )
     plt.xlabel("x [m]")
     plt.ylabel("y [m]")
+
+    # Save the plot
     if save_plot:
         plt.savefig(
             folder + "/" + name + "_beckman_solution_fluxes.png",
-            dpi=500,
+            dpi=dpi,
             transparent=True,
         )
 
@@ -96,14 +99,16 @@ def plot_2d_wasserstein_distance(
     plt.colorbar(label="flux modulus")
     plt.xlabel("x [m]")
     plt.ylabel("y [m]")
+
+    # Save the plot
     if save_plot:
-        dpi = kwargs.get("dpi", 500)
         plt.savefig(
             folder + "/" + name + "_beckman_solution_transport_density.png",
             dpi=dpi,
             transparent=True,
         )
 
+    # Show the plot
     if show_plot:
         plt.show()
     else:
