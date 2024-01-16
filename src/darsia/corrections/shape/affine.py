b9405c73c4d9e39d29f76ebcfb255ccbbb9e1d19
Jakub Both <jakub.both@uib.no>
2024-01-16 14:39:24 +0100

    MAINT: Initialize change of affine transformation to be based on BaseTransformation.

--- src/darsia/corrections/shape/affine.py
+++ src/darsia/corrections/shape/affine.py
@@ -7,7 +7,6 @@ contained.
 """
 from __future__ import annotations
 
-import itertools
 from typing import Optional, Union
 from warnings import warn
 
@@ -17,11 +16,8 @@ from scipy.spatial.transform import Rotation
 
 import darsia
 
-# Shortcut for coordinate type (voxel and Cartesian)
-CoordinateType = list[Union[float, int]]
 
-
-class AffineTransformation:
+class AffineTransformation(darsia.BaseTransformation):
     """Affine map, restricted to translation, scaling, rotation, resulting in
     conservation of angles.
 
@@ -31,78 +27,34 @@ class AffineTransformation:
 
     def __init__(
         self,
-        pts_src: Optional[Union[list[CoordinateType], np.ndarray]] = None,
-        pts_dst: Optional[Union[list[CoordinateType], np.ndarray]] = None,
-        isometry: bool = True,
-        **kwargs,
+        dim: int,
     ) -> None:
-        """Constructor.
+        """Constructor for identity encoded as affine transformation.
 
         Args:
-            pts_src (list, or array, optional): coordinates corresponding to source data
-            pts_dst (list, or array, optional): coordinates corresponding to destination
-                data
-            isometry (bool): Flag controlling whether the underlying transformation is
-                an isometry.
-
-        NOTE: If no input is provided, an identity is constructed.
+            dim (int): dimension of the Euclidean space
 
         Raises:
             ValueError: if not sufficient input data is provided.
             ValueError: if dimension not 2 or 3.
 
         """
+        # Pre-define management of input/output types
+        super().__init__()
 
-        # ! ---- Characteristics
-
-        self.isometry = isometry
-        """Flag storing whether the underlying transformation is an isometry."""
-
-        # ! ---- Dimension
-
-        # Determine dimensionality from input - perform additional safety checks
-        if pts_src is not None and pts_dst is not None:
-            assert len(pts_src) == len(pts_dst)
-            assert all([len(pts_src[0]) == len(p) for p in pts_src])
-            assert all([len(pts_dst[0]) == len(p) for p in pts_dst])
-            assert len(pts_src[0]) == len(pts_dst[0])
-            dim = len(pts_src[0])
-
-        elif pts_src is not None or pts_dst is not None:
-            raise ValueError("Both pts_src and pts_dst need to be provided.")
-
-        else:
-            if "dim" not in kwargs:
-                raise ValueError("Dimension needs to be provided.")
-            dim = kwargs["dim"]
-
-            assert isinstance(dim, int), "Dimension needs to be an integer."
-
-        if dim not in [2, 3]:
-            raise ValueError("Dimension needs to be 2 or 3.")
+        # Define identity
         self.dim = dim
         """Dimension of the Euclidean space."""
-
-        # ! ---- Map
-
-        # If data provided, fit the parameters accordingly
-        if pts_src is not None and pts_dst is not None:
-            options = kwargs.get("options", {})
-            self.fit(pts_src, pts_dst, **options)
-
-        else:
-            # Define identity
-            self.translation = np.zeros(self.dim, dtype=float)
-            """Translation vector."""
-
-            self.scaling = 1.0
-            """Scaling factor."""
-
-            self.rotation = np.eye(self.dim)
-            """Rotation matrix."""
-
-            self.rotation_inv = np.eye(self.dim)
-            """Inverse of rotation matrix."""
+        self.translation = np.zeros(self.dim, dtype=float)
+        """Translation vector."""
+        self.scaling = 1.0
+        """Scaling factor."""
+        self.rotation = np.eye(self.dim)
+        """Rotation matrix."""
+        self.rotation_inv = np.eye(self.dim)
+        """Inverse of rotation matrix."""
+        self.isometry = False
+        """Flag storing whether the underlying transformation is restricted to isometry."""
 
     def set_parameters(
         self,
@@ -176,40 +128,52 @@ class AffineTransformation:
                 (translation, scaling, rotation).
 
         """
-        rotations_dofs: int = 1 if self.dim == 2 else self.dim
+        num_rotations_dofs: int = 1 if self.dim == 2 else self.dim
         if self.isometry:
-            assert len(parameters) == self.dim + rotations_dofs
+            assert len(parameters) == self.dim + num_rotations_dofs
         else:
-            assert len(parameters) == self.dim + 1 + rotations_dofs
+            assert len(parameters) == self.dim + 1 + num_rotations_dofs
         translation = parameters[0 : self.dim]
         scaling = 1.0 if self.isometry else parameters[self.dim]
-        rotation = parameters[-rotations_dofs:]
+        rotation = parameters[-num_rotations_dofs:]
 
         self.set_parameters(translation, scaling, rotation)
 
     def fit(
         self,
-        pts_src: Union[list[CoordinateType], np.ndarray],
-        pts_dst: Union[list[CoordinateType], np.ndarray],
-        preconditioning: bool = True,
-        **kwargs,
+        pts_src: Union[darsia.VoxelArray, darsia.CoordinateArray],
+        pts_dst: Union[darsia.VoxelArray, darsia.CoordinateArray],
+        fit_options: dict = {},
     ) -> bool:
         """Least-squares parameter fit based on source and target coordinates.
 
         Args:
-            pts_src (list or array): coordinates corresponding to source data
-            pts_dst (list or array): coordinates corresponding to destination data
-            preconditioning (bool): Flag controlling whether the optimization problem
-                is preconditioned by estimating a better initial guess for the
-                translation.
+            pts_src (VoxelArray or CoordinateArray): source points
+            pts_dst (VoxelArray or CoordinateArray): target points
+            fit_options (dict): options for the fit routine
+                preconditioning (bool): Flag controlling whether the optimization
+                    problem is preconditioned by estimating a better initial guess for
+                    the translation.
+                tol (float): tolerance for optimization
+                maxiter (int): maximum number of iterations for optimization
+                isometry (bool): Flag controlling whether the underlying transformation
+                    is an isometry.
 
         Returns:
             bool: success of parameter fit
 
         """
-        # Fetch calibration options
-        tol = kwargs.get("tol", 1e-2)
-        maxiter = kwargs.get("maxiter", 100)
+        # Check input
+        assert pts_src.shape == pts_dst.shape, "Shape mismatch."
+        assert pts_src.shape[1] == self.dim, "Dimension mismatch."
+        self.set_dtype(pts_src, pts_dst)
+
+        # Fetch calibration options - update internal flag controlling the parameters
+        # entering the optimization (isometry)
+        preconditioning = fit_options.get("preconditioning", True)
+        tol = fit_options.get("tol", 1e-2)
+        maxiter = fit_options.get("maxiter", 100)
+        self.isometry = fit_options.get("isometry", False)
 
         # For the initial guess, start with the identity
         if self.isometry:
@@ -231,13 +195,13 @@ class AffineTransformation:
             # Estimate better initial guess for the translation and correct the src
             # points. This will help the optimization to converge faster. Estimate the
             # translation by comparing the centers of mass of the src and dst points.
-            center_src = np.mean(np.array(pts_src), axis=0)
-            center_dst = np.mean(np.array(pts_dst), axis=0)
+            center_src = np.mean(pts_src, axis=0)
+            center_dst = np.mean(pts_dst, axis=0)
             preconditioning_translation = center_dst - center_src
 
             # Guess some better initial translation and update the src points
             # initial_translation = np.zeros(self.dim, dtype=float)
-            pts_src = [np.array(pt) + preconditioning_translation for pt in pts_src]
+            pts_src = pts_src + preconditioning_translation
 
             # When preconditioning is enabled, the identity is used as initial guess
             # as the parameterswe use the initial guess as the
@@ -245,20 +209,19 @@ class AffineTransformation:
         else:
             # Allow the user to define a tailored initial guess, but use the identity
             # as default.
-            initial_guess = kwargs.get("initial_guess", identity_parameters)
+            initial_guess = fit_options.get("initial_guess", identity_parameters)
 
         # Define least squares objective function
         def objective_function(params: np.ndarray):
             self.set_parameters_as_vector(params)
-            pts_mapped = self.__call__(np.array(pts_src))
-            diff = np.array(pts_dst) - pts_mapped
-            defect = np.sum(diff**2)
-            return defect
+            pts_mapped = self.__call__(pts_src)
+            return np.sum((pts_dst - pts_mapped) ** 2)
 
         # Perform optimization step
         opt_result = optimize.minimize(
             objective_function,
             initial_guess,
+            # method="Powell",
             tol=tol,
             options={"maxiter": maxiter, "disp": True},
         )
@@ -283,6 +246,10 @@ class AffineTransformation:
         # Final update of model parameters
         self.set_parameters_as_vector(opt_result.x)
 
+        # print(opt_result.x)
+        # print(pts_src)
+        # print(self.__call__(pts_src))
+
         # Check success
         if opt_result.success:
             print(
@@ -295,37 +262,37 @@ class AffineTransformation:
 
     # ! ---- Application
 
-    def __call__(self, array: np.ndarray) -> np.ndarray:
-        """Application of map.
+    def call_array(self, x: np.ndarray) -> np.ndarray:
+        """Application of map to arrays.
 
         Args:
-            array (np.ndarray): (collection of) dim-dimensional Euclidean vector
+            x (np.ndarray): (collection of) dim-dimensional Euclidean vector
 
         Returns:
             np.ndarray: function values of affine map
 
         """
-        num, dim = np.atleast_2d(array).shape
+        num, dim = np.atleast_2d(x).shape
         assert dim == self.dim
         function_values = np.outer(
             np.ones(num), self.translation
         ) + self.scaling * np.transpose(
-            self.rotation.dot(np.transpose(np.atleast_2d(array)))
+            self.rotation.dot(np.transpose(np.atleast_2d(x)))
         )
 
-        return function_values.reshape(array.shape)
+        return function_values.reshape(x.shape)
 
-    def inverse(self, array: np.ndarray) -> np.ndarray:
-        """Application of inverse of the map.
+    def inverse_array(self, x: np.ndarray) -> np.ndarray:
+        """Application of inverse of the map to arrays.
 
         Args:
-            array (np.ndarray): (collection of) dim-dimensional Euclidean vector
+            x (np.ndarray): (collection of) dim-dimensional Euclidean vector,
 
         Returns:
             np.ndarray: function values of affine inverse map
 
         """
-        num, dim = np.atleast_2d(array).shape
+        num, dim = np.atleast_2d(x).shape
         assert dim == self.dim
         function_values = (
             1.0
@@ -333,16 +300,16 @@ class AffineTransformation:
             * np.transpose(
                 self.rotation_inv.dot(
                     np.transpose(
-                        np.atleast_2d(array) - np.outer(np.ones(num), self.translation)
+                        np.atleast_2d(x) - np.outer(np.ones(num), self.translation)
                     )
                 )
             )
         )
 
-        return function_values.reshape(array.shape)
+        return function_values.reshape(x.shape)
 
 
-class AffineCorrection(darsia.BaseCorrection):
+class AffineCorrection(darsia.TransformationCorrection):
     """Affine correction based on affine transformation (translation, scaling,
     rotation).
 
@@ -352,11 +319,13 @@ class AffineCorrection(darsia.BaseCorrection):
         self,
         coordinatesystem_src: darsia.CoordinateSystem,
         coordinatesystem_dst: darsia.CoordinateSystem,
-        voxels_src: list[Union[np.ndarray, list[int]]],
-        voxels_dst: list[Union[np.ndarray, list[int]]],
-        isometry: bool = False,
-        use_cartesian: bool = False,
-        **kwargs,
+        pts_src: Union[
+            darsia.CoordinateArray, darsia.VoxelArray, darsia.VoxelCenterArray
+        ],
+        pts_dst: Union[
+            darsia.CoordinateArray, darsia.VoxelArray, darsia.VoxelCenterArray
+        ],
+        fit_options: dict = {},
     ) -> None:
         """Constructor.
 
@@ -365,15 +334,12 @@ class AffineCorrection(darsia.BaseCorrection):
                 to voxels_src
             coordinatesystem_dst (CoordinateSystem): coordinate system corresponding
                 to voxels_dst
-            voxels_src (list): voxel coordinates corresponding to source data; in matrix
-                indexing
-            voxels_dst (list): voxel coordinates corresponding to destination data; use
-                matrix indexing
-            isoemtry (bool): Flag controlling whether the underlying transformation is
-                an isometry.
-            use_cartesian (bool): Flag controlling whether the coordinate transformation
-                uses Cartesian or  voxel coordinates for the actual map; will be set to
-                True if isometry is activated.
+            pts_src (CoordinateArray, VoxelArray, or VoxelCenterArray): source points
+            pts_dst (CoordinateArray, VoxelArray, or VoxelCenterArray): target points
+            fit_options (dict): options for the fit routine
+                isometry (bool): Flag controlling whether the underlying transformation
+                    is an isometry - in this case, the underlying transformation will
+                    operate in the Coordinate space.
 
         """
         # Cache coordinate systems
@@ -383,92 +349,25 @@ class AffineCorrection(darsia.BaseCorrection):
         self.coordinatesystem_dst = coordinatesystem_dst
         """Coordinate system corresponding to the output/target."""
 
-        # For increased robustness, work with centers of voxels
-        dim = self.coordinatesystem_src.dim
-        shifted_voxels_src = np.array(voxels_src) + 0.5 * np.ones(dim)
-        shifted_voxels_dst = np.array(voxels_dst) + 0.5 * np.ones(dim)
-
-        # Construct optimal coordinate transform in the Cartesian coordinate space.
-        # Thus, need to base the construction on the actual relative coordinates.
-        self.use_cartesian = use_cartesian or isometry
-        """Flag controlling whether the coordinate transformation uses Cartesian or
-        voxel coordinates for the actual map. Overwritten if isometry is activated."""
-        if self.use_cartesian:
-            pts_src: np.ndarray[float] = self.coordinatesystem_src.coordinate(
-                shifted_voxels_src
-            )
-            pts_dst: np.ndarray[float] = self.coordinatesystem_dst.coordinate(
-                shifted_voxels_dst
-            )
-        else:
-            pts_src = shifted_voxels_src
-            pts_dst = shifted_voxels_dst
-
         # Fetch additional properties
         assert self.coordinatesystem_src.dim == self.coordinatesystem_dst.dim
         self.dim = self.coordinatesystem_src.dim
         """Dimension of the underlying Euclidean spaces."""
 
-        options = kwargs.get("fit_options", {})
-        self.affine_transformation = AffineTransformation(
-            pts_src,
-            pts_dst,
-            dim=self.dim,
-            isometry=isometry,
-            options=options,
+        # If isometry is turned on, make sure to use the coordinates of voxel centers
+        isometry = fit_options.get("isometry", False)
+        if isometry:
+            pts_src = pts_src.to_voxel_center().to_coordinate(self.coordinatesystem_src)
+            pts_dst = pts_dst.to_voxel_center().to_coordinate(self.coordinatesystem_dst)
+
+        affine_transformation = AffineTransformation(self.dim)
+        affine_transformation.fit(pts_src, pts_dst, fit_options)
+        # print(affine_transformation.translation)
+        # print(affine_transformation.scaling)
+        # print(affine_transformation.rotation)
+
+        super().__init__(
+            coordinatesystem_src=coordinatesystem_src,
+            coordinatesystem_dst=coordinatesystem_dst,
+            transformation=affine_transformation,
         )
-        """Affine coordinate transformation operating between Cartesian spaces."""
-
-    def correct_array(self, array_src: np.ndarray) -> np.ndarray:
-        """Correction routine of array data.
-
-        Args:
-            image_src (np.ndarray): array corresponding to some source image
-
-        Returns:
-            np.ndarray: array corresponding to some destination image
-
-        """
-        # Strategy: Warp entire array by mapping target voxels to destination voxels by
-        # applying the inverse mapping.
-
-        # Collect all target voxels in num_voxels_dst x dim format, and convert to
-        # Cartesian coordinates
-        shape_dst = self.coordinatesystem_dst.shape
-        voxels_dst = np.array(
-            list(itertools.product(*[range(shape_dst[i]) for i in range(self.dim)]))
-        )
-
-        # As in the setup, shift voxels
-        dim = self.coordinatesystem_src.dim
-        shifted_voxels_dst = voxels_dst + 0.5 * np.ones(dim)
-
-        # Find corresponding voxels in the original image by applying the inverse map
-        if self.use_cartesian:
-            coordinates_dst = self.coordinatesystem_dst.coordinate(shifted_voxels_dst)
-            coordinates_src = self.affine_transformation.inverse(coordinates_dst)
-            voxels_src = self.coordinatesystem_src.voxel(coordinates_src)
-        else:
-            voxels_src = np.floor(
-                self.affine_transformation.inverse(shifted_voxels_dst)
-            ).astype(int)
-        num_voxels = len(voxels_src)
-
-        # Determine active voxels - have to lie within active coordinate system
-        shape_src = self.coordinatesystem_src.shape
-        mask = np.all(
-            np.logical_and(
-                0 <= voxels_src,
-                voxels_src <= np.outer(np.ones(num_voxels), np.array(shape_src) - 1),
-            ),
-            axis=1,
-        )
-
-        # Warp. Assign voxel values (no interpolation)
-        shape = *shape_dst, *list(array_src.shape)[self.dim :]
-        array_dst = np.zeros(shape)
-        array_dst[tuple(voxels_dst[mask, j] for j in range(self.dim))] = array_src[
-            tuple(voxels_src[mask, j] for j in range(self.dim))
-        ]
-
-        return array_dst
