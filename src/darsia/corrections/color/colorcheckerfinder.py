e0ab5aad4e8bad54809f729ed970f1738fe17a0d
Jakub Both <jakub.both@uib.no>
2024-01-17 15:07:18 +0100

    DOC: Improve doc

--- src/darsia/corrections/color/colorcheckerfinder.py
+++ src/darsia/corrections/color/colorcheckerfinder.py
@@ -1,3 +1,5 @@
+"""Module for finding a colorchecker in an image."""
+
 from typing import Literal
 
 import numpy as np
@@ -10,7 +12,7 @@ import darsia
 
 
 def find_colorchecker(img: darsia.Image, strategy=Literal["upper_right"]):
-    """Detect colorchecker in upper right corner.
+    """Detect colorchecker in corner.
 
     Search for position and colors.
 
@@ -23,21 +25,24 @@ def find_colorchecker(img: darsia.Image, strategy=Literal["upper_right"]):
         global_voxels (np.ndarray): Voxels of the colorchecker.
 
     """
-    # Preproccess image array
+    # Preproccess image array (required by the algorthms provided by colour-science)
     eotf = darsia.EOTF()
     arr = eotf.adjust(img.img)
     shape = arr.shape
 
     def detect_colorchecker(arr):
         """Colour-based routine to detect the location and swatches of a colorchecker."""
+
+        # Detect swatches using colour science algorithm
         swatches = detect_colour_checkers_segmentation(arr)
         success = len(swatches) > 0
         if success:
-            # TODO Add explanation
+            # Also find position
             detection_data = colour_checkers_coordinates_segmentation(arr, True)
+
+            # Reshape to original image size to retrieve the correct position
             detection_shape = detection_data.segmented_image.shape
             input_shape = arr.shape
-            # min_detection_coarsening_rate = min([input_shape[i] / detection_shape[i] for i in range(2)])
             max_detection_coarsening_rate = max(
                 [input_shape[i] / detection_shape[i] for i in range(2)]
             )
@@ -45,7 +50,7 @@ def find_colorchecker(img: darsia.Image, strategy=Literal["upper_right"]):
             coarse_detection_voxels = darsia.make_voxel(
                 detection_data.colour_checkers[0], matrix_indexing=False
             )
-            # Reshape - use max rate on all - should be min rate for the min values...
+            # Determine the voxels of the colorchecker
             voxels = darsia.make_voxel(
                 max_detection_coarsening_rate * coarse_detection_voxels.copy()
             )
@@ -62,7 +67,7 @@ def find_colorchecker(img: darsia.Image, strategy=Literal["upper_right"]):
     else:
         raise NotImplementedError
 
-    # Iterate
+    # Iterate by decreasing a window for searching the colorchecker
     success = False
     current = 1
     while not success:
@@ -73,6 +78,7 @@ def find_colorchecker(img: darsia.Image, strategy=Literal["upper_right"]):
             slice(*np.sort([current_corner[0], target_corner[0]]).astype(int)),
             slice(*np.sort([current_corner[1], target_corner[1]]).astype(int)),
         )
+
         # Do not use swatches as detected by colour, just use the location.
         success, _, local_voxels = detect_colorchecker(arr[roi])
         if success:
@@ -81,7 +87,7 @@ def find_colorchecker(img: darsia.Image, strategy=Literal["upper_right"]):
         if current < update**20:
             assert False, "No color checker found."
 
-    # Extract the color checker
+    # Extract the color checker directly from the image (more robust than colour)
     img_cc = img.subregion(voxels=roi).subregion(voxels=local_voxels)
     colorchecker = darsia.CustomColorChecker(image=img_cc.img)
 
