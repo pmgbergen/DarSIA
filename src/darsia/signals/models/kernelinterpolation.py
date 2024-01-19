d3cc3d3967044c77c2055fa2337a47b9a1bc316a
Jakub Both <jakub.both@uib.no>
2024-01-19 22:30:43 +0100

    MAINT: Make kernel interpolation a model, make names more general, and reduce linear problem if needed

--- src/darsia/signals/models/kernelinterpolation.py
+++ src/darsia/signals/models/kernelinterpolation.py
@@ -3,7 +3,7 @@
 The kernel-based interpolation is a general interpolation method that can be used
 for different interpolation tasks. The interpolation is based on a kernel matrix
 that is computed from the input data. The input data consists of a set of data
-points (colours) and a set of goal points (concentrations). The kernel matrix is
+points (supports) and a set of goal points (values). The kernel matrix is
 then used to compute the interpolation weights. The interpolation weights are
 computed by solving a linear system of equations. The interpolation weights are
 then used to compute the interpolated image.
@@ -11,52 +11,140 @@ then used to compute the interpolated image.
 One example use case is the concentration analysis.
 
 """
+from typing import Literal, Optional, Union
+from warnings import warn
 
 import numpy as np
 
 import darsia
 
 
-class KernelInterpolation:
+class KernelInterpolation(darsia.Model):
     """General kernel-based interpolation."""
 
     def __init__(
-        self, kernel: darsia.BaseKernel, colours: np.ndarray, concentrations: np.ndarray
+        self,
+        kernel: darsia.BaseKernel,
+        supports: Optional[np.ndarray] = None,
+        values: Optional[np.ndarray] = None,
     ):
         """Setup of the kernel-based interpolation.
 
         Args:
             kernel (darsia.BaseKernel): kernel
-            colours (np.ndarray): colours (support points)
-            concentrations (np.ndarray): concentrations (goal points)
+            supports (np.ndarray): support points
+            values (np.ndarray): goal points
 
         Raises:
             AssertionError: if the input data is not compatible, i.e., the number of
-                colours and concentrations does not match.
+                supports and values does not match.
 
         """
-
         self.kernel = kernel
         """Kernel."""
-
-        num_data = colours.shape[0]
-        assert len(concentrations) == num_data, "Input data not compatible."
-
-        x = np.array(colours)  # data points / control points / support points
-        y = np.array(concentrations)  # goal points
-        X = np.ones((num_data, num_data))  # kernel matrix
-        for i in range(num_data):
-            for j in range(num_data):
-                X[i, j] = self.kernel(x[i], x[j])
-
-        alpha = np.linalg.solve(X, y)
-
-        # Cache
-        self.x = x
+        self.supports = supports
         """Support points."""
-
-        self.alpha = alpha
+        self.values = values
+        """Goal points."""
+        self.num_supports = 0
+        """Number of support points."""
+        self.interpolation_weights = None
         """Interpolation weights."""
+        self.update(kernel=kernel, supports=supports, values=values)
+
+    def update(
+        self,
+        kernel: Optional[darsia.BaseKernel] = None,
+        supports: Optional[np.ndarray] = None,
+        values: Optional[np.ndarray] = None,
+    ) -> None:
+        """Update kernel and interpolation weights.
+
+        Args:
+            kernel (darsia.BaseKernel, optional): kernel
+            supports (np.ndarray, optional): support points
+            values (np.ndarray, optional): goal points
+
+        """
+        # Convert to arrays if necessary
+        if isinstance(supports, list):
+            supports = np.array(supports)
+        if isinstance(values, list):
+            values = np.array(values)
+
+        # Update single components of the interpolation
+        if kernel is not None:
+            self.update_kernel(kernel)
+        if supports is not None:
+            self.supports = supports
+            self.num_supports = self.supports.shape[0]
+        if values is not None:
+            self.values = values
+        if self.supports is not None and self.values is not None:
+            self.update_interpolation()
+        else:
+            warn("No input data given. Interpolation weights not updated.")
+
+    def update_kernel(self, kernel: darsia.BaseKernel) -> None:
+        """Update kernel.
+
+        Args:
+            kernel (darsia.BaseKernel): kernel
+
+        """
+        self.kernel = kernel
+
+    def update_interpolation(self) -> None:
+        """Update interpolation weights.
+
+        Raises:
+            AssertionError: if the input data is not compatible, i.e., the number of
+                supports and values does not match.
+
+        """
+        assert len(self.values) == self.num_supports, "Input data not compatible."
+
+        # Reduce to unique supports for unique solvability
+        self.supports, indices, counts = np.unique(
+            np.round(self.supports, decimals=5),
+            return_index=True,
+            return_counts=True,
+            axis=0,
+        )
+        # Warn the user that some supports were removed
+        if not np.allclose(counts, 1):
+            warn("Supports are not unique. Some supports were removed.")
+        # Adapt the remaining values
+        self.num_supports = self.supports.shape[0]
+        self.values = self.values[indices]
+
+        # Solve the interpolation problem, i.e., set up the kernel matrix, and solve
+        # for the interpolation weights
+        X = np.ones((self.num_supports, self.num_supports))  # kernel matrix
+        for i in range(self.num_supports):
+            for j in range(self.num_supports):
+                X[i, j] = self.kernel(self.supports[i], self.supports[j])
+        self.interpolation_weights = np.linalg.solve(X, self.values)
+
+    def update_model_parameters(
+        self,
+        parameters: np.ndarray,
+        dofs: Optional[Union[list[Literal["kernel", "values"]], Literal["all"]]] = None,
+    ) -> None:
+        if "supports" in dofs:
+            raise ValueError("Supports cannot be updated. Use update method.")
+
+        if dofs is None or dofs == "all" or set(dofs) == set(["kernel", "values"]):
+            self.update(
+                kernel=parameters[0],
+                values=parameters[1 : 1 + self.num_supports],
+            )
+        elif set(dofs) == set(["kernel"]):
+            self.update(kernel=parameters[0])
+        elif set(dofs) == set(["values"]):
+            self.update(values=parameters[: self.num_supports])
+        else:
+            raise ValueError("Invalid dofs.")
 
     def __call__(self, signal: np.ndarray) -> np.ndarray:
         """Apply interpolation.
@@ -68,7 +156,9 @@ class KernelInterpolation:
             np.ndarray: interpolated signal
 
         """
-        ph_image = np.zeros(signal.shape[:2])
-        for n in range(self.alpha.shape[0]):
-            ph_image += self.alpha[n] * self.kernel(signal, self.x[n])
-        return ph_image
+        output = np.zeros(signal.shape[:2])
+        for n in range(self.num_supports):
+            output += self.interpolation_weights[n] * self.kernel(
+                signal, self.supports[n]
+            )
+        return output
