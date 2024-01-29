1ccb3c8ec59138d3104a460abf8d5f7fff3f4cf1
Jakub Both <jakub.both@uib.no>
2024-01-29 10:36:40 +0100

    ENH: Add fit for affine balance; and shortcuts

--- src/darsia/corrections/color/colorbalance.py
+++ src/darsia/corrections/color/colorbalance.py
@@ -3,8 +3,7 @@
 from abc import ABC, abstractmethod
 
 import numpy as np
-
-import darsia
+import scipy.optimize
 
 
 class BaseBalance(ABC):
@@ -60,7 +59,7 @@ class BaseBalance(ABC):
 
 
 class ColorBalance(BaseBalance):
-    """Class for finding and applying a color balance."""
+    """Class for finding and applying a linear color balance."""
 
     def __init__(self) -> None:
         self.balance: np.ndarray = np.eye(3)
@@ -89,7 +88,7 @@ class ColorBalance(BaseBalance):
             swatches_src_balanced = np.dot(swatches_src, balance)
             return np.sum((swatches_src_balanced - swatches_dst) ** 2)
 
-        opt_result = darsia.optimize.minimize(
+        opt_result = scipy.optimize.minimize(
             objective_funcion,
             self.balance.flatten(),
             method="Powell",
@@ -101,7 +100,7 @@ class ColorBalance(BaseBalance):
 
 
 class WhiteBalance(BaseBalance):
-    """Class for finding and applying a white balance."""
+    """Class for finding and applying a diagional linear balance."""
 
     def __init__(self) -> None:
         self.balance: np.ndarray = np.diag(np.ones(3))
@@ -130,7 +129,7 @@ class WhiteBalance(BaseBalance):
             swatches_src_balanced = np.dot(swatches_src, balance)
             return np.sum((swatches_src_balanced - swatches_dst) ** 2)
 
-        opt_result = darsia.optimize.minimize(
+        opt_result = scipy.optimize.minimize(
             objective_funcion,
             np.diag(self.balance),
             method="Powell",
@@ -139,3 +138,121 @@ class WhiteBalance(BaseBalance):
         )
 
         self.balance = np.diag(opt_result.x)
+
+
+class AffineBalance(BaseBalance):
+    """Class for finding and applying a general affine balance."""
+
+    def __init__(self) -> None:
+        self.balance_scaling: np.ndarray = np.eye(3)
+        """Balance scaling matrix."""
+        self.balance_translation: np.ndarray = np.zeros(3)
+        """Balance translation vector."""
+
+    def find_balance(self, swatches_src: np.ndarray, swatches_dst) -> None:
+        """Find the color balance of an image.
+
+        Args:
+            swatches_src (np.ndarray): Source swatches.
+            swatches_dst (np.ndarray): Destination swatches.
+
+        """
+
+        def objective_funcion(flat_balance: np.ndarray) -> float:
+            """Objective function for the minimization.
+
+            Args:
+                flat_balance (np.ndarray): Flat color balance matrix.
+
+            Returns:
+                float: Objective function value.
+
+            """
+            balance_scaling = flat_balance[:9].reshape((3, 3))
+            balance_translation = flat_balance[9:12]
+            swatches_src_balanced = (
+                np.dot(swatches_src, balance_scaling) + balance_translation
+            )
+            return np.sum((swatches_src_balanced - swatches_dst) ** 2)
+
+        opt_result = scipy.optimize.minimize(
+            objective_funcion,
+            np.concatenate((self.balance_scaling.flatten(), self.balance_translation)),
+            method="Powell",
+            tol=1e-6,
+            options={"maxiter": 1000, "disp": False},
+        )
+
+        self.balance_scaling = opt_result.x[:9].reshape((3, 3))
+        self.balance_translation = opt_result.x[9:12]
+
+    def apply_balance(self, img: np.ndarray) -> np.ndarray:
+        """Apply the color balance to an image.
+
+        Args:
+            img (np.ndarray): Image to apply the color balance to.
+
+        Returns:
+            balanced_img (np.ndarray): Balanced image.
+
+        """
+        balanced_img = np.dot(img, self.balance_scaling) + self.balance_translation
+        balanced_img = np.clip(balanced_img, 0, 1)
+        return balanced_img
+
+
+# ! ---- Shortcut functions ---- ! #
+
+
+def color_balance(
+    img: np.ndarray, swatches_src: np.ndarray, swatches_dst
+) -> np.ndarray:
+    """Apply the color balance to an image.
+
+    Args:
+        img (np.ndarray): Image to apply the color balance to.
+        swatches_src (np.ndarray): Source swatches.
+        swatches_dst (np.ndarray): Destination swatches.
+
+    Returns:
+        balanced_img (np.ndarray): Balanced image.
+
+    """
+    cb = ColorBalance()
+    return cb(img, swatches_src, swatches_dst)
+
+
+def white_balance(
+    img: np.ndarray, swatches_src: np.ndarray, swatches_dst
+) -> np.ndarray:
+    """Apply the color balance to an image.
+
+    Args:
+        img (np.ndarray): Image to apply the color balance to.
+        swatches_src (np.ndarray): Source swatches.
+        swatches_dst (np.ndarray): Destination swatches.
+
+    Returns:
+        balanced_img (np.ndarray): Balanced image.
+
+    """
+    wb = WhiteBalance()
+    return wb(img, swatches_src, swatches_dst)
+
+
+def affine_balance(
+    img: np.ndarray, swatches_src: np.ndarray, swatches_dst
+) -> np.ndarray:
+    """Apply the color balance to an image.
+
+    Args:
+        img (np.ndarray): Image to apply the color balance to.
+        swatches_src (np.ndarray): Source swatches.
+        swatches_dst (np.ndarray): Destination swatches.
+
+    Returns:
+        balanced_img (np.ndarray): Balanced image.
+
+    """
+    ab = AffineBalance()
+    return ab(img, swatches_src, swatches_dst)
