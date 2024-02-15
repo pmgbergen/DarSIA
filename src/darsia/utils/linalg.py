8feb034f6b52d7b955d7c4b92e341937753dae14
enrico facca <enrico.facca@gmail.com>
2024-02-15 18:25:21 +0100

    passing ises and kernel to linear solver

--- src/darsia/utils/linalg.py
+++ src/darsia/utils/linalg.py
@@ -4,6 +4,7 @@ import numpy as np
 import scipy.sparse as sps
 from scipy.sparse.linalg import cg, gmres
 from petsc4py import PETSc
+import matplotlib.pyplot as plt
 
 
 class CG:
@@ -23,7 +24,9 @@ class GMRES:
 
 
 class KSP:
-    def __init__(self, A: sps.csc_matrix) -> None:
+    def __init__(self, A: sps.csc_matrix, 
+                 field_ises=None, 
+                 nullspace=None) -> None:
         # store original matrix
         self.A = A
 
@@ -32,9 +35,28 @@ class KSP:
             size=A.shape, csr=(A.indptr, A.indices, A.data)
         )
 
+        # set nullspace
+        if nullspace is not None:
+            # convert to petsc vectors
+            self._comm = PETSc.COMM_WORLD
+            for i, v in enumerate(nullspace):
+                self._petsc_kernels[i] = PETSc.Vec().createWithArray(v, comm=self._comm)
+            self._nullspace = PETSc.NullSpace().create(constant=None,
+                                                   vectors=self._petsc_kernels,
+                                                   comm=self._comm)
+            self.A_petsc.setNullSpace(self._nullspace)
+
         # preallocate petsc vectors
         self.sol_petsc = self.A_petsc.createVecLeft()
         self.rhs_petsc = self.A_petsc.createVecRight()
+
+        # set field_ises
+        if field_ises is not None:
+            self.field_ises = field_ises
+        else:
+            self.field_ises = None
+
+        
 
     def setup(self, petsc_options: dict) -> None:
         self.prefix = "petsc_solver_"
@@ -66,12 +88,31 @@ class KSP:
         self.rhs_petsc.setOptionsPrefix(self.prefix)
         self.rhs_petsc.setFromOptions()
 
+        # TODO: set field split in __init__
+        if self.field_ises is not None: 
+            pc = ksp.getPC()
+            pc.setFromOptions()
+            #pc.setFieldSplitIS(('0',self.pot_is),('1',self.tdens_is))
+            pc.setFieldSplitIS(*self.field_ises)
+            pc.setOptionsPrefix(self.prefix)
+            pc.setFromOptions()
+
     def solve(self, b: np.ndarray, **kwargs) -> np.ndarray:
         # convert to petsc vector the rhs
         self.rhs_petsc.setArray(b)
         self.ksp.solve(self.rhs_petsc, self.sol_petsc)
         sol = self.sol_petsc.getArray()
+
+        # solve using scipy
+        #sol2, _ = gmres(self.A, b)
+        #res = self.A.dot(sol2) - b
+        #print('residual norm: ', np.linalg.norm(res)/np.linalg.norm(b))
+
         # DEBUG CODE: check convergence
-        # res = self.A.dot(sol) - b
-        # print('residual norm: ', np.linalg.norm(res)/np.linalg.norm(b))
+        #res = self.A.dot(sol) - b
+        #print('residual norm: ', np.linalg.norm(res)/np.linalg.norm(b))
+        # plot residual
+        #plt.plot(sol)
+        #plt.plot(sol2, linestyle='--')
+        #plt.show()
         return sol
