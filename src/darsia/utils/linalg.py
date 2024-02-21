c220d18a356aca1477ad472ffd572893e8cfd9d1
enrico facca <enrico.facca@gmail.com>
2024-02-21 12:18:47 +0100

    passing kernel info

--- src/darsia/utils/linalg.py
+++ src/darsia/utils/linalg.py
@@ -39,8 +39,10 @@ class KSP:
         if nullspace is not None:
             # convert to petsc vectors
             self._comm = PETSc.COMM_WORLD
-            for i, v in enumerate(nullspace):
-                self._petsc_kernels[i] = PETSc.Vec().createWithArray(v, comm=self._comm)
+            self._petsc_kernels = []
+            for v in nullspace:
+                p_vec = PETSc.Vec().createWithArray(v, comm=self._comm)
+                self._petsc_kernels.append(p_vec)
             self._nullspace = PETSc.NullSpace().create(constant=None,
                                                    vectors=self._petsc_kernels,
                                                    comm=self._comm)
@@ -59,6 +61,9 @@ class KSP:
         
 
     def setup(self, petsc_options: dict) -> None:
+        petsc_options = flatten_parameters(petsc_options)
+
+
         self.prefix = "petsc_solver_"
         # TODO: define a unique name in case of multiple problems
 
@@ -96,6 +101,20 @@ class KSP:
             pc.setFieldSplitIS(*self.field_ises)
             pc.setOptionsPrefix(self.prefix)
             pc.setFromOptions()
+            pc.setUp()
+            if self._nullspace is not None:
+                if len(self._petsc_kernels) > 1:
+                        raise NotImplementedError("Nullspace currently works with one kernel only")
+                # split subproblems
+                ksps = pc.getFieldSplitSubKSP()
+                # assign nullspace to each subproblem
+                for i, local_ksp in enumerate(ksps):
+                    for k in self._petsc_kernels:
+                        sub_vec = k.getSubVector(self.field_ises[i][1])
+                        if sub_vec.norm() > 0:
+                            local_nullspace = PETSc.NullSpace().create(constant=False, vectors=[sub_vec])
+                            A_i, _ = local_ksp.getOperators()
+                            A_i.setNullSpace(local_nullspace)
 
     def solve(self, b: np.ndarray, **kwargs) -> np.ndarray:
         # convert to petsc vector the rhs
@@ -109,10 +128,74 @@ class KSP:
         #print('residual norm: ', np.linalg.norm(res)/np.linalg.norm(b))
 
         # DEBUG CODE: check convergence
-        #res = self.A.dot(sol) - b
+        res = self.A.dot(sol) - b
         #print('residual norm: ', np.linalg.norm(res)/np.linalg.norm(b))
-        # plot residual
-        #plt.plot(sol)
-        #plt.plot(sol2, linestyle='--')
-        #plt.show()
         return sol
+
+#
+# following code is taken from firedrake
+#           
+def flatten_parameters(parameters, sep="_"):
+    """Flatten a nested parameters dict, joining keys with sep.
+
+    :arg parameters: a dict to flatten.
+    :arg sep: separator of keys.
+
+    Used to flatten parameter dictionaries with nested structure to a
+    flat dict suitable to pass to PETSc.  For example:
+
+    .. code-block:: python3
+
+       flatten_parameters({"a": {"b": {"c": 4}, "d": 2}, "e": 1}, sep="_")
+       => {"a_b_c": 4, "a_d": 2, "e": 1}
+
+    If a "prefix" key already ends with the provided separator, then
+    it is not used to concatenate the keys.  Hence:
+
+    .. code-block:: python3
+
+       flatten_parameters({"a_": {"b": {"c": 4}, "d": 2}, "e": 1}, sep="_")
+       => {"a_b_c": 4, "a_d": 2, "e": 1}
+       # rather than
+       => {"a__b_c": 4, "a__d": 2, "e": 1}
+    """
+    new = type(parameters)()
+
+    if not len(parameters):
+        return new
+
+    def flatten(parameters, *prefixes):
+        """Iterate over nested dicts, yielding (*keys, value) pairs."""
+        sentinel = object()
+        try:
+            option = sentinel
+            for option, value in parameters.items():
+                # Recurse into values to flatten any dicts.
+                for pair in flatten(value, option, *prefixes):
+                    yield pair
+            # Make sure zero-length dicts come back.
+            if option is sentinel:
+                yield (prefixes, parameters)
+        except AttributeError:
+            # Non dict values are just returned.
+            yield (prefixes, parameters)
+
+    def munge(keys):
+        """Ensure that each intermediate key in keys ends in sep.
+
+        Also, reverse the list."""
+        for key in reversed(keys[1:]):
+            if len(key) and not key.endswith(sep):
+                yield key + sep
+            else:
+                yield key
+        else:
+            yield keys[0]
+
+    for keys, value in flatten(parameters):
+        option = "".join(map(str, munge(keys)))
+        if option in new:
+            print("Ignoring duplicate option: %s (existing value %s, new value %s)",
+                    option, new[option], value)
+        new[option] = value
+    return new
