889f101fe9b50b1f60a5b7f98e7bf952165f9b45
Jakub Both <jakub.both@uib.no>
2024-03-02 17:09:11 +0100

    ENH: Extend the base model to labeled media.

--- src/darsia/signals/models/basemodel.py
+++ src/darsia/signals/models/basemodel.py
@@ -4,9 +4,12 @@ Models convert signals to data.
 
 """
 import abc
+import copy
 from typing import Literal, Optional, Union
 
 import numpy as np
+
+import darsia
 
 
 class Model:
@@ -34,3 +37,21 @@ class Model:
         Update routine of model parameters.
         """
         pass
+
+
+class HeterogeneousModel(Model):
+    def __init__(self, obj: Union[Model, list[Model]], labels: darsia.Image):
+
+        self.masks = darsia.Masks(labels)
+        self.obj = {}
+        for i in range(self.masks.size):
+            self.obj[i] = copy.copy(obj)
+
+    def __call__(self, signal: np.ndarray) -> np.ndarray:
+        output = np.zeros(signal.shape[:2])  # TODO shape?
+        for i, mask in enumerate(self.masks):
+            output[mask.img] = self[i](signal[mask.img])
+        return output
+
+    def __getitem__(self, key):
+        return self.obj[key]
