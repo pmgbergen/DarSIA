6ebe4e68905a955c3eae3760dbe01b8111bfce9e
Jakub Both <jakub.both@uib.no>
2024-03-04 22:07:39 +0100

    MAINT: Optimize calculations and decrease coputational expense by 40%

--- src/darsia/utils/linear_solvers/jacobi.py
+++ src/darsia/utils/linear_solvers/jacobi.py
@@ -38,11 +38,18 @@ class Jacobi(da.Solver):
         """
         im_av: np.ndarray = np.zeros_like(im)
         for ax in range(im.ndim):
-            im_av += np.concatenate(
-                (da.array_slice(im, ax, 0, 1), da.array_slice(im, ax, 0, -1)), axis=ax
-            ) + np.concatenate(
-                (da.array_slice(im, ax, 1, None), da.array_slice(im, ax, -1, None)),
-                axis=ax,
+            # Use ghost copy from outside
+            im_av[da.array_slice_argument(im, ax, 0, 1)] += da.array_slice(im, ax, 0, 1)
+            # 'Left' and 'right' copies in the interior
+            im_av[da.array_slice_argument(im, ax, 1, None)] += da.array_slice(
+                im, ax, 0, -1
+            )
+            im_av[da.array_slice_argument(im, ax, 0, -1)] += da.array_slice(
+                im, ax, 1, None
+            )
+            # Use ghost copy from outside
+            im_av[da.array_slice_argument(im, ax, -1, None)] += da.array_slice(
+                im, ax, -1, None
             )
         return im_av
 
@@ -77,26 +84,41 @@ class Jacobi(da.Solver):
             h (float): mesh diameter
 
         Returns:
-            np.ndarray: solution to the linear system
+            np.ndarray: approximation to the linear system
 
         """
         x: np.ndarray = x0
 
-        diag = self._diag(h)
+        # Main idea - Iteration is performed in non-residual form:
+        #   x_new = np.divide(
+        #       rhs + self.diffusion_coeff * self._neighbor_accumulation(x) / h**2,
+        #       diag
+        #   )
+        # To reduce computations, the expression is mildly optimized.
+
+        # Precompute constant expressions
+        if not hasattr(self, "const_diag"):
+            self.const_diag = self._diag(h)
+        if not hasattr(self, "const_diag_scaled"):
+            self.const_diag_scaled = np.divide(
+                self.const_diag, self.diffusion_coeff / h**2
+            )
+        rhs_scaled = np.divide(rhs, self.const_diag)
 
         # Split the tolerance based part to avoid unnecessary boolean evaluation
         if self.tol is None:
             for _ in range(self.maxiter):
-                x = (
-                    rhs + self.diffusion_coeff * self._neighbor_accumulation(x) / h**2
-                ) / diag
+                x = rhs_scaled + np.divide(
+                    self._neighbor_accumulation(x), self.const_diag_scaled
+                )
                 if self.verbose:
                     print(f"Jacobi iteration {_} of {self.maxiter} completed.")
+
         else:
             for _ in range(self.maxiter):
-                x_new = (
-                    rhs + self.diffusion_coeff * self._neighbor_accumulation(x) / h**2
-                ) / diag
+                x_new = rhs_scaled + np.divide(
+                    self._neighbor_accumulation(x), self.const_diag_scaled
+                )
                 err = np.linalg.norm(x_new - x) / np.linalg.norm(x0)
                 if err < self.tol:
                     break
