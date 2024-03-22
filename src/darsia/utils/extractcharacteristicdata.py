621261e93f10ccc16fda1e833fccb8bd2ce9b9c7
Jakub Both <jakub.both@uib.no>
2024-03-22 11:47:48 +0100

    ENH: Allow masks for extrating characteristic data.

--- src/darsia/utils/extractcharacteristicdata.py
+++ src/darsia/utils/extractcharacteristicdata.py
@@ -1,6 +1,7 @@
 """Module to extract characteristic data from input image for given patches."""
 
 import string
+from typing import Optional
 from warnings import warn
 
 import cv2
@@ -11,7 +12,8 @@ import numpy as np
 
 def extract_characteristic_data(
     signal: np.ndarray,
-    samples: list[tuple[slice]],
+    mask: Optional[np.ndarray] = None,
+    samples: Optional[list[tuple[slice]]] = None,
     filter: callable = lambda x: x,
     num_clusters: int = 5,
     num_attempts: int = 100,
@@ -24,7 +26,10 @@ def extract_characteristic_data(
     Args:
         signal (np.ndarray): input signal, assumed to have the structure of a 2d,
             colored image.
-        samples (list of slices): list of 2d regions of interest
+        mask (np.ndarray): boolean array flagging pixels of interest; by default all pixels
+            considered.
+        samples (list of slices): list of 2d regions of interest; if None, full region
+            considered
         filter (callable): function to preprocess the signal before analysis, e.g.,
             Gaussian filter.
         num_clusters (int): number of clusters to be extracted from data.
@@ -37,6 +42,10 @@ def extract_characteristic_data(
         np.ndarray: characteristic colors for chosen samples.
 
     """
+    # Define default inputs if not provided
+    if samples is None:
+        samples = [(slice(0,None), slice(0, None))]
+
     # Init data vector
     data_dim = signal.shape[-1]
     if data_dim not in [1, 3]:
@@ -55,7 +64,7 @@ def extract_characteristic_data(
         ax.set_xlabel("horizontal pixel")
         ax.set_ylabel("vertical pixel")
 
-    # Preprocess signal
+    # Preprocess signal - TODO obsolete
     if False:
         signal = filter(signal).copy()
 
@@ -64,12 +73,18 @@ def extract_characteristic_data(
         # Control patch dimension
         assert len(p) == 2, "Patch must be 2d"
 
-        # Cluster analysis for extractin dominant data/colors
+        # Cluster analysis for extracting dominant data/colors
         if False:
             patch = signal[p]
         else:
             patch = filter(signal[p])
         flat_image = np.reshape(patch, (-1, data_dim))
+
+        # Reduce to active pixels
+        if mask is not None:
+            flat_mask = np.ravel(mask[p])
+            flat_image = flat_image[flat_mask]
+
         pixels = np.float32(flat_image)
         criteria = (
             cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER,
@@ -85,37 +100,48 @@ def extract_characteristic_data(
 
         # Visualise patches on image
         if show_plot:
-            rect = patches.Rectangle(
-                (p[1].start, p[0].start),
-                p[1].stop - p[1].start,
-                p[0].stop - p[0].start,
-                linewidth=1,
-                edgecolor="w",
-                facecolor="none",
-            )
-            ax.text(
-                int(p[1].start + 0.05 * (p[1].stop - p[1].start)),
-                int(p[0].start + 0.95 * (p[0].stop - p[0].start)),
-                letters[i],
-                fontsize=12,
-                color="white",
-            )
-            ax.add_patch(rect)
+            if None not in [p[0].start, p[0].stop, p[1].start, p[1].stop]:
+                rect = patches.Rectangle(
+                    (p[1].start, p[0].start),
+                    p[1].stop - p[1].start,
+                    p[0].stop - p[0].start,
+                    # linewidth=1,
+                    # edgecolor="w",
+                    linewidth=2,
+                    edgecolor=[1, 100.0 / 255.0, 160.0 / 255.0],
+                    facecolor="none",
+                )
+                # ax.text(
+                #    int(p[1].start + 0.05 * (p[1].stop - p[1].start)),
+                #    int(p[0].start + 0.95 * (p[0].stop - p[0].start)),
+                #    letters[i],
+                #    fontsize=12,
+                #    color="white",
+                # )
+                ax.add_patch(rect)
 
     if show_plot:
+
+        if mask is not None:
+            plt.imshow(mask, alpha = 0.2)
+
         if data_dim == 3:
             warn("Assuming data is color data and using RGB as axes.")
-            c = np.abs(data_clusters)
+            c = np.clip(np.abs(data_clusters), 0, 1)
             plt.figure("Relative dominant colors")
             ax = plt.axes(projection="3d")
             ax.set_xlabel("R")
             ax.set_ylabel("G")
             ax.set_zlabel("B")
+            # ax.set_xlim(0.57, 0.88)
+            # ax.set_ylim(0.42, 0.78)
+            # ax.set_zlim(0.17, 0.53)
             ax.scatter(
                 data_clusters[:, 0], data_clusters[:, 1], data_clusters[:, 2], c=c
             )
-            for i, c in enumerate(data_clusters):
-                ax.text(c[0], c[1], c[2], letters[i])
+            #            for i, c in enumerate(data_clusters):
+            #                ax.text(c[0], c[1], c[2], letters[i])
+            # plt.savefig("rgb_cube.png", dpi=800, transparent=True)
         plt.show()
 
     return data_clusters
