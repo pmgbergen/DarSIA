a128eb981fc33b729b1d33618322189383cf8a6d
Jakub Both <jakub.both@uib.no>
2024-04-18 00:31:54 +0200

    ENH: Provide linear combination of kernel applications and numba implementation for gaussian kernels

--- src/darsia/utils/kernels.py
+++ src/darsia/utils/kernels.py
@@ -8,6 +8,7 @@ Provided:
 
 from abc import ABC, abstractmethod
 
+import numba
 import numpy as np
 
 
@@ -27,6 +28,23 @@ class BaseKernel(ABC):
 
         """
         pass
+
+    def linear_combination(self, signal: np.ndarray, supports, interpolation_weights):
+        num_supports = len(supports)
+        if num_supports == 0:
+            return np.zeros_like(signal)
+        # NOTE: Shape is not clear at input as it may be used via advenced indexing
+        from time import time
+
+        tic = time()
+        output = interpolation_weights[0] * self.__call__(signal, supports[0])
+        print("interpolation call", time() - tic)
+        for n in range(1, num_supports):
+            tic = time()
+            output += interpolation_weights[n] * self.__call__(signal, supports[n])
+            print("interpolation call", time() - tic)
+
+        return output
 
 
 class LinearKernel(BaseKernel):
@@ -72,3 +90,25 @@ class GaussianKernel(BaseKernel):
             np.ndarray: kernel between x and y
         """
         return np.exp(-self.gamma * np.sum(np.multiply(x - y, x - y), axis=-1))
+
+    def linear_combination(self, signal: np.ndarray, supports, interpolation_weights):
+        # Make numba version from linear_combination for GaussianKernel
+        @numba.jit(nopython=True, parallel=True, fastmath=True, cache=True)
+        def _linear_combination_numba(
+            signal: np.ndarray, supports, interpolation_weights, gamma
+        ):
+            num_supports = len(supports)
+            diff = signal - supports[0]
+            output = interpolation_weights[0] * np.exp(
+                -gamma * np.sum(np.multiply(diff, diff), axis=-1)
+            )
+            for n in range(1, num_supports):
+                diff = signal - supports[n]
+                output += interpolation_weights[n] * np.exp(
+                    -gamma * np.sum(np.multiply(diff, diff), axis=-1)
+                )
+            return output
+
+        return _linear_combination_numba(
+            signal, supports, interpolation_weights, self.gamma
+        )
