2584a941ec7d7df499826d629f11614fc93f9663
Jakub Both <jakub.both@uib.no>
2024-06-15 11:20:12 +0200

    ENH: Add approximations spaces and linear approximation space.

--- src/darsia/__init__.py
+++ src/darsia/__init__.py
@@ -37,6 +37,7 @@ from darsia.utils import quadrature
 from darsia.utils import plotting
 from darsia.utils.detection import *
 from darsia.utils.standard_images import *
+from darsia.utils.approximations import *
 from darsia.image.subregions import *
 from darsia.corrections.basecorrection import *
 from darsia.corrections.typecorrection import *
