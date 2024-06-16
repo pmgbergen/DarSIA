a38a306d9c0ecd2bd0a40cc54397af86fd1cca32
Jakub Both <jakub.both@uib.no>
2024-06-17 01:14:38 +0200

    MAINT: Overhaul of simple fluidflower - add type and relative color correction - and major clean up

--- src/darsia/presets/fluidflower/simplefluidflower.py
+++ src/darsia/presets/fluidflower/simplefluidflower.py
@@ -31,11 +31,14 @@ class SimpleFluidFlower:
         self,
         baseline: Path,
         active_corrections: list[
-            Literal["drift", "curvature", "illumination", "color"]
+            Literal[
+                "type", "drift", "curvature", "illumination", "relative-color", "color"
+            ]
         ] = [
+            "type",
             "drift",
             "curvature",
-            "illumination",
+            "relative-color",
             "color",
         ],
         debug: bool = False,
@@ -68,11 +71,17 @@ class SimpleFluidFlower:
         self.debug = debug
         """Flag for debugging."""
 
+        self.active_type_correction = "type" in active_corrections
+        """Flag for type correction."""
+
         self.active_drift_correction = "drift" in active_corrections
         """Flag for drift correction."""
 
         self.active_curvature_correction = "curvature" in active_corrections
         """Flag for curvature correction."""
+
+        self.active_relative_color_correction = "relative-color" in active_corrections
+        """Flag for relative color correction."""
 
         self.active_illumination_correction = "illumination" in active_corrections
         """Flag for illumination correction."""
@@ -82,19 +91,30 @@ class SimpleFluidFlower:
 
     def setup(
         self,
-        roi: Path,
-        segmentation: Path,
-        color_checker_position: darsia.ColorCheckerPosition,
         specs: dict,
-        **kwargs,
+        segmentation: Optional[Path] = None,
+        curvature_options: Optional[dict] = None,
+        relative_color_options: Optional[dict] = None,
+        illumination_options: Optional[dict] = None,
     ) -> None:
         """Setup Table top based on characteristic input image (preferably the baseline).
 
         Args:
-            roi (Path): path to image with ROI
+            specs (dict): specifications of the FluidFlower, includes:
+                - width (float): width of the ROI
+                - height (float): height of the ROI
+                - depth (float): depth of the porous medium
+                - porosity (float): porosity of the porous medium
+                - colorchecker_position (str): position of the color checker
             segmentation (Path): path to segmentation image
-            color_checker_position (ColorCheckerPosition): position of the color checker
-            specs (dict): specifications of the tabletop
+            curvature_options (dict): options for curvature correction, includes:
+                - roi (Path): path to image with ROI
+                - roi_mode (str): mode for ROI selection
+                - roi_color (list[float]): color of the ROI, only used in automatic mode
+            relative_color_options (dict): options for relative color correction, includes:
+                - images (list[Path]): list of paths to images
+            illumination_options (dict): options for illumination correction, includes:
+                - illumination_mode (str): mode for illumination
 
         """
 
@@ -103,30 +123,58 @@ class SimpleFluidFlower:
         self.height = specs.get("height", 0.55)
         self.depth = specs.get("depth", 0.012)
         self.porosity = specs.get("porosity", 0.44)
+        self.colorchecker_position = specs.get("colorchecker_position", "upper_right")
 
         # ! ---- INITIALIZATION ----
         self.corrections = []
+        self.baseline = self.raw_baseline.copy()
+
+        # ! ---- SETUP TYPE CORRECTION/CONVERSION ----
+        if self.active_type_correction:
+            self.type_conversion = darsia.TypeCorrection(np.float64)
+            self.corrections.append(self.type_conversion)
+            self.baseline = self.type_conversion(self.baseline)
 
         # ! ---- SETUP SHAPE CORRECTION ----
 
-        if self.active_drift_correction or self.active_curvature_correction:
-            roi_mode = kwargs.get("roi_mode", "interactive")
-            roi_color = kwargs.get("roi_color")
-            self.drift_correction, self.curvature_correction = (
-                self.setup_shape_correction(
-                    roi, roi_mode, roi_color, color_checker_position
-                )
-            )
         if self.active_drift_correction:
+            self.drift_correction = self.setup_drift_correction()
             self.corrections.append(self.drift_correction)
+            self.baseline = self.drift_correction(self.baseline)
+
         if self.active_curvature_correction:
+            assert curvature_options is not None
+            if "cache" in curvature_options:
+                # Read from cache
+                self.curvature_correction = darsia.CurvatureCorrection()
+                self.curvature_correction.load(
+                    curvature_options["cache"] / Path("curvature.npz")
+                )
+            else:
+                # Build from scratch
+                roi = curvature_options["roi"]
+                roi_mode = curvature_options.get("roi_mode", "interactive")
+                roi_color = curvature_options.get("roi_color", None)
+                self.curvature_correction = self.setup_curvature_correction(
+                    roi, roi_mode, roi_color
+                )
             self.corrections.append(self.curvature_correction)
+            self.baseline = self.curvature_correction(self.baseline)
 
-        # ! ---- SETUP BASELINE ----
-
-        self.baseline = self.raw_baseline.copy()
-        for correction in self.corrections:
-            self.baseline = correction(self.baseline)
+        # ! ---- SETUP RELATIVE COLOR CORRECTION ----
+        if self.active_relative_color_correction:
+            assert relative_color_options is not None
+            relative_color_images = []
+            for path in relative_color_options.pop("images"):
+                img = darsia.imread(path)
+                for correction in self.corrections:
+                    img = correction(img)
+                relative_color_images.append(img)
+            self.relative_color_correction = darsia.RelativeColorCorrection(
+                self.baseline, relative_color_images, relative_color_options
+            )
+            self.corrections.append(self.relative_color_correction)
+            self.baseline = self.relative_color_correction(self.baseline)
 
         # ! ---- SETUP SEGMENTATION ----
 
@@ -135,15 +183,20 @@ class SimpleFluidFlower:
         else:
             self.labels = self.setup_segmentation(segmentation)
 
-        # ! ---- SETUP COLOR CORRECTION ----
-        illumination_mode = kwargs.get("illumination_mode", "automatic")
-        if self.active_illumination_correction or self.active_color_correction:
-            self.illumination_correction, self.color_correction = (
-                self.setup_color_correction(color_checker_position, illumination_mode)
-            )
+        # ! ---- SETUP ILLUMINATION CORRECTION ----
         if self.active_illumination_correction:
+            assert illumination_options is not None
+            illumination_mode = illumination_options.get(
+                "illumination_mode", "automatic"
+            )
+            self.illumination_correction = self.setup_illumination_correction(
+                illumination_mode
+            )
             self.corrections.append(self.illumination_correction)
+
+        # ! ---- SETUP COLOR CORRECTION ----
         if self.active_color_correction:
+            self.color_correction = self.setup_color_correction()
             self.corrections.append(self.color_correction)
 
         # ! ---- BASELINE ----
@@ -160,32 +213,43 @@ class SimpleFluidFlower:
             depth=self.depth, porosity=self.porosity, **shape_meta
         )
 
-    def setup_shape_correction(
+    def setup_drift_correction(
         self,
-        roi: Path,
-        roi_mode: Literal["interactive", "automatic"],
-        roi_color: Optional[list[float]],
-        color_checker_position: darsia.ColorCheckerPosition,
-    ) -> tuple:
-        """Setup shape correction based on provided images.
-
-        Args:
-            roi (Path): path to image with ROI
-            color_checker_position (ColorCheckerPosition): position of the color checker
+    ) -> darsia.DriftCorrection:
+        """Setup drift correction based on color checker.
 
         Returns:
-            tuple: drift_correction, curvature_correction
+            DriftCorrection: drift_correction
 
         """
 
         # Define translation correction object based on color checker
         _, cc_voxels = darsia.find_colorchecker(
-            self.raw_baseline, color_checker_position
+            self.raw_baseline, self.colorchecker_position
         )
         self.drift_config = {"roi": cc_voxels}
         drift_correction = darsia.DriftCorrection(
             self.raw_baseline, config=self.drift_config
         )
+        return drift_correction
+
+    def setup_curvature_correction(
+        self,
+        roi: Path,
+        roi_mode: Literal["interactive", "automatic"],
+        roi_color: Optional[list[float]] = None,
+    ) -> darsia.CurvatureCorrection:
+        """Setup shape correction based on provided images.
+
+        Args:
+            roi (Path): path to image with ROI
+            roi_mode (Literal["interactive", "automatic"]): mode for ROI selection
+            roi_color (Optional[list[float]]): color of the ROI, only used in automatic mode
+
+        Returns:
+            CurvatureCorrection: curvature_correction
+
+        """
 
         # Read auxiliary images for calibration - make sure they are of the same size
         roi_image = darsia.resize(
@@ -194,48 +258,33 @@ class SimpleFluidFlower:
         )
 
         # Define  Restrict to region with frame
-        crop_assistant = darsia.CropAssistant(roi_image)
+        crop_assistant = darsia.CropAssistant(
+            roi_image, width=self.width, height=self.height
+        )
         if roi_mode == "interactive":
             # Generate curvature config from image using interactive mode
             self.curvature_config = crop_assistant()
         elif roi_mode == "automatic":
             # Generate curvature config from marked image
-            self.curvature_config = crop_assistant.from_image(
-                color=roi_color, width=self.width, height=self.height
-            )
+            self.curvature_config = crop_assistant.from_image(color=roi_color)
             # Close current plot (internally opened in the assistant)
             plt.close()
         else:
             raise ValueError(f"Unknown roi_mode: {roi_mode}")
         curvature_correction = darsia.CurvatureCorrection(config=self.curvature_config)
-        return drift_correction, curvature_correction
+        return curvature_correction
 
-    def set_corrections(self) -> None:
-        if self.drift_config:
-            self.drift_correction = darsia.DriftCorrection(
-                self.raw_baseline, config=self.drift_config
-            )
-
-        if self.curvature_config:
-            self.curvature_correction = darsia.CurvatureCorrection(
-                config=self.curvature_config
-            )
-
-        if self.color_config:
-            self.color_correction = darsia.ColorCorrection(config=self.color_config)
-
-    def setup_color_correction(
+    def setup_illumination_correction(
         self,
-        color_checker_position: darsia.ColorCheckerPosition,
         illumination_mode: Literal["automatic", "interactive"] = "automatic",
-    ) -> tuple:
+    ) -> darsia.IlluminationCorrection:
         """Setup color correction based on color checker.
 
         Args:
-            color_checker_position (ColorCheckerPosition): position of the color checker
+            illumination_mode (Literal["automatic", "interactive"]): mode for illumination
 
         Returns:
-            tuple: illumination_correction, color_correction
+            IlluminationCorrection: illumination_correction
 
         """
         # Define illumination gradient correction by estimating the lightness on distributed
@@ -268,10 +317,21 @@ class SimpleFluidFlower:
             show_plot=False,
         )
 
+        return illumination_correction
+
+    def setup_color_correction(
+        self,
+    ) -> darsia.ColorCorrection:
+        """Setup color correction based on color checker.
+
+        Returns:
+            ColorCorrection: color_correction
+
+        """
         # Define color correction object - target here the same colors as in the original
         # image (modulo curvature correction)
         colorchecker, cc_aligned_voxels = darsia.find_colorchecker(
-            self.baseline, color_checker_position
+            self.baseline, self.colorchecker_position
         )
         self.color_config = {
             "colorchecker": colorchecker,
@@ -280,7 +340,7 @@ class SimpleFluidFlower:
         }
         color_correction = darsia.ColorCorrection(config=self.color_config)
 
-        return illumination_correction, color_correction
+        return color_correction
 
     def setup_segmentation(self, segmentation: Path) -> darsia.Image:
         """Setup segmentation based on provided image.
@@ -306,6 +366,53 @@ class SimpleFluidFlower:
         labels = assistant()
 
         return labels
+
+    def set_corrections(self) -> None:
+        """Set corrections based on configuration."""
+
+        if self.drift_config:
+            self.drift_correction = darsia.DriftCorrection(
+                self.raw_baseline, config=self.drift_config
+            )
+
+        if self.curvature_config:
+            self.curvature_correction = darsia.CurvatureCorrection(
+                config=self.curvature_config
+            )
+
+        if self.color_config:
+            self.color_correction = darsia.ColorCorrection(config=self.color_config)
+
+    def activate_corrections(self, corrections: list[str]) -> None:
+        """Activate corrections based on input list and update baseline.
+
+        Args:
+            corrections (list[str]): list of corrections to activate; expected values are
+                "drift", "curvature", "illumination", "color"
+
+        """
+        # Update corrections
+        self.corrections = []
+        if "type" in corrections and hasattr(self, "type_conversion"):
+            self.corrections.append(self.type_conversion)
+        if "drift" in corrections and hasattr(self, "drift_correction"):
+            self.corrections.append(self.drift_correction)
+        if "curvature" in corrections and hasattr(self, "curvature_correction"):
+            self.corrections.append(self.curvature_correction)
+        if "relative-color" in corrections and hasattr(
+            self, "relative_color_correction"
+        ):
+            self.corrections.append(self.relative_color_correction)
+        if "illumination" in corrections and hasattr(self, "illumination_correction"):
+            self.corrections.append(self.illumination_correction)
+        if "color" in corrections and hasattr(self, "color_correction"):
+            self.corrections.append(self.color_correction)
+
+        # Update baseline
+        self.baseline = self.raw_baseline.copy()
+        for correction in self.corrections:
+            self.baseline = correction(self.baseline)
+        self.expert_knowledge(self.baseline)
 
     def expert_knowledge(self, img: darsia.Image) -> None:
         """Possibility to apply expert knowledge to the image after preprocessing.
@@ -357,6 +464,8 @@ class SimpleFluidFlower:
             self.drift_correction.save(folder / Path("drift.npz"))
         if self.active_curvature_correction:
             self.curvature_correction.save(folder / Path("curvature.npz"))
+        if self.active_relative_color_correction:
+            self.relative_color_correction.save(folder / Path("relative_color.npz"))
         if self.active_illumination_correction:
             self.illumination_correction.save(folder / Path("illumination.npz"))
         if self.active_color_correction:
@@ -386,36 +495,56 @@ class SimpleFluidFlower:
         self.depth = specs["depth"]
         self.porosity = specs["porosity"]
 
+        # Load baseline
+        self.reference_date = self.raw_baseline.date
+        self.baseline = self.raw_baseline.copy()
+
         # Load corrections
         self.corrections = []
+
+        if self.active_type_correction:
+            self.type_conversion = darsia.TypeCorrection(np.float64)
+            self.corrections.append(self.type_conversion)
+
         if self.active_drift_correction:
             self.drift_correction = darsia.DriftCorrection(self.raw_baseline)
             self.drift_correction.load(folder / Path("drift.npz"))
             self.corrections.append(self.drift_correction)
+            self.baseline = self.drift_correction(self.baseline)
 
         if self.active_curvature_correction:
             self.curvature_correction = darsia.CurvatureCorrection()
             self.curvature_correction.load(folder / Path("curvature.npz"))
             self.corrections.append(self.curvature_correction)
+            self.baseline = self.curvature_correction(self.baseline)
+
+        if self.active_relative_color_correction:
+            self.relative_color_correction = darsia.RelativeColorCorrection(
+                self.baseline
+            )
+            self.relative_color_correction.load(folder / Path("relative_color.npz"))
+            self.corrections.append(self.relative_color_correction)
+            self.baseline = self.relative_color_correction(self.baseline)
 
         if self.active_illumination_correction:
             self.illumination_correction = darsia.IlluminationCorrection()
             self.illumination_correction.load(folder / Path("illumination.npz"))
             self.corrections.append(self.illumination_correction)
+            self.baseline = self.illumination_correction(self.baseline)
 
         if self.active_color_correction:
             self.color_correction = darsia.ColorCorrection()
             self.color_correction.load(folder / Path("color.npz"))
             self.corrections.append(self.color_correction)
+            self.baseline = self.color_correction(self.baseline)
 
         # Load segmentation
-        self.labels = darsia.imread(folder / Path("labels.npz"))
+        if (folder / Path("labels.npz")).exists():
+            self.labels = darsia.imread(folder / Path("labels.npz"))
+        else:
+            self.labels = darsia.ones_like(self.baseline, dtype=np.uint8)
 
-        # Load baseline
-        self.reference_date = self.raw_baseline.date
-        self.baseline = self.raw_baseline.copy()
-        for correction in self.corrections:
-            self.baseline = correction(self.baseline)
+        # Apply expert knowledge
         self.expert_knowledge(self.baseline)
 
         # Load geometry
