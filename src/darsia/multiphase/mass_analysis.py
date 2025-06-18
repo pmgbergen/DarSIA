ff085c7a3af3534182bee4966945f6deb3cc208e
enrico facca <enrico.facca@gmail.com>
2025-06-18 16:14:20 +0200

    flake8 corrections

--- src/darsia/multiphase/mass_analysis.py
+++ src/darsia/multiphase/mass_analysis.py
@@ -54,7 +54,8 @@ class CO2MassAnalysis:
             plt.imshow(self.density_gaseous_co2)
             plt.colorbar()
             plt.title(
-                f"density gaseous CO2 - {self.atmospheric_pressure} bar - {self.temperature} deg Celsius"
+                f"density gaseous CO2 - {self.atmospheric_pressure}" 
+                + f" bar - {self.temperature} deg Celsius"
             )
             plt.savefig(log / "density_gaseous_co2.png")
             plt.close()
@@ -63,7 +64,8 @@ class CO2MassAnalysis:
             plt.imshow(self.solubility_co2)
             plt.colorbar()
             plt.title(
-                f"solubility CO2 - {self.atmospheric_pressure} bar - {self.temperature} deg Celsius"
+                f"solubility CO2 - {self.atmospheric_pressure}" + 
+                f" bar - {self.temperature} deg Celsius"
             )
             plt.savefig(log / "solubility_co2.png")
             plt.close()
