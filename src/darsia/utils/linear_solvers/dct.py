4e68326abfc3e2b2b900547cac4835393728bda8
enrico facca <enrico.facca@gmail.com>
2025-10-03 11:49:27 +0200

    proper kernel, it was 1/n while 1/(n-1) is correct

--- src/darsia/utils/linear_solvers/dct.py
+++ src/darsia/utils/linear_solvers/dct.py
@@ -5,24 +5,39 @@ import darsia
 
 
 class DCTSolver:
-    # TODO: Move later to a different location
+    """
+    A solver for the Poisson equation with zero Neumann boundary conditions.
+    This solver uses the discrete cosine transform (DCT) to efficiently solve the
+    Poisson equation on a rectangular grid.
+    Args:
+        grid (darsia.Grid): The grid on which to solve the Poisson equation.    
+    """ 
     def __init__(self, grid: darsia.Grid):
+        """
+        Initialize the Poisson solver with the given grid.
+        Args:
+            grid (darsia.Grid): The grid on which to solve the Poisson equation.
+        """
         if grid.dim == 2:
             # note the permutation of x and y in the grid
             ny, nx = grid.shape
-            hy, hx = grid.voxel_size
+            hy, hx = 1.0 / (nx-1) , 1.0/(ny-1) #grid.voxel_size
+            
 
             lambda_x = 2 * (np.cos(np.pi * np.arange(nx) / (nx - 1)) - 1)
             lambda_y = 2 * (np.cos(np.pi * np.arange(ny) / (ny - 1)) - 1)
 
-            kernel_inv = np.outer(lambda_x / (hx**2), np.ones(ny)) + np.outer(
-                np.ones(nx), lambda_y / (hy**2)
+            kernel_inv = (
+                np.outer(lambda_x / (hx**2), np.ones(ny)) 
+                + np.outer(np.ones(nx), lambda_y / (hy**2))
             )
+            zero_mode_index = (0,0)
+            kernel_inv[zero_mode_index]=1.0
 
         if grid.dim == 3:
             # note the permutation of x and y in the grid
             ny, nx, nz = grid.shape
-            hy, hx, hz = grid.voxel_size
+            hy, hx, hz = hy, hx = 1.0 / (nx-1) , 1.0/(ny-1), 1.0/ (nz-1) #grid.voxel_size
 
             lambda_z = 2 * (
                 np.cos(np.pi * np.arange(grid.shape[2]) / (grid.shape[2] - 1)) - 1
@@ -34,13 +49,25 @@ class DCTSolver:
                 )
                 + np.outer(np.ones((nx, ny)), lambda_z / (hz**2))
             )
+            zero_mode_index = (0,0,0)
+            kernel_inv[zero_mode_index]=1.0
 
         self.kernel = np.divide(1.0, kernel_inv)
-        self.kernel[np.isclose(kernel_inv, 0)] = 0
+        #self.kernel[np.isclose(kernel_inv, 0)] = 0
+        self.kernel[zero_mode_index] = 0
         self.volume = np.prod(grid.voxel_size)
         self.grid = grid
 
     def solve(self, rhs, **kwargs):
+        """
+        Solve the Poisson equation -Delta u = rhs with zero Neumann boundary conditions
+        
+        Args:
+            rhs (np.ndarray): Right-hand side of the Poisson equation. MUST BE INTEGRATED.(1D array)
+        Returns:
+            np.ndarray: Solution to the Poisson equation (1D array)
+        """
+
         # de-integrate the rhs
         rhs_2d = np.reshape(rhs, self.grid.shape, order="F") / self.volume
 
