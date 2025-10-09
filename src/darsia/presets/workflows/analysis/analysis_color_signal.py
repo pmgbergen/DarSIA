65682c6717159b7bf40b7ee9eb2ef0d585df17a9
Jakub Both <jakub.both@uib.no>
2025-10-09 19:01:25 +0200

    FEAT: Analysis for cropping and color signal

---
+++ src/darsia/presets/workflows/analysis/analysis_color_signal.py
@@ -0,0 +1,87 @@
+"""Template for color signal analysis."""
+
+from pathlib import Path
+import darsia
+import logging
+
+from darsia.presets.workflows.heterogeneous_color_analysis import (
+    HeterogeneousColorAnalysis,
+)
+from darsia.presets.workflows.fluidflower_config import FluidFlowerConfig
+
+logger = logging.getLogger(__name__)
+
+
+def analysis_color_signal(
+    cls,
+    path: Path,
+    show: bool = False,
+    save_jpg: bool = False,
+    save_npz: bool = False,
+):
+    # ! ---- LOAD RUN AND RIG ----
+    config = FluidFlowerConfig(path)
+    fluidflower = cls()
+    fluidflower.load(config.data.results / "fluidflower")
+
+    # Load experiment
+    experiment = darsia.ProtocolledExperiment(
+        imaging_protocol=config.protocol.imaging,
+        injection_protocol=config.protocol.injection,
+        pressure_temperature_protocol=config.protocol.pressure_temperature,
+        blacklist_protocol=config.protocol.blacklist,
+        pad=config.data.pad,
+    )
+    fluidflower.load_experiment(experiment)
+
+    # ! ---- CONCENTRATION ANALYSIS ---- ! #
+
+    color_signal_analysis = HeterogeneousColorAnalysis(
+        baseline=fluidflower.baseline,
+        labels=fluidflower.labels,
+        # restoration=fluidflower.restoration,
+        ignore_labels=config.color_paths.ignore_labels,
+    )
+    color_signal_analysis.load(config.color_signal.calibration_file)
+
+    # ! ---- VISUALIZATION ----
+
+    # Pick a reference color path - merely for visualization
+    reference_label = config.color_paths.reference_label
+    ref_color_path = darsia.ColorPath()
+    ref_color_path.load(
+        config.color_paths.calibration_file / f"color_path_{reference_label}.json"
+    )
+    custom_cmap = ref_color_path.get_color_map()
+
+    # ! ---- ANALYSIS ----
+
+    image_times = config.analysis.color_signal.image_times
+    image_datetimes = [
+        experiment.experiment_start + darsia.timedelta(hours=t) for t in image_times
+    ]
+    image_paths = experiment.imaging_protocol.find_images_for_datetimes(
+        paths=config.data.data,
+        datetimes=image_datetimes,
+    )
+
+    # Loop over images and analyze
+    for path in image_paths:
+        # Extract color signal
+        img = fluidflower.read_image(path)
+        color_signal = color_signal_analysis(img)
+
+        if show:
+            img.show(title=f"Image at {path.stem}", delay=True)
+            color_signal.show(
+                title=f"Concentration at {path.stem}", cmap=custom_cmap, delay=False
+            )
+
+        if save_npz:
+            path = config.data.results / "color_signal_analysis" / f"{path.stem}.npz"
+            color_signal.save(path)
+
+        if save_jpg:
+            path = config.data.results / "color_signal_analysis" / f"{path.stem}.jpg"
+            path.parent.mkdir(parents=True, exist_ok=True)
+            color_signal.write(path, cmap=custom_cmap, quality=80)
---
+++ src/darsia/presets/workflows/analysis/analysis_cropping.py
@@ -0,0 +1,73 @@
+"""Template for cropping/reading images."""
+
+from pathlib import Path
+
+import darsia
+import logging
+import numpy as np
+from darsia.presets.workflows.fluidflower_config import FluidFlowerConfig
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+def analysis_cropping(
+    cls,
+    path: Path,
+    show: bool = False,
+    save_jpg: bool = False,
+    save_npz: bool = False,
+):
+    # Read data from meta
+    config = FluidFlowerConfig(path)
+
+    # Plotting
+    plot_folder = config.data.results / "cropped_images"
+    plot_folder.mkdir(parents=True, exist_ok=True)
+
+    # ! ---- LOAD RIG AND RUN ----
+
+    fluidflower = cls()
+    fluidflower.load(config.data.results / "fluidflower")
+
+    # Load run
+    experiment = darsia.ProtocolledExperiment(
+        imaging_protocol=config.protocol.imaging,
+        injection_protocol=config.protocol.injection,
+        pressure_temperature_protocol=config.protocol.pressure_temperature,
+        blacklist_protocol=config.protocol.blacklist,
+        pad=config.data.pad,
+    )
+    fluidflower.load_experiment(experiment)
+
+    # Make selection of images to analyze
+    image_times = config.analysis.cropping.image_times
+    image_datetimes = [
+        experiment.experiment_start + darsia.timedelta(hours=t) for t in image_times
+    ]
+    image_paths = experiment.imaging_protocol.find_images_for_datetimes(
+        paths=config.data.data, datetimes=image_datetimes
+    )
+
+    for path in image_paths:
+        # Update
+        fluidflower.update(path)
+
+        # Read image
+        img = fluidflower.read_image(path)
+
+        # Convert image to darsia.OpticalImage
+        img = darsia.OpticalImage(img.img, **img.metadata())
+
+        if show:
+            img.show()
+
+        if save_npz:
+            img.save(plot_folder / f"{path.stem}.npz")
+
+        if save_jpg:
+            img = img.img_as(np.uint8)
+            img.original_dtype = np.uint8  # Hack to allow plotting
+            img.write(plot_folder / f"{path.stem}.jpg", quality=50)
+
+    print("Done. Analysis.")
