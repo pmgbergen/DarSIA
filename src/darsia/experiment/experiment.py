930e9f24e902bdaac592e112823f00d5303b6f67
Jakub Both <jakub.both@uib.no>
2025-10-09 08:21:33 +0200

    FEAT: Add protocolled experiment - managing protocols.

--- src/darsia/experiment/experiment.py
+++ src/darsia/experiment/experiment.py
@@ -2,8 +2,12 @@
 
 from abc import ABC, abstractmethod
 from datetime import datetime
+from pathlib import Path
+from typing import Optional
+import darsia
 
 
+# TODO rm.
 class Experiment(ABC):
     @property
     @abstractmethod
@@ -24,3 +28,36 @@ class Experiment(ABC):
     def hours_since_start(self, date: datetime) -> float:
         """Hours since start of the experiment."""
         return (date - self.injection_start).total_seconds() / 3600
+
+
+class ProtocolledExperiment:
+    def __init__(
+        self,
+        imaging_protocol: Path | tuple[Path, str],
+        injection_protocol: Optional[Path | tuple[Path, str]] = None,
+        pressure_temperature_protocol: Optional[Path | tuple[Path, str]] = None,
+        blacklist_protocol: Optional[Path | tuple[Path, str]] = None,
+        pad: int = 5,
+    ):
+        self.imaging_protocol = darsia.ImagingProtocol(
+            imaging_protocol, pad, blacklist_protocol
+        )
+        self.injection_protocol = darsia.InjectionProtocol(injection_protocol)
+        self.pressure_temperature_protocol = darsia.PressureTemperatureProtocol(
+            pressure_temperature_protocol
+        )
+
+        # Reference date
+        self.experiment_start = self.injection_protocol.df["start"].min()
+
+    def time_since_start(self, date: datetime) -> float:
+        """Hours since start of the experiment.
+
+        Args:
+            date (datetime): Date to compute the time since start for.
+
+        Returns:
+            float: Time since start in hours.
+
+        """
+        return (date - self.experiment_start).total_seconds() / 3600
