ddc54058d5cb4d2aff5c81a316ed1dd546ff93c2
Jakub Both <jakub.both@uib.no>
2025-11-03 14:23:38 +0100

    MAINT: Remove redundant code

--- src/darsia/presets/workflows/fluidflower_config.py
+++ src/darsia/presets/workflows/fluidflower_config.py
@@ -737,27 +737,6 @@ class FluidFlowerConfig:
         # except KeyError:
         #    self.ref_colorchecker = None
 
-        ## ! ---- COLOR ANALYSIS DATA ---- ! #
-        # self.color_analysis = {
-        #    "format": None,
-        #    "baseline_images": None,
-        #    "calibration_images": None,
-        # }
-        # self.color_analysis["format"] = meta_data["color_analysis"].get("format", "JPG")
-        # try:
-        #    self.color_analysis["baseline_images"] = (
-        #        common_folder / meta_data["color_analysis"]["baseline_images"]
-        #    )
-        # except KeyError:
-        #    self.color_analysis["baseline_images"] = None
-
-        # try:
-        #    self.color_analysis["calibration_images"] = (
-        #        common_folder / meta_data["color_analysis"]["calibration_images"]
-        #    )
-        # except KeyError:
-        #    self.color_analysis["calibration_images"] = None
-
         ## ! ---- CALIBRATION DATA ---- ! #
         # self.calibration = {
         #    "format": None,
@@ -765,38 +744,6 @@ class FluidFlowerConfig:
         #    "mass_images": None,
         # }
         # self.calibration["format"] = meta_data["calibration"].get("format", "JPG")
-
-        ## CO2 calibration data
-        # try:
-        #    self.co2_calibration = data_folder / meta_data["data"]["co2_calibration"]
-        # except KeyError:
-        #    self.co2_calibration = None
-
-        ## CO2(g) calibration data
-        # try:
-        #    self.co2_g_calibration = (
-        #        data_folder / meta_data["data"]["co2_g_calibration"]
-        #    )
-        # except KeyError:
-        #    self.co2_g_calibration = None
-
-        ## Scaling calibration data
-        # try:
-        #    self.calibration["scaling_image"] = meta_data["calibration"][
-        #        "scaling_image"
-        #    ]
-        # except KeyError:
-        #    self.calibration["scaling_image"] = None
-
-        ## Mass calibration data
-        # try:
-        #    self.calibration["mass_images"] = sorted(
-        #        Path(meta_data["calibration"]["mass_images"]).glob(
-        #            f"*.{self.calibration['format']}"
-        #        )
-        #    )
-        # except KeyError:
-        #    self.calibration["mass_images"] = None
 
     def _check(self, key: str):
         if key == "data" and not self.data:
@@ -864,79 +811,3 @@ class FluidFlowerConfig:
         else:
             raise ValueError(f"Unsupported meta file format: {meta.suffix}")
         return meta_data
-
-    # More results data
-
-    @property
-    def log_folder(self):
-        """Path to the log folder."""
-        return Path.cwd() / "log"
-
-    # Calibration data
-
-    # @property
-    # def co2_analysis_data(self):
-    #    """Path to the CO2 analysis data."""
-    #    return self.fluidflower_folder / "co2_analysis.csv"
-
-    # @property
-    # def co2_g_analysis_data(self):
-    #    """Path to the CO2 gas analysis data."""
-    #    return self.fluidflower_folder / "co2_g_analysis.csv"
-
-    # @property
-    # def pw_transformation_g_data(self):
-    #    """Path to the pressure-weighted transformation data for gas."""
-    #    return self.fluidflower_folder / "pw_transformation_g.csv"
-
-    # @property
-    # def pw_transformation_aq_data(self):
-    #    """Path to the pressure-weighted transformation data for aqueous phase."""
-    #    return self.fluidflower_folder / "pw_transformation_aq.csv"
-
-    # def update(self, key: str, path: Path) -> None:
-    #    """Update the folder path for a given key in the meta data.
-
-    #    Args:
-    #        key (str): Key to update. Currently only "fluidflower" is supported.
-    #        folder (Path): New folder path.
-
-    #    """
-    #    # Update the folder path
-    #    if key == "fluidflower":
-    #        self.fluidflower_folder = path
-    #    elif key == "labels":
-    #        self.labels = path
-    #    else:
-    #        raise ValueError(f"Key {key} not recognized.")
-
-    # def save(self, meta: Path) -> None:
-    #    """Save the updated meta data to a file.
-
-    #    Args:
-    #        meta (Path): Path to the meta data file.
-
-    #    """
-    #    # Load existing meta data
-    #    meta_data = self.load_meta(meta)
-
-    #    # Make paths relative (if needed)
-    #
-    #    if "results" not in meta_data:
-    #        meta_data["results"] = {}
-    #    meta_data["results"]["fluidflower"] = str(
-    #        self.fluidflower_folder.relative_to(self.results_folder)
-    #    )
-
-    #    # Save the updated meta data
-    #    if meta.suffix == ".json":
-    #        with open(meta, "w") as f:
-    #            json.dump(meta_data, f, indent=4)
-    #    elif meta.suffix == ".toml":
-    #        import tomli_w
-
-    #        with open(meta, "wb") as f:
-    #            f.write(tomli_w.dumps(meta_data).encode("utf-8"))
-    #        logger.info(f"Saved updated meta data to {meta}.")
-    #    else:
-    #        raise ValueError(f"Unsupported meta file format: {meta.suffix}")
