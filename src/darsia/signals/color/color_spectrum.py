b989e5d7bd6faf5ce3130520d9861fce75c842d9
Jakub Both <jakub.both@uib.no>
2025-11-05 08:02:16 +0100

    FEAT: Separate module with color spectrum

---
+++ src/darsia/signals/color/color_spectrum.py
@@ -0,0 +1,42 @@
+"""Module defining the ColorSpectrum data structure."""
+
+import numpy as np
+from dataclasses import dataclass
+import darsia
+
+
+@dataclass
+class ColorSpectrum:
+    """Data structure to hold a color spectrum."""
+
+    base_color: np.ndarray
+    """Reference color for the spectrum."""
+    spectrum: np.ndarray
+    """Distribution of colors in the spectrum."""
+    histogram: np.ndarray
+    """Histogram of color occurrences."""
+    color_range: darsia.ColorRange
+    """Color range associated with the spectrum."""
+
+    @property
+    def shape(self) -> tuple[int, int, int]:
+        """Return the shape of the spectrum."""
+        return self.spectrum.shape
+
+    @property
+    def effective_spectrum(self) -> np.ndarray:
+        """Compute the effective color spectrum as colors within the color range.
+
+        Returns:
+            np.ndarray: Effective color spectrum.
+
+        """
+        points = np.where(self.spectrum)
+        relative_color_components = []
+        for i in range(3):
+            relative_color_components.append(
+                points[i] / (self.spectrum.shape[i] - 1) * self.color_range.extent[i]
+                + self.color_range.min_color[i]
+            )
+        relative_colors = np.vstack(relative_color_components).T
+        return relative_colors
