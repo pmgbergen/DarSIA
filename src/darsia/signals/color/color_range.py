1931c1923c9f1dea8a106b70a0cbde93994a2fd1
Jakub Both <jakub.both@uib.no>
2025-11-06 11:33:21 +0100

    FEAT: Add I/O to color range

--- src/darsia/signals/color/color_range.py
+++ src/darsia/signals/color/color_range.py
@@ -2,6 +2,11 @@
 
 import darsia
 import numpy as np
+from pathlib import Path
+import json
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class ColorRange:
@@ -57,3 +62,48 @@ class ColorRange:
 
     def __repr__(self) -> str:
         return self.__str__()
+
+    def save(self, path: Path) -> None:
+        """Save color range to a json file.
+
+        Args:
+            path (Path): Path to the json file.
+
+        """
+        with open(path.with_suffix(".json"), "w") as f:
+            json.dump(
+                {
+                    "relative": self.relative,
+                    "min_color": self.min_color.tolist(),
+                    "max_color": self.max_color.tolist(),
+                },
+                f,
+                indent=4,
+            )
+        logger.info("Saved color range to %s", path.with_suffix(".json"))
+
+    @classmethod
+    def load(cls, path: Path) -> "ColorRange":
+        """Load color range from a json file.
+
+        Args:
+            path (Path): Path to the json file.
+
+        Returns:
+            ColorRange: Loaded color range.
+
+        """
+        with open(path.with_suffix(".json"), "r") as f:
+            data = json.load(f)
+        color_range = cls.__new__(cls)
+        color_range.relative = data["relative"]
+        color_range.min_color = np.array(data["min_color"])
+        color_range.max_color = np.array(data["max_color"])
+        color_range.range = (
+            (color_range.min_color[0], color_range.max_color[0]),
+            (color_range.min_color[1], color_range.max_color[1]),
+            (color_range.min_color[2], color_range.max_color[2]),
+        )
+        color_range.center = (color_range.min_color + color_range.max_color) / 2
+        color_range.extent = color_range.max_color - color_range.min_color
+        return color_range
