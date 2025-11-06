fd31817b8e9f3ac0169aedbc39d913c995267db9
Jakub Both <jakub.both@uib.no>
2025-11-06 10:17:49 +0100

    MAINT: Adapt calibration of color paths

--- src/darsia/presets/workflows/calibration/calibration_color_paths.py
+++ src/darsia/presets/workflows/calibration/calibration_color_paths.py
@@ -20,10 +20,14 @@ def calibration_color_paths(cls, path: Path, show: bool = False) -> None:
     config.check("rig", "data", "protocol", "color_paths")
 
     # Mypy type checking
-    for c in [
-        config.color_paths,
-    ]:
-        assert c is not None
+    assert config.color_paths is not None
+    assert config.rig is not None
+    assert config.data is not None
+    assert config.protocol is not None
+    assert config.protocol.imaging is not None
+    assert config.protocol.injection is not None
+    assert config.protocol.pressure_temperature is not None
+    assert config.protocol.blacklist is not None
 
     # ! ---- LOAD RUN AND RIG ----
 
@@ -32,6 +36,7 @@ def calibration_color_paths(cls, path: Path, show: bool = False) -> None:
 
     # Load experiment
     experiment = darsia.ProtocolledExperiment(
+        data=config.data.data,
         imaging_protocol=config.protocol.imaging,
         injection_protocol=config.protocol.injection,
         pressure_temperature_protocol=config.protocol.pressure_temperature,
@@ -40,67 +45,102 @@ def calibration_color_paths(cls, path: Path, show: bool = False) -> None:
     )
     fluidflower.load_experiment(experiment)
 
-    # ! ---- COLOR PATH TOOL ----
-    color_path_regression = darsia.ColorPathRegression(
-        labels=fluidflower.labels,
-        ignore_labels=config.color_paths.ignore_labels,
+    # ! ---- LOAD IMAGES ----
+    baseline_images = []
+    for p in config.color_paths.baseline_image_paths:
+        cache_path = Path(".") / "tmp" / f"cache_{p.stem}.npz"
+        if not cache_path.exists():
+            baseline_image = fluidflower.read_image(p)
+            baseline_image.save(cache_path)
+        else:
+            baseline_image = darsia.imread(cache_path)
+        baseline_images.append(baseline_image)
+
+    calibration_image_paths = config.color_paths.calibration_image_paths
+    if len(calibration_image_paths) == 0:
+        calibration_image_paths = experiment.find_images_for_times(
+            times=config.color_paths.calibration_image_times
+        )
+    calibration_images = []
+    for p in calibration_image_paths:
+        cache_path = Path(".") / "tmp" / f"cache_{p.stem}.npz"
+        if not cache_path.exists():
+            calibration_image = fluidflower.read_image(p)
+            calibration_image.save(cache_path)
+        else:
+            calibration_image = darsia.imread(cache_path)
+        calibration_images.append(calibration_image)
+
+    # ! ---- IDENTIFY COLOR RANGE ----
+
+    tracer_color_range = darsia.ColorRange(
+        images=calibration_images,
+        baseline=fluidflower.baseline,
         mask=fluidflower.boolean_porosity,
     )
 
-    # ! ---- ANALYZE FLUCTUATIONS IN 10 BASELINE IMAGES ----
-    baseline_images = []
-    for path in config.color_paths.baseline_images:
-        logger.info("Reading baseline image: %s", path)
-        baseline_image = fluidflower.read_image(path)
-        baseline_images.append(baseline_image)
+    # ! ---- COLOR PATH TOOL ----
 
-    baseline_color_spectrum = color_path_regression.get_color_spectrum(
-        images=baseline_images,
-        baseline=fluidflower.baseline,
+    color_path_regression = darsia.LabelColorPathMapRegression(
+        labels=fluidflower.labels,
+        mask=fluidflower.boolean_porosity,
+        color_range=tracer_color_range,
         resolution=config.color_paths.resolution,
-        threshold_significant=config.color_paths.threshold_baseline,
-        verbose=show,
+        ignore_labels=config.color_paths.ignore_labels,
     )
 
+    # ! ---- ANALYZE FLUCTUATIONS IN BASELINE IMAGES ----
+
+    baseline_color_spectrum: darsia.LabelColorSpectrumMap = (
+        color_path_regression.get_label_color_spectrum_map(
+            images=baseline_images,
+            baseline=fluidflower.baseline,
+            threshold_significant=config.color_paths.threshold_baseline,
+            verbose=False,
+        )
+    )
+    # Free memory for performance
+    del baseline_images
+
     # Expand the baseline color spectrum through linear regression
-    baseline_color_spectrum = color_path_regression.expand_color_spectrum(
-        color_spectrum=baseline_color_spectrum, verbose=show
+    expanded_baseline_color_spectrum: darsia.LabelColorSpectrumMap = (
+        color_path_regression.expand_label_color_spectrum_map(
+            label_color_spectrum_map=baseline_color_spectrum,
+            verbose=False,
+        )
     )
 
     # ! ---- EXTRACT COLOR SPECTRUM OF TRACERS ---- ! #
 
-    calibration_images = []
-    for path in config.color_paths.calibration_images:
-        logger.info("Reading calibration image: %s", path)
-        calibration_image = fluidflower.read_image(path)
-        calibration_images.append(calibration_image)
-
-    tracer_color_spectrum = color_path_regression.get_color_spectrum(
+    tracer_color_spectrum = color_path_regression.get_label_color_spectrum_map(
         images=calibration_images,
         baseline=fluidflower.baseline,
-        resolution=config.color_paths.resolution,
-        ignore_color_spectrum=baseline_color_spectrum,
+        ignore=expanded_baseline_color_spectrum,
         threshold_significant=config.color_paths.threshold_calibration,
-        verbose=show,
+        verbose=False,
     )
+    # Free memory for performance
+    del calibration_images
 
     # Find a relative color path through the significant boxes
-    color_path: dict[int, darsia.ColorPath] = {}
-    for label, spectrum in tracer_color_spectrum.items():
-        color_path[label] = color_path_regression.find_relative_color_path(
-            spectrum=spectrum,
-            base_color_spectrum=baseline_color_spectrum.get(label, None),
+    label_color_path_map: darsia.LabelColorPathMap = (
+        color_path_regression.find_label_color_path_map(
+            label_color_spectrum_map=tracer_color_spectrum,
+            ignore=expanded_baseline_color_spectrum,
             num_segments=config.color_paths.num_segments,
+            directory=config.color_paths.calibration_file,
             verbose=show,
-            plot_title=f"Color Path Analysis - Label {label}",
         )
+    )
 
-    config.color_paths.calibration_file.mkdir(parents=True, exist_ok=True)
-    for label, path in color_path.items():
-        print(f"Label {label}:")
-        print(f"  Base color: {path.base_color}")
-        print(f"  Relative colors: {path.relative_colors}")
-        print(f"  Values: {path.values}")
-        if show:
-            path.show()
-        path.save(config.color_paths.calibration_file / f"color_path_{label}.json")
+    # Store the color paths to file
+    label_color_path_map.save(config.color_paths.calibration_file)
+
+    # Display the color paths
+    if show:
+        print(label_color_path_map)
+        label_color_path_map.show()
+
+    # TODO: Provide advanced plotting - mostly for paper publication.
+
+    logger.info("Calibration of color paths completed.")
