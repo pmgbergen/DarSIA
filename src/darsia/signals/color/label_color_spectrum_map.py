9fa7ff695b480db070875d32d36a5fd874acdd42
Jakub Both <jakub.both@uib.no>
2025-11-10 05:02:38 +0100

    FEAT: Add I/O to label color spectrum map

--- src/darsia/signals/color/label_color_spectrum_map.py
+++ src/darsia/signals/color/label_color_spectrum_map.py
@@ -30,3 +30,33 @@ class LabelColorSpectrumMap(dict[int, darsia.ColorSpectrum]):
         for label, color_spectrum in self.items():
             str_str += f"Label {label}: {str(color_spectrum)}\n"
         return str_str
+
+    def save(self, directory: darsia.Path) -> None:
+        """Save color spectra to a directory.
+
+        Stores each color spectrum in a separate file named `color_spectrum_{label}.json`.
+
+        Args:
+            directory (darsia.Path): The directory to save the color spectrum files.
+
+        """
+        directory.mkdir(parents=True, exist_ok=True)
+        for label, color_spectrum in self.items():
+            color_spectrum.save(directory / f"color_spectrum_{label}.json")
+
+    @classmethod
+    def load(cls, directory: darsia.Path) -> "LabelColorSpectrumMap":
+        """Load color spectra from a directory.
+
+        Identifies files named `color_spectrum_{label}.json` and loads them.
+
+        Args:
+            directory (darsia.Path): The directory containing color spectrum files.
+
+        """
+        color_spectra = {}
+        for file in directory.glob("color_spectrum_*.json"):
+            label = int(file.stem.split("_")[-1])
+            color_spectrum = darsia.ColorSpectrum.load(file)
+            color_spectra[label] = color_spectrum
+        return cls(color_spectra)
