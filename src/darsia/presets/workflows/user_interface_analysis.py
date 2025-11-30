765f548674c00a265034385ddc7236740da146d3
Jakub Both <jakub.both@uib.no>
2025-11-30 08:23:28 +0100

    MAINT: Clean up interface for analysis; allow for mulitple configs

--- src/darsia/presets/workflows/user_interface_analysis.py
+++ src/darsia/presets/workflows/user_interface_analysis.py
@@ -6,14 +6,8 @@ from pathlib import Path
 import logging
 
 from darsia.presets.workflows.analysis.analysis_cropping import analysis_cropping
-from darsia.presets.workflows.analysis.analysis_color_signal import (
-    analysis_color_signal,
-)
 from darsia.presets.workflows.analysis.analysis_segmentation import (
     analysis_segmentation,
-)
-from darsia.presets.workflows.analysis.analysis_mass import (
-    analysis_mass,
 )
 from darsia.presets.workflows.analysis.analysis_color_to_mass import (
     analysis_color_to_mass,
@@ -27,24 +21,21 @@ def build_parser_for_analysis():
     parser.add_argument(
         "--config",
         type=str,
-        default="config.toml",
-        help="Path to config file.",
-    )
-    parser.add_argument(
-        "--all", action="store_true", help="Activate all analysis steps."
+        nargs="+",
+        required=True,
+        help="Path(s) to config file(s). Multiple files can be specified.",
     )
     parser.add_argument(
         "--cropping", action="store_true", help="Apply cropping to analysis images."
     )
     parser.add_argument(
-        "--color-signal", action="store_true", help="Perform signal analysis."
-    )
-    parser.add_argument(
         "--segmentation", action="store_true", help="Perform segmentation analysis."
     )
-    parser.add_argument("--mass", action="store_true", help="Perform mass analysis.")
     parser.add_argument(
-        "--color-to-mass", action="store_true", help="Perform color to mass analysis."
+        "--mass", action="store_true", help="Perform color to mass analysis."
+    )
+    parser.add_argument(
+        "--all", action="store_true", help="Perform analysis on entire dataset."
     )
     parser.add_argument(
         "--show",
@@ -62,11 +53,6 @@ def build_parser_for_analysis():
         help="Save output figures as NPZ.",
     )
     parser.add_argument(
-        "--test-run",
-        action="store_true",
-        help="Perform a test run with limited data.",
-    )
-    parser.add_argument(
         "--info", action="store_true", help="Provide help for activated flags."
     )
     return parser
@@ -80,17 +66,9 @@ def print_help_for_flags(args, parser):
             print(
                 "Cropping analysis: Applies cropping to specified images based on configuration."
             )
-        if args.color_signal:
-            print(
-                "Color signal analysis: Analyzes color signals in the images as per configuration."
-            )
         if args.segmentation:
             print(
                 "Segmentation analysis: Performs segmentation on images according to configuration."
-            )
-        if args.mass:
-            print(
-                "Mass analysis: Conducts mass-related analysis based on the provided configuration."
             )
         print("To run the analysis, remove the '--info' flag.")
         import sys
@@ -99,33 +77,33 @@ def print_help_for_flags(args, parser):
 
 
 def run_analysis(rig, args, **kwargs):
-    if args.all or args.cropping:
+    if args.cropping:
         analysis_cropping(
-            rig, Path(args.config), args.show, args.save_jpg, args.save_npz, **kwargs
+            rig,
+            args.config,
+            args.show,
+            args.save_jpg,
+            args.save_npz,
+            args.all,
         )
 
-    if args.all or args.color_signal:
-        analysis_color_signal(
-            rig, Path(args.config), args.show, args.save_jpg, args.save_npz, **kwargs
-        )
-
-    if args.all or args.segmentation:
-        analysis_segmentation(
-            rig, Path(args.config), args.show, args.save_jpg, args.save_npz, **kwargs
-        )
-
-    if args.all or args.mass:
-        analysis_mass(
-            rig, Path(args.config), args.show, args.save_jpg, args.save_npz, **kwargs
-        )
-
-    if args.all or args.color_to_mass:
+    if args.mass:
         analysis_color_to_mass(
             rig,
             Path(args.config),
+            show=args.show,
+            rois=kwargs.get("rois"),
+            rois_and_labels=kwargs.get("rois_and_labels"),
+            all=args.all,
+        )
+
+    if args.segmentation:
+        analysis_segmentation(
+            rig,
+            Path(args.config),
+            rois=kwargs.get("rois"),
             test_run=args.test_run,
             show=args.show,
-            **kwargs,
         )
 
 
