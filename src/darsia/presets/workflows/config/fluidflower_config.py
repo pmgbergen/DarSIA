ef389215747b803903209da043a4e56653cdb0ca
Jakub Both <jakub.both@uib.no>
2026-02-22 15:33:03 +0100

    MAINT: Reorganize config for protocosl.

--- src/darsia/presets/workflows/config/fluidflower_config.py
+++ src/darsia/presets/workflows/config/fluidflower_config.py
@@ -21,81 +21,7 @@ from .data import DataConfig
 from .labeling import LabelingConfig
 from .facies import FaciesConfig
 from .depth import DepthConfig
-
-
-@dataclass
-class FluidFlowerProtocolConfig:
-    imaging: Path | tuple[Path, str] | None = None
-    """Path to the imaging protocol file or (file, sheet)."""
-    injection: Path | tuple[Path, str] | None = None
-    """Path to the injection protocol file or (file, sheet)."""
-    blacklist: Path | tuple[Path, str] | None = None
-    """Path to the blacklist protocol file or (file, sheet)."""
-    pressure_temperature: Path | tuple[Path, str] | None = None
-    """Path to the pressure-temperature protocol file or (file, sheet)."""
-
-    def load(self, path: Path) -> "FluidFlowerProtocolConfig":
-        sec = _get_section_from_toml(path, "protocols")
-        try:
-            imaging_protocol = sec["imaging"]
-            if isinstance(imaging_protocol, list):
-                self.imaging = (Path(imaging_protocol[0]), imaging_protocol[1])
-            elif isinstance(imaging_protocol, str):
-                self.imaging = Path(imaging_protocol)
-            else:
-                raise ValueError(
-                    "Imaging protocol must be a string or a list of [path, sheet]."
-                )
-
-        except KeyError:
-            self.imaging = None
-
-        try:
-            injection_protocol = sec["injection"]
-            if isinstance(injection_protocol, list):
-                self.injection = (Path(injection_protocol[0]), injection_protocol[1])
-            elif isinstance(injection_protocol, str):
-                self.injection = Path(injection_protocol)
-            else:
-                raise ValueError(
-                    "Injection protocol must be a string or a list of [path, sheet]."
-                )
-        except KeyError:
-            self.injection = None
-
-        try:
-            blacklist_protocol = sec["blacklist"]
-            if isinstance(blacklist_protocol, list):
-                self.blacklist = (Path(blacklist_protocol[0]), blacklist_protocol[1])
-            elif isinstance(blacklist_protocol, str):
-                self.blacklist = Path(blacklist_protocol)
-            else:
-                raise ValueError(
-                    "Blacklist protocol must be a string or a list of [path, sheet]."
-                )
-        except KeyError:
-            self.blacklist = None
-
-        try:
-            pressure_temperature_protocol = sec["pressure_temperature"]
-            if isinstance(pressure_temperature_protocol, list):
-                self.pressure_temperature = (
-                    Path(pressure_temperature_protocol[0]),
-                    pressure_temperature_protocol[1],
-                )
-            elif isinstance(pressure_temperature_protocol, str):
-                self.pressure_temperature = Path(pressure_temperature_protocol)
-            else:
-                raise ValueError(
-                    "Pressure-temperature protocol must be a string or a list of [path, sheet]."
-                )
-        except KeyError:
-            self.pressure_temperature = None
-
-        return self
-
-    def error(self):
-        raise ValueError(f"Use [protocols] in the config file to load protocols.")
+from .protocol import ProtocolConfig
 
 
 @dataclass
@@ -570,9 +496,7 @@ class FluidFlowerConfig:
 
         # ! ---- PROTOCOLS ---- ! #
         try:
-            self.protocol: FluidFlowerProtocolConfig | None = (
-                FluidFlowerProtocolConfig()
-            )
+            self.protocol: ProtocolConfig | None = ProtocolConfig()
             self.protocol.load(path)
         except KeyError:
             self.protocol = None
@@ -640,7 +564,7 @@ class FluidFlowerConfig:
         elif key == "rig" and not self.rig:
             RigConfig().error()
         elif key == "protocol" and not self.protocol:
-            FluidFlowerProtocolConfig().error()
+            ProtocolConfig().error()
         elif key == "color_paths" and not self.color_paths:
             ColorPathsConfig().error()
         elif key == "analysis.data" and (not self.analysis or not self.analysis.data):
