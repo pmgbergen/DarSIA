cda93185f886711627c679677c81ad64d696af60
Jakub Both <jakub.both@uib.no>
2026-02-26 07:19:17 +0100

    FEAT: Prepare corrections config to integarte colro and illumination corrections

--- src/darsia/presets/workflows/config/corrections.py
+++ src/darsia/presets/workflows/config/corrections.py
@@ -13,18 +13,20 @@ class CorrectionsConfig:
         type_conversion: Enable type conversion to float32 (default: True).
         resize: Enable resize correction to baseline shape (default: True).
         drift: Enable drift correction based on color checker (default: True).
-        color: Enable color correction based on color checker (default: False).
-        illumination: Enable illumination correction based on color checker (default: False).
         curvature: Enable curvature correction from laser grid (default: True).
+        color: Enable color correction based on color checker (default: False).
+        relative_color: Enable relative color correction based on color checker (default: False).
+        illumination: Enable illumination correction based on color checker (default: False).
 
     """
 
     type: bool = True
     resize: bool = True
     drift: bool = True
-    color: bool = False
-    illumination: bool = False
     curvature: bool = True
+    color: bool = False
+    relative_color: bool = False
+    illumination: bool = False
 
     def load(self, path: Path | list[Path]) -> "CorrectionsConfig":
         """Load correction configuration from TOML file.
@@ -42,8 +44,9 @@ class CorrectionsConfig:
         self.type = sec.get("type", self.type)
         self.resize = sec.get("resize", self.resize)
         self.drift = sec.get("drift", self.drift)
-        self.color = sec.get("color", self.color)
-        self.illumination = sec.get("illumination", self.illumination)
         self.curvature = sec.get("curvature", self.curvature)
+        self.color = sec.get("color", self.color)
+        self.relative_color = sec.get("relative_color", self.relative_color)
+        self.illumination = sec.get("illumination", self.illumination)
 
         return self
