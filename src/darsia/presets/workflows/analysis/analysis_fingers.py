00c5e66f6ff0825759e0330da9435a599449282d
Jakub Both <jakub.both@uib.no>
2026-03-09 07:12:20 +0100

    FEAT: Store finger analysis in form of images and csv file.

--- src/darsia/presets/workflows/analysis/analysis_fingers.py
+++ src/darsia/presets/workflows/analysis/analysis_fingers.py
@@ -12,6 +12,7 @@ from darsia.presets.workflows.analysis.analysis_context import (
 from darsia.presets.workflows.rig import Rig
 from darsia.presets.workflows.segmentation_contours import SimpleSegmentation
 from darsia.single_image_analysis.contouranalysis import ContourAnalysis
+import pandas as pd
 
 logger = logging.getLogger(__name__)
 
@@ -40,7 +41,18 @@ def analysis_fingers_from_context(
     segmentation_analysis = SimpleSegmentation(
         mode=fingers_config.mode, threshold=fingers_config.threshold
     )
-    contour_analysis = ContourAnalysis(verbosity=True)
+    contour_analysis = ContourAnalysis()
+
+    # Data management.
+    results_folder = ctx.config.analysis.fingers.folder
+    img_folder = ctx.config.analysis.fingers.img_folder
+    results_folder.mkdir(parents=True, exist_ok=True)
+    img_folder.mkdir(parents=True, exist_ok=True)
+
+    # DataFrame to store results.
+    df = pd.DataFrame(
+        columns=["time", "key", "image", "contour_length", "number_peaks"]
+    )
 
     # Loop over images and analyze
     for path in image_paths:
@@ -56,21 +68,57 @@ def analysis_fingers_from_context(
             mass=mass_analysis_result.mass,
         )
 
-        for key, roi in fingers_config.roi.items():
-            # Perform finger analysis if configured
-            contour_analysis.load(img, segmentation, roi=roi.roi, fill_holes=False)
+        for key, roi_config in fingers_config.roi.items():
+            # TODO: allow to tune the threshold value, and mode in a interactive way.
 
-            print(key, roi.name, roi.roi)
+            # Perform finger analysis if configured
+            contour_analysis.load(
+                img, segmentation, roi=roi_config.roi, fill_holes=False
+            )
+
+            # Extract contour
+            contours = contour_analysis.contours()
 
             # Determine various contour values.
             contour_length = contour_analysis.length()
-            print(f"Contour length: {contour_length=}")
-
-            fingers = contour_analysis.fingers()
-            print(f"Fingers: {fingers=}")
-
+            peaks, _ = contour_analysis.fingers()
             number_peaks = contour_analysis.number_peaks()
-            print(f"Number of peaks: {number_peaks=}")
+            contour_analysis.plot_finger_peaks(
+                img,
+                peaks,
+                roi_config.roi,
+                contours=contours,
+                path=img_folder / f"{path.stem}_{key}.png",
+                show=show,
+                **{
+                    "peak_color": "r",
+                    "peak_size": 10,
+                    "contour_color": "w",
+                    "contour_linewidth": 1,
+                    # "plot_boundary": True,
+                    # "boundary_color": "y",
+                    # "boundary_linewidth": 2,
+                    # "highlight_roi": True,
+                },
+            )
+
+            df = pd.concat(
+                [
+                    df,
+                    pd.DataFrame(
+                        {
+                            "time": img.time,
+                            "key": key,
+                            "image": path.name,
+                            "contour_length": contour_length,
+                            "number_peaks": number_peaks,
+                        },
+                        index=[0],
+                    ),
+                ],
+                ignore_index=True,
+            )
+            df.to_csv(results_folder / "results.csv", index=False)
 
 
 def analysis_fingers(
