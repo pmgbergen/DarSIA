c8198eef5b3725f6f1edfceefba7f64b2768411b
Jakub Both <jakub.both@uib.no>
2026-03-11 05:44:17 +0100

    STY: flake8

--- src/darsia/presets/workflows/analysis/analysis_cropping.py
+++ src/darsia/presets/workflows/analysis/analysis_cropping.py
@@ -40,11 +40,13 @@ def analysis_cropping_from_context(
     # Require that output options are selected
     while not (show or save_jpg or save_npz):
         logger.warning(
-            "\033[33mNo output options selected. The images will be read but not shown or saved. "
-            "Please set at least one of show, save_jpg, or save_npz to True to see results.\033[0m"
+            """\033[33mNo output options selected. The images will be read but not shown """
+            """or saved.\nPlease set at least one of show, save_jpg, or save_npz to True """
+            """to see results.\033[0m"""
         )
         user_input = input(
-            "Enter a number to select output options (1=show, 2=save_jpg, 3=save_npz, e.g. 13 for show and save_npz): "
+            """Enter a number to select output options (1=show, 2=save_jpg, 3=save_npz, """
+            """e.g. 13 for show and save_npz): """
         )
         show = "1" in user_input
         save_jpg = "2" in user_input
