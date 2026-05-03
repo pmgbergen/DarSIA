7194df461da46b8ac39db8fc4fc24cb4c68aa7b0
Jakub Both <jakub.both@uib.no>
2026-05-03 06:11:07 +0200

    FEAT: Add skeleton analysis (alternaive for finger analysis)

---
+++ src/darsia/single_image_analysis/skeleton_analysis.py
@@ -0,0 +1,419 @@
+"""Module containing analysis tools for segmented images - use skeletonization."""
+
+from __future__ import annotations
+
+import logging
+from collections import namedtuple
+from pathlib import Path
+from typing import cast
+
+import cv2
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import scipy.ndimage as ndi
+import skimage
+from scipy.spatial import distance_matrix
+
+import darsia
+
+from .contouranalysis import _corners_of_roi
+
+logger = logging.getLogger(__name__)
+
+
+class SkeletonAnalysis:
+    """Skeleton analysis object."""
+
+    def __init__(
+        self,
+        verbosity: bool = False,
+        contour_smoother: darsia.ContourSmoother | None = None,
+        reduce_to_main_contour: bool = False,
+    ) -> None:
+        """Constructor.
+
+        Args:
+            verbosity (bool): Verbosity flag.
+
+        """
+
+        self.verbosity = verbosity
+        """Vebosity flag."""
+        self.contour_smoother = contour_smoother
+        """Optional contour smoother for the contours determined from the mask."""
+        self.reduce_to_main_contour = reduce_to_main_contour
+        """Whether to reduce to main contour."""
+
+    @darsia.timing_decorator
+    def load(
+        self,
+        img: darsia.Image,
+        mask: darsia.Image,
+        roi: darsia.CoordinateArray | None = None,
+        fill_holes: bool = False,
+    ) -> None:
+        """Read labeled image and restrict to values of interest.
+
+        Args:
+            img (Image): image to analyze.
+            mask (Image): labeled image.
+            roi (array, optional): set of points defining a box.
+            values_of_interest (int, list of int, optional): label values of interest.
+            fill_holes (bool): flag controlling whether holes in labels are filles.
+
+        """
+
+        # Make copy of image and restrict to region of interest
+        mask_roi: darsia.Image = (
+            mask.copy() if roi is None else cast(darsia.Image, mask.subregion(roi))
+        )
+
+        # Extract boolean mask covering values of interest.
+        mask_roi_array = mask_roi.img
+
+        # Fill all holes
+        if fill_holes:
+            mask_roi_array = ndi.binary_fill_holes(mask_roi_array)
+
+        self.coordinatesystem = mask_roi.coordinatesystem
+        """Coordinate system of subimage."""
+
+        self.img = img
+        """Image."""
+
+        self.mask = mask_roi_array
+        """Mask."""
+
+        self.roi = roi
+        """Region of interest."""
+
+    @darsia.timing_decorator
+    def skeleton(self, contours: list[np.ndarray] | None) -> list[np.ndarray]:
+        """Determine skeleton of loaded labeled image.
+
+        Returns:
+            list[np.ndarray]: list of skeletons, where each skeleton is given as an
+                array of pixels.
+
+        """
+        if contours is None:
+            # Extract contours.
+            contours, _ = cv2.findContours(
+                skimage.img_as_ubyte(self.mask), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE
+            )
+        if len(contours) == 0:
+            self.contour = None
+            return None
+
+        # Determine the main contour as the one with the largest area.
+        if self.reduce_to_main_contour and len(contours) > 1:
+            contour_areas = [cv2.contourArea(contour) for contour in contours]
+            main_contour_index = np.argmax(contour_areas)
+            contours = [contours[main_contour_index]]
+
+        # Smooth contours if smoother provided.
+        if self.contour_smoother:
+            contours = [self.contour_smoother(contour) for contour in contours]
+
+        assert (
+            len(contours) == 1
+        ), "Skeletonization currently only implemented for one contour."
+        self.contour = contours[0]
+
+        # Get mask from contour by filling
+        contour_mask = np.zeros_like(self.mask, dtype=np.uint8)
+        cv2.fillPoly(contour_mask, [self.contour], color=1)
+
+        # Skeletonize the contour mask
+        skeleton = skimage.morphology.skeletonize(contour_mask)
+
+        return skeleton
+
+    @darsia.timing_decorator
+    def leaves_and_junctions(
+        self,
+        skeleton: np.ndarray | None,
+        min_branch_length: float = 0.05,
+        max_group_distance: float = 0.01,
+    ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+        """
+        Determine leaves and junctions of the skeleton.
+
+        Args:
+            skeleton (np.ndarray): skeleton for which to determine leaves and junctions.
+            min_branch_length (float): minimum branch length in metric units;
+                branches shorter than this are removed before determining leaves and junctions,
+                as they are likely noise; default is 0.05.
+            max_group_distance (float): maximum distance for grouping pixels in metric units;
+                default is 0.01.
+
+        Returns:
+            array: pixels of leaves.
+            array: pixels of junctions.
+            array: pixels of junctions that are part of the top line
+
+        """
+        # Convert Euclidean distances to pixel distance
+        min_branch_pixel_distance = self.coordinatesystem.num_voxels(
+            min_branch_length, "x"
+        )
+        max_group_pixel_distance = self.coordinatesystem.num_voxels(
+            max_group_distance, "x"
+        )
+
+        # Special case of no contour
+        if skeleton is None or len(skeleton) == 0:
+            return (
+                np.zeros((0, 1, 2), dtype=int),
+                np.zeros((0, 1, 2), dtype=int),
+                np.zeros((0, 1, 2), dtype=int),
+            )
+
+        # Continue with each contour separately.
+        leaves_pixels = np.zeros((0, 2), dtype=int)
+        junctions_pixels = np.zeros((0, 2), dtype=int)
+        base_pixels = np.zeros((0, 2), dtype=int)
+
+        # Use networkx to convert the skeleton into a graph.
+        skeleton_graph = nx.Graph()
+        skeleton_pixels = np.argwhere(skeleton)
+        for pixel in skeleton_pixels:
+            skeleton_graph.add_node(tuple(pixel))
+
+        # Connect neighboring pixels in the graph.
+        for pixel in skeleton_graph.nodes:
+            x, y = pixel
+            potential_neighbors = [
+                (x + dx, y + dy)
+                for dx, dy in [
+                    (-1, -1),
+                    (-1, 0),
+                    (-1, 1),
+                    (0, -1),
+                    (0, 1),
+                    (1, -1),
+                    (1, 0),
+                    (1, 1),
+                ]
+            ]
+            neighbors = [n for n in potential_neighbors if n in skeleton_graph.nodes]
+            for neighbor in neighbors:
+                skeleton_graph.add_edge(tuple(pixel), neighbor)
+
+        # Make graph unidirected by removing self-loops and parallel edges.
+        skeleton_graph = skeleton_graph.to_undirected()
+
+        # Remove very short branches - these are likely noise and can lead to spurious leaves
+        # and junctions.
+        for component in nx.connected_components(skeleton_graph):
+            if len(component) < min_branch_pixel_distance:
+                skeleton_graph.remove_nodes_from(component)
+
+        # Identify all pixels that have min [:,0] value, i.e., the pixels that are closest to
+        # the top of the image. These will be considered as part of the "top line" and not
+        # as leaves or junctions, even if they have degree 1 or >2, respectively.
+        top_line = []
+        for col in np.unique(skeleton_pixels[:, 1]):
+            rows = skeleton_pixels[skeleton_pixels[:, 1] == col][:, 0]
+            if len(rows) > 0:
+                min_row = np.min(rows)
+                top_line.append((min_row, col))
+
+        # Determine leaves and junctions based on node degree.
+        for node in skeleton_graph.nodes:
+            degree = skeleton_graph.degree(node)
+            if degree == 1 and node not in top_line:
+                leaves_pixels = np.vstack((leaves_pixels, node))
+            elif degree > 2 and node not in top_line:
+                junctions_pixels = np.vstack((junctions_pixels, node))
+            elif degree > 2 and node in top_line:
+                base_pixels = np.vstack((base_pixels, node))
+
+        # Clean up - uniquify pixels if they are in a group. Touching junction_pixels
+        # should be reduced to a single junction pixel, and the same for leaves_pixels.
+        # For this one needs to compute the distance matrix and group pixels that are
+        # close to each other, then replace each group by the mean pixel of the group.
+        def uniquify_pixels(pixels: np.ndarray) -> np.ndarray:
+            if len(pixels) == 0:
+                return pixels
+
+            # Strategy: Keep track of all unvisited pixels. Pick an unvisited pixel,
+            # compute the distance to all other pixels, and group those that are close.
+            # Then mark all pixels in the group as visited and repeat until all are visited.
+            unvisited = pixels.copy()
+            close_groups = []
+            while len(unvisited) > 0:
+                pixel = unvisited[0]
+                distance = nx.shortest_path_length(skeleton_graph, source=tuple(pixel))
+                close_pixels = unvisited[
+                    np.array([distance[tuple(p)] for p in unvisited])
+                    < max_group_pixel_distance
+                ]
+                close_groups.append(close_pixels)
+                unvisited = np.array([p for p in unvisited if p not in close_pixels])
+
+            # Replace each group by the mean pixel of the group
+            unique_pixels = []
+            for group in close_groups:
+                print(group)
+                group_representative = group[0]
+                unique_pixels.append(group_representative)
+            return np.array(unique_pixels)
+
+        leaves_pixels = uniquify_pixels(leaves_pixels)
+        junctions_pixels = uniquify_pixels(junctions_pixels)
+
+        # Sort peaks and valleys - no reason why the contours have been traversed not ordered.
+        arg_sorted_leaves_pixels = np.argsort(leaves_pixels[:, 0], axis=0)
+        arg_sorted_junctions_pixels = np.argsort(junctions_pixels[:, 0], axis=0)
+        arg_sorted_base_pixels = np.argsort(base_pixels[:, 0], axis=0)
+
+        sorted_leaves_pixels = leaves_pixels[arg_sorted_leaves_pixels]
+        sorted_junctions_pixels = junctions_pixels[arg_sorted_junctions_pixels]
+        sorted_base_pixels = base_pixels[arg_sorted_base_pixels]
+
+        reshaped_leaves_pixels = np.reshape(sorted_leaves_pixels, (-1, 1, 2))
+        reshaped_junctions_pixels = np.reshape(sorted_junctions_pixels, (-1, 1, 2))
+        reshaped_base_pixels = np.reshape(sorted_base_pixels, (-1, 1, 2))
+
+        return reshaped_leaves_pixels, reshaped_junctions_pixels, reshaped_base_pixels
+
+    @darsia.timing_decorator
+    def plot_skeleton(
+        self,
+        img: darsia.Image,
+        skeleton: np.ndarray,
+        leaves: np.ndarray | None = None,
+        junctions: np.ndarray | None = None,
+        base_junctions: np.ndarray | None = None,
+        roi: darsia.CoordinateArray | None = None,
+        path: Path | None = None,
+        show: bool = True,
+        dpi: int = 1000,
+        **kwargs,
+    ) -> None:
+        """Plot skeleton with leaves and junctions on top of the provided image.
+
+        Args:
+            img (darsia.Image): image to plot on.
+            skeleton (np.ndarray): pixels of the skeleton.
+            roi (darsia.CoordinateArray | None): region of interest. If provided, skeleton is
+                translated to the top left corner of the ROI; default is None.
+            path (Path, optional): path to save the plot; if None, no saving is performed.
+            show (bool): flag controlling whether the plot is shown; default is True.
+            dpi (int): dots per inch for the saved plot; default is 1000.
+            **kwargs: additional keyword arguments for plotting.
+                - color (str): color for the skeleton; default is "r".
+                - size (int): size for the skeleton; default is 20.
+
+        """
+        # Extract the top left pixel of the roi. NOTE: Need to swap for matplotlib,
+        # which uses (x, y) convention for pixels, while the image uses (row, column)
+        # convention.
+        top_left_roi_pixel, bottom_right_roi_pixel = _corners_of_roi(img, roi)
+        top_left_roi_index = (top_left_roi_pixel[1], top_left_roi_pixel[0])
+
+        # Start with original image in the background.
+        img = img.img
+
+        # Plot the image and the skeleton on top.
+        plt.figure("Image with skeleton")
+        plt.imshow(img, zorder=0)
+
+        try:
+
+            # Display skeleton on top of original mask and contour mask for debugging.
+            skeleton_coordinates = np.argwhere(skeleton)
+            if len(skeleton_coordinates) > 0:
+                skeleton_coordinates += top_left_roi_index  # Need row/col format
+
+            # Overlay the skeleton with skeleton color just at the skeleton pixels
+            skeleton_color = kwargs.get("skeleton_color", "w")
+            skeleton_linewidth = kwargs.get("skeleton_linewidth", 3)
+            plt.scatter(
+                skeleton_coordinates[:, 1],
+                skeleton_coordinates[:, 0],
+                c=skeleton_color,
+                s=skeleton_linewidth,
+            )
+
+            # Overlay with the contour
+            plt.plot(
+                self.contour[:, 0, 0] + top_left_roi_pixel[0],
+                self.contour[:, 0, 1] + top_left_roi_pixel[1],
+                c=kwargs.get("contour_color", "w"),
+                linewidth=kwargs.get("contour_linewidth", 1),
+                alpha=kwargs.get("contour_alpha", 0.5),
+                zorder=2,
+            )
+
+            plt.scatter(
+                # Translate pixels to the top left corner of the ROI
+                leaves[:, 0, 1] + top_left_roi_pixel[0],
+                leaves[:, 0, 0] + top_left_roi_pixel[1],
+                c=kwargs.get("leaf_color", "g"),
+                s=kwargs.get("leaf_size", 20),
+                edgecolors=kwargs.get("leaf_edge_color", "k"),
+                zorder=10,
+            )
+
+            plt.scatter(
+                # Translate pixels to the top left corner of the ROI
+                junctions[:, 0, 1] + top_left_roi_pixel[0],
+                junctions[:, 0, 0] + top_left_roi_pixel[1],
+                c=kwargs.get("junction_color", "b"),
+                s=kwargs.get("junction_size", 20),
+                edgecolors=kwargs.get("leaf_edge_color", "k"),
+                zorder=10,
+            )
+
+            plt.scatter(
+                # Translate pixels to the top left corner of the ROI
+                base_junctions[:, 0, 1] + top_left_roi_pixel[0],
+                base_junctions[:, 0, 0] + top_left_roi_pixel[1],
+                c=kwargs.get("base_junction_color", "m"),
+                s=kwargs.get("base_junction_size", 20),
+                edgecolors=kwargs.get("leaf_edge_color", "k"),
+                zorder=10,
+            )
+        except Exception as e:
+            logger.error(f"Failed to plot skeleton: {e}")
+
+        if kwargs.get("plot_boundary", False):
+            plt.gca().add_patch(
+                plt.Rectangle(
+                    (top_left_roi_pixel[0], top_left_roi_pixel[1]),
+                    bottom_right_roi_pixel[0] - top_left_roi_pixel[0],
+                    bottom_right_roi_pixel[1] - top_left_roi_pixel[1],
+                    linewidth=kwargs.get("boundary_linewidth", 2),
+                    edgecolor=kwargs.get("boundary_color", "y"),
+                    facecolor="none",
+                )
+            )
+        if kwargs.get("highlight_roi", False):
+            # Add dark overlay to the area outside the ROI
+            plt.gca().add_patch(
+                plt.Rectangle(
+                    (0, 0), img.img.shape[1], img.img.shape[0], color="black", alpha=0.5
+                )
+            )
+            plt.gca().add_patch(
+                plt.Rectangle(
+                    (top_left_roi_pixel[0], top_left_roi_pixel[1]),
+                    bottom_right_roi_pixel[0] - top_left_roi_pixel[0],
+                    bottom_right_roi_pixel[1] - top_left_roi_pixel[1],
+                    color="white",
+                    alpha=0.5,
+                )
+            )
+        if path is not None:
+            plt.tight_layout()
+            plt.axis("off")
+            plt.savefig(path, format="png", dpi=dpi, bbox_inches="tight", pad_inches=0)
+
+        if show:
+            plt.show()
+        else:
+            plt.close()
+
