8b918966b8a80f672e0c52673ac240b30ba0c010
Teggo19 <trygveteg@gmail.com>
2026-06-15 14:33:34 +0200

    Added functionality to read any .toml setting file

--- src/darsia/gui/gui_main.py
+++ src/darsia/gui/gui_main.py
@@ -1,4 +1,6 @@
-from PySide6.QtWidgets import QMainWindow, QApplication, QLabel, QWidget, QVBoxLayout, QPushButton, QLineEdit, QTextEdit, QSlider, QProgressBar, QComboBox, QListWidget, QRadioButton, QHBoxLayout
+from PySide6.QtWidgets import (QScrollArea,QMainWindow, QApplication, QLabel, QWidget, QVBoxLayout, QPushButton,
+                               QLineEdit, QTextEdit, QSlider, QProgressBar, QComboBox, QListWidget, QRadioButton,
+                               QHBoxLayout)
 from PySide6.QtCore import Qt
 import toml
 
@@ -8,90 +10,124 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("Darsia")
 
-        container = QWidget()
-        self.setCentralWidget(container)
-
-        layout = QVBoxLayout(container)
+        # Create main container for scroll area
+        main_container = QWidget()
+        main_layout = QVBoxLayout(main_container)
 
         label = QLabel("Settings:")
-        layout.addWidget(label)
+        main_layout.addWidget(label)
+        self.subsections = {}
 
-        settings_dict = config_dict["slider_settings"]
-        settings_container = QWidget()
-        h_layout = QHBoxLayout(settings_container)
-        self.sliders = {}
-        for key, value in settings_dict.items():
-            slider_label = QLabel(key)
-            slider_value_label = QLabel(str(value))
-            slider = QSlider(Qt.Horizontal)
-            slider.setMinimum(0)
-            slider.setMaximum(10)
-            slider.setValue(value)
-            self.sliders[key] = slider
-            h_layout.addWidget(slider_label)
-            h_layout.addWidget(slider)
-            h_layout.addWidget(slider_value_label)
+        for key, value in config_dict.items():
+            #local_container = QWidget()
+            #local_layout = QVBoxLayout(local_container)
+            #label = QLabel(key)
+            #local_layout.addWidget(label)
 
-            slider.valueChanged.connect(lambda v, lbl=slider_value_label: lbl.setText(str(v)))
-        layout.addWidget(settings_container)
+            self.subsections[key] = {}
+            self.unravel_settings(key, value, [], main_layout)
+            """
+            for loc_key, loc_value in value.items():
+                setting_container = QWidget()
+                setting_layout = QHBoxLayout(setting_container)
+                input_label = QLabel(loc_key)
+                line_edit = QLineEdit()
+                line_edit.setText(str(loc_value))
+                self.subsections[key][loc_key] = line_edit
+                setting_layout.addWidget(input_label)
+                setting_layout.addWidget(line_edit)
+                local_layout.addWidget(setting_container)
+            """
+            #main_layout.addWidget(local_container)
 
-        input_settings_dict = config_dict["input_settings"]
-        input_settings_container = QWidget()
-        v_layout = QVBoxLayout(input_settings_container)
-        self.input_settings = {}
-        for key, value in input_settings_dict.items():
-            local_container = QWidget()
-            h_layout = QHBoxLayout(local_container)
-            input_label = QLabel(key)
-            line_edit = QLineEdit()
-            line_edit.setText(str(value))
-            self.input_settings[key] = line_edit
-            h_layout.addWidget(input_label)
-            h_layout.addWidget(line_edit)
-            v_layout.addWidget(local_container)
-        layout.addWidget(input_settings_container)
+        # Add scroll area
+        scroll_area = QScrollArea()
+        scroll_area.setWidget(main_container)
+        scroll_area.setWidgetResizable(True)
 
-        menu_container = QWidget()
-        menu_layout_vertical = QVBoxLayout(menu_container)
-        self.menu_settings = {}
-        menu_settings_dict = config_dict["menu_settings"]
-        options = ["Option 1", "Option 2", "Option 3"]
-        for key, value in menu_settings_dict.items():
-            combo_box = QComboBox()
-            combo_box.addItems(options)
-            self.menu_settings[key] = combo_box
-            for i, option in enumerate(options):
-                if option == value:
-                    combo_box.setCurrentIndex(i)
-            menu_layout_vertical.addWidget(QLabel(key))
-            menu_layout_vertical.addWidget(combo_box)
-        layout.addWidget(menu_container)
+        # Create central widget and set scroll area
+        container = QWidget()
+        self.setCentralWidget(container)
+        layout = QVBoxLayout(container)
+        layout.addWidget(scroll_area)
 
         button = QPushButton("Save Settings")
         button.clicked.connect(self.saveSettings)
 
         layout.addWidget(button)
 
+
     def saveSettings(self):
-        setting_dict = {}
-        for key, slider in self.sliders.items():
-            setting_dict[key] = slider.value()
+        total_dict = {}
+        """
+        for key, value in self.subsections.items():
+            local_dict = {}
+            for loc_key, line_edit in value.items():
+                local_dict[loc_key] = int(line_edit.text())
+            total_dict[key] = local_dict"""
+        for key, value in self.subsections.items():
+            #total_dict[key] = {}
+            self.get_save_dict(key, value, [], total_dict)
 
-        line_edit_dict = {}
-        for key, line_edit in self.input_settings.items():
-            line_edit_dict[key] = int(line_edit.text())
 
-        menu_edit_dict = {}
-        for key, menu in self.menu_settings.items():
-            menu_edit_dict[key] = menu.currentText()
-
-        total_dict = {"slider_settings": setting_dict, "input_settings": line_edit_dict, "menu_settings": menu_edit_dict}
         with open("test.toml", "w") as f:
             toml.dump(total_dict, f)
+        print("Settings saved")
+
+    def get_save_dict(self, inp_key, inp_val, path, total_dict):
+        if type(inp_val) == dict:
+            local_dict = total_dict
+            for key in path:
+                local_dict = local_dict[key]
+            local_dict[inp_key] = {}
+            for key, value in inp_val.items():
+                self.get_save_dict(key, value, path + [inp_key], total_dict)
+        else:
+            set_dict_value(total_dict, path+[inp_key], inp_val.text())
+
+
+
+
+
+    def unravel_settings(self, inp_key, inp_val, path, parent_layout):
+        if type(inp_val) == dict:
+            local_container = QWidget()
+            local_layout = QVBoxLayout(local_container)
+            label = QLabel(inp_key)
+            local_layout.addWidget(label)
+            parent_layout.addWidget(local_container)
+            loc_dict = self.subsections
+            for key in path:
+                loc_dict = loc_dict[key]
+            loc_dict[inp_key] = {}
+
+            for key, value in inp_val.items():
+                self.unravel_settings(key, value, path + [inp_key], local_layout)
+        else:
+            setting_container = QWidget()
+            setting_layout = QHBoxLayout(setting_container)
+            input_label = QLabel(inp_key)
+            line_edit = QLineEdit()
+            line_edit.setText(str(inp_val))
+
+            loc_dict = self.subsections
+            for key in path:
+                loc_dict = loc_dict[key]
+            loc_dict[inp_key] = line_edit
+            setting_layout.addWidget(input_label)
+            setting_layout.addWidget(line_edit)
+            parent_layout.addWidget(setting_container)
+
+
+def set_dict_value(inp_dict, path, value):
+    local_dict = inp_dict
+    for key in path[:-1]:
+        local_dict = local_dict[key]
+    local_dict[path[-1]] = value
 
 if __name__ == '__main__':
 
-    with open("test.toml", "r") as f:
+    with open("common.toml", "r") as f:
         config = toml.load(f)
 
 
