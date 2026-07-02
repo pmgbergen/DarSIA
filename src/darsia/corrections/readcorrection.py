a11598106deea3283da12c06d585d633c33cb356
Jakub Both <jakub.both@uib.no>
2026-07-02 14:26:01 +0200

    MAINT: Integrate Patchwise illumination correction to standard corrections

--- src/darsia/corrections/readcorrection.py
+++ src/darsia/corrections/readcorrection.py
@@ -9,6 +9,7 @@ from darsia import (
     CurvatureCorrection,
     DriftCorrection,
     IlluminationCorrection,
+    PatchwiseIlluminationCorrection,
     Resize,
     TypeCorrection,
 )
@@ -18,6 +19,7 @@ AnyCorrection = (
     | DriftCorrection
     | CurvatureCorrection
     | IlluminationCorrection
+    | PatchwiseIlluminationCorrection
     | ColorCorrection
     | Resize
 )
