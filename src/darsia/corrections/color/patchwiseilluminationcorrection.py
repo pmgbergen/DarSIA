050091ac7c8b81b9d47d29e2b55781b3e6f73d20
Thibault Vautier <thibault.vautier@insa-lyon.fr>
2026-07-02 10:41:09 +0200

    add extension of correction coefficients to the full image , and possibility to have more than 2 baseline images

--- src/darsia/corrections/color/patchwiseilluminationcorrection.py
+++ src/darsia/corrections/color/patchwiseilluminationcorrection.py
@@ -7,8 +7,7 @@ class PatchwiseIlluminationCorrection:
     def __init__(
         self,
         image_path: str,
-        baseline_path1: str,
-        baseline_path2: str,
+        baseline_paths: list[str],
         nw: int = 1000,
         limit: int = 1450,
         show_images: bool = True,
@@ -25,24 +24,42 @@ class PatchwiseIlluminationCorrection:
         if self.img is None:
             raise ValueError(f"Image not found : {image_path}")
 
-        self.baseline1 = cv2.imread(baseline_path1)
-        if self.baseline1 is None:
-            raise ValueError(f"Image not found : {baseline_path1}")
+        self.baseline_images = []
+        for path in baseline_paths:
+            baseline = cv2.imread(path)
+            if baseline is None:
+                raise ValueError(f"Image not found : {path}")
+            self.baseline_images.append(baseline)
 
-        self.baseline2 = cv2.imread(baseline_path2)
-        if self.baseline2 is None:
-            raise ValueError(f"Image not found : {baseline_path2}")
+        n_baseline_images = len(self.baseline_images)
 
-        
         self.height, self.width = self.img.shape[:2]
         self.nh = int((self.height - self.limit) * self.nw / self.width)
-        self.dh = int((self.height - self.limit) / self.nh)
-        self.dw = int(self.width / self.nw)
+        self.dh = ((self.height - self.limit) / self.nh)
+        self.dw = self.width / self.nw
 
-        r1, g1, b1 = self.extract_color_values_patches(self.baseline1)
-        r2, g2, b2 = self.extract_color_values_patches(self.baseline2)
+        r, g, b = [], [], []
+        r_mean, g_mean, b_mean = [], [], []
 
-        self.r_diff, self.g_diff, self.b_diff = self.correction_coefficient(r1, g1, b1, r2, g2, b2)
+        for i in range(n_baseline_images):
+            ri, gi, bi = self.extract_color_values_patches(self.baseline_images[i])
+            r.append(ri)
+            g.append(gi)
+            b.append(bi)
+            r_m = np.mean(ri)
+            g_m = np.mean(gi)
+            b_m = np.mean(bi)
+            r_mean.append(r_m)
+            g_mean.append(g_m)
+            b_mean.append(b_m)
+        
+        self.r_diff = self.compute_correction(r, r_mean)
+        self.g_diff = self.compute_correction(g, g_mean)
+        self.b_diff = self.compute_correction(b, b_mean)
+
+        self.r_diff = self.extend_correction_coefficients(self.r_diff)
+        self.g_diff = self.extend_correction_coefficients(self.g_diff)
+        self.b_diff = self.extend_correction_coefficients(self.b_diff)
       
         image_calibrated = self.correct_array(self.img)
 
@@ -66,13 +83,13 @@ class PatchwiseIlluminationCorrection:
 
         for i in range(self.nh):
 
+            y0 = max(self.limit + int(round((i - 0.5) * self.dh)), 0)
+            y1 = min(self.limit + int(round((i + 0.5) * self.dh)), self.height)
+
             for j in range(self.nw):
 
-                y0 = max(self.limit + int((i - 0.5) * self.dh), 0)
-                y1 = min(self.limit + int((i + 0.5) * self.dh), self.height)
-
-                x0 = max(int((j - 0.5) * self.dw), 0)
-                x1 = min(int((j + 0.5) * self.dw), self.width)
+                x0 = max(int(round((j - 0.5) * self.dw)), 0)
+                x1 = min(int(round((j + 0.5) * self.dw)), self.width)
 
                 roi = image[y0:y1, x0:x1]
 
@@ -84,32 +101,72 @@ class PatchwiseIlluminationCorrection:
 
         return np.array(r), np.array(g), np.array(b)
 
+    def extract_color_values_patches_no_limit(self, image: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+        """
+        Extract RGB values from image patches on the full image.
+
+        Returns:
+            Tuple containing R, G, B matrices.
+        """
+        nh = self.nh + int(self.limit / self.dh)
+        r = np.zeros((nh, self.nw), dtype=np.float32)
+        g = np.zeros((nh, self.nw), dtype=np.float32)
+        b = np.zeros((nh, self.nw), dtype=np.float32)
+
+        for i in range(nh):
+            y0 = max(int(round((i - 0.5) * self.dh)), 0)
+            y1 = min(int(round((i + 0.5) * self.dh)), self.height)
+
+            for j in range(self.nw):
+                x0 = max(int(round((j - 0.5) * self.dw)), 0)
+                x1 = min(int(round((j + 0.5) * self.dw)), self.width)
+                
+                roi = image[y0:y1, x0:x1]
+                mean_color = cv2.mean(roi)
+
+                r[i, j] = int(mean_color[2])
+                g[i, j] = int(mean_color[1])
+                b[i, j] = int(mean_color[0])
+
+        return np.array(r), np.array(g), np.array(b)
      
-    def correction_coefficient(self, r1: np.ndarray, g1: np.ndarray, b1: np.ndarray, r2: np.ndarray, g2: np.ndarray, b2: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    def compute_correction(
+        self,
+        coefficient_list: list[np.ndarray],
+        coefficient_mean_list: list[np.ndarray],
+    ) -> np.ndarray:
         """Calculate correction coefficients based on baseline images."""
-        eps = self.eps
-        r_m1 = np.mean(r1)
-        g_m1 = np.mean(g1)
-        b_m1 = np.mean(b1)
-        r_m2 = np.mean(r2)
-        g_m2 = np.mean(g2)
-        b_m2 = np.mean(b2)
-        r_diff2 = 1 / ((r1**2)/(r1**2+r2**2+eps) * r_m1/(r1+eps) + (r2**2)/(r1**2+r2**2+eps) * r_m2/(r2+eps)+eps)
-        g_diff2 = 1 / ((g1**2)/(g1**2+g2**2+eps) * g_m1/(g1+eps) + (g2**2)/(g1**2+g2**2+eps) * g_m2/(g2+eps)+eps)
-        b_diff2 = 1 / ((b1**2)/(b1**2+b2**2+eps) * b_m1/(b1+eps) + (b2**2)/(b1**2+b2**2+eps) * b_m2/(b2+eps)+eps)
-        return r_diff2, g_diff2, b_diff2
+
+        sum_sq = np.sum([r**2 for r in coefficient_list], axis=0)
+
+        correction = np.zeros_like(sum_sq, dtype=float)
+
+        for r, r_m in zip(coefficient_list, coefficient_mean_list):
+            weight = (r**2) / (sum_sq + self.eps)
+            correction += weight * (r_m / (r + self.eps))
+
+        return 1.0 / (correction + self.eps)
+    
+    def extend_correction_coefficients(self, corr: np.ndarray) -> np.ndarray:
+        """Extend correction coefficients to the upper part of the image."""
+        new_corr = np.zeros((int(self.limit/self.dh), self.nw))
+        lim = int(self.nh/3)
+        for col in range (self.nw):
+            avg_top = np.mean(corr[:lim, col])
+            new_corr[:, col] = avg_top
+        return np.vstack((new_corr, corr))
 
     def correct_array(self, img: np.ndarray) -> np.ndarray:
         """Apply patchwise illumination correction to the input image."""
 
-        r, g, b = self.extract_color_values_patches(img)
+        r, g, b = self.extract_color_values_patches_no_limit(img)
         
         r_new = r/self.r_diff
         g_new = g/self.g_diff
         b_new = b/self.b_diff
 
         image_calib = cv2.merge((b_new.astype(np.uint8), g_new.astype(np.uint8), r_new.astype(np.uint8)))
-        img_rebuilt = cv2.resize(image_calib, (self.width, self.height-self.limit), interpolation=cv2.INTER_LINEAR)
+        img_rebuilt = cv2.resize(image_calib, (self.width, self.height), interpolation=cv2.INTER_LINEAR)
 
         return img_rebuilt
 
