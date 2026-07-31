357e99993a0697d13c91d8319cbee5ad7386920a
Jakub Both <jakub.both@uib.no>
2026-07-31 07:47:16 +0200

    DOC: Add docstring to gui main

--- src/darsia/gui/__main__.py
+++ src/darsia/gui/__main__.py
@@ -1,3 +1,14 @@
+"""Entry point for the GUI application.
+
+Run the GUI using one of the following methods:
+
+1. From anywhere (recommended):
+    python -m darsia.gui
+
+2. From the darsia.gui directory:
+    python __main__.py
+"""
+
 from .gui_main import MainWindow
 from PySide6.QtWidgets import QApplication
 
