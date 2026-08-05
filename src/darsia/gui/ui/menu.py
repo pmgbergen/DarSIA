3c86b12bdf6a1fe18564d94577193f194ea39f67
Jakub Both <jakub.both@uib.no>
2026-08-05 13:12:57 +0200

    FEAT: Add menubar to gui

---
+++ src/darsia/gui/ui/menu.py
@@ -0,0 +1,27 @@
+from PySide6.QtGui import QAction
+
+
+class MenuBuilder:
+    """Builds the main window's menu bar from a declarative structure.
+
+    To add a new menu, add an entry to build(); to add an action to an existing
+    menu, add an _add_action call. No other wiring is required.
+    """
+
+    def __init__(self, main_window):
+        self.main_window = main_window
+
+    def build(self):
+        menu_bar = self.main_window.menuBar()
+
+        file_menu = menu_bar.addMenu("&File")
+        self._add_action(file_menu, "&Open Config...", self.main_window.open_config)
+
+        help_menu = menu_bar.addMenu("&Help")
+        self._add_action(help_menu, "&About", self.main_window.show_about_dialog)
+
+    def _add_action(self, menu, text, handler):
+        action = QAction(text, self.main_window)
+        action.triggered.connect(handler)
+        menu.addAction(action)
+        return action
